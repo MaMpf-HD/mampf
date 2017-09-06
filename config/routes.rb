943cb2371d4a6c4d6d446fc5a00a3d83d5a28c14
Denis Vogel <denisvgl@gmail.com>
2017-09-06 18:18:03 +0200

    added StaticPages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
+
+  root 'static_pages#home'
+  get 'about', to: 'static_pages#about'
+  get 'static_pages/home'
+  get 'static_pages/about'
+
   namespace :api do
     namespace :v1 do
-      get 'tags', :to => 'tags#index'
-      get 'tags/:id', :to => 'tags#show'
-      get 'keks_questions/:id', :to => 'media#keks_question'
+      get 'tags', to: 'tags#index'
+      get 'tags/:id', to: 'tags#show'
+      get 'keks_questions/:id', to: 'media#keks_question'
     end
   end
 
---
+++ spec/controllers/static_pages_controller_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe StaticPagesController, type: :controller do
+
+  describe "GET #home" do
+    it "returns http success" do
+      get :home
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #about" do
+    it "returns http success" do
+      get :about
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
