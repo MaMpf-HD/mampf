34ca8da7b2ecf4feb9f28fb19e5288f4e2ffa826
Denis Vogel <denisvgl@gmail.com>
2017-09-10 20:07:18 +0200

    JSON response in API renders _video partial

--- app/controllers/api/v1/media_controller.rb
+++ app/controllers/api/v1/media_controller.rb
@@ -2,6 +2,19 @@ class Api::V1::MediaController < ApplicationController
   respond_to :json
   def keks_question
     @medium = Medium.KeksQuestion.find_by(question_id: params[:id])
-    render :json => @medium, width: params[:width]
+    if @medium != nil then
+      width = params[:width].to_i
+      aspect_ratio = @medium.width.to_f / @medium.height
+      height = (width.to_i/aspect_ratio).to_i.to_s
+      render json: { medium: MediumSerializer.new(@medium),
+                     embedded_video:
+                       render_to_string(partial: 'shared/video',
+                                        formats: :html,
+                                        layout: false,
+                                        locals: { medium: @medium, width: width,
+                                                  height: height}) }
+    else
+      render json: { medium: {}, embedded_video: ''}
+    end
   end
 end
--- app/serializers/medium_serializer.rb
+++ app/serializers/medium_serializer.rb
@@ -1,21 +1,3 @@
 class MediumSerializer < ActiveModel::Serializer
-  attributes :video_file_link, :embedded_video
-  def embedded_video
-    unless object.video_file_link.nil?
-      aspect_ratio = object.width.to_f / object.height
-      width = @instance_options[:width]
-      if width != nil then
-        height = (width.to_i/aspect_ratio).to_i.to_s
-        dimensions = 'width="' + width + '" height="' + height +'"'
-      else
-        dimensions = ''
-      end
-      html =  ['<video ', dimensions, ' controls><source src="',
-               object.video_file_link, '" type="video/mp4"></video>']
-      html.join
-    end
-   end
-   def passed
-     @instance_options[:width]
-   end
+  attributes :video_file_link
 end
