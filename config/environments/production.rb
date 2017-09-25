00eeea8c7bd38ab52b1264f76eef91a9c68f1b58
Denis Vogel <denisvgl@gmail.com>
2017-09-25 17:19:02 +0200

    disabled public_file_server

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,7 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.public_file_server.enabled = true
+  config.public_file_server.enabled = false
 
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
