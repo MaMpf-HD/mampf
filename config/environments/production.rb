f58adbdd8c53526e3df6cf3cfd5c659571197a42
Denis Vogel <denisvgl@gmail.com>
2017-10-11 11:15:41 +0200

    layout changes, configured action_mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,18 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "mampf_#{Rails.env}"
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+
+  config.action_mailer.smtp_settings = {
+    address: ENV["MAILSERVER"]
+    port: 25,
+    domain: ENV["MAILSERVER"],
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
