8fa86cb2904e1f7bacbc6f733072699d01353cad
Denis Vogel <denisvgl@gmail.com>
2017-10-11 17:29:10 +0200

    added german localization for password reset mail

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,7 +1,11 @@
 class RegistrationsController < Devise::RegistrationsController
 
   def after_sign_up_path_for(resource)
-    edit_profile_path
+    if resource.sign_in_count == 1
+      welcome_path
+    else
+      root_path
+    end
   end
 
   private
