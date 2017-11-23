fe06eb888df5e4034117a6c3ce87752eec3bd9f5
Denis Vogel <denisvgl@gmail.com>
2017-11-23 19:13:55 +0100

    improved handling of misspelled searches

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -4,7 +4,7 @@ module SearchHelper
 
   def similar_tags(search_string)
     jarowinkler = FuzzyStringMatch::JaroWinkler.create(:pure)
-    tags = Tag.where(id: Tag.all.select{ |t| jarowinkler.getDistance(t.title, search_string) > 0.9 }
+    tags = Tag.where(id: Tag.all.select{ |t| jarowinkler.getDistance(t.title.downcase, search_string.downcase) > 0.9 }
                             .map(&:id))
   end
 end
