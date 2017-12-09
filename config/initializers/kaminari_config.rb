07fa5b308dfeb216fac576f1f84b9204bed913b9
Denis Vogel <denisvgl@gmail.com>
2017-12-09 17:34:38 +0100

    resolved conflict between rails_admin and kaminari

--- config/initializers/kaminari_config.rb
+++ config/initializers/kaminari_config.rb
@@ -6,7 +6,7 @@ Kaminari.configure do |config|
   # config.outer_window = 0
   # config.left = 0
   # config.right = 0
-  # config.page_method_name = :page
   # config.param_name = :page
   # config.params_on_first_page = false
+  config.page_method_name = :per_page_kaminari
 end
