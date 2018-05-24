6ab1bba0e540c07153c79860d5e46a5383f8a4b7
Denis Vogel <denisvgl@gmail.com>
2018-05-24 14:04:24 +0200

    fixed specs

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     email { Faker::Internet.email }
     password {Faker::Internet.password}
     subscription_type Faker::Number.between(1, 3)
-    sign_in_count Faker::Number.between(5, 10)
+    consents true
     after(:build) { |user| user.lectures = FactoryBot.create_list(:lecture, 2) }
   end
 end
