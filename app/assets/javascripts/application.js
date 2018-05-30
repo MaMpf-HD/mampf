f9fd4a982b407bb5d9db9b0054464965d62b3d35
Denis Vogel <denisvgl@gmail.com>
2018-05-30 21:30:03 +0200

    bugfix reset password dsgvo

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery3
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
 
-//= require jquery3
 //= require popper
 //= require bootstrap
