418e049fb4784332467807981638affb394f526f
fosterfarrell9 <28628554+fosterfarrell9@users.noreply.github.com>
2018-09-05 17:56:27 +0200

    change file names in migration for relocation of media

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_05_085400) do
+ActiveRecord::Schema.define(version: 2018_09_05_134510) do
 
   create_table "chapters", force: :cascade do |t|
     t.integer "lecture_id"
