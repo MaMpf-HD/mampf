87a2c4fd890676addea931bcfeeff77fcef6492e
fosterfarrell9 <28628554+fosterfarrell9@users.noreply.github.com>
2018-09-07 14:09:17 +0200

    add environment variables for development

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,9 @@
 # Load the Rails application.
 require_relative 'application'
 
+# Load the app's custom environment variables here, so that they are loaded before environments/*.rb
+app_environment_variables = File.join(Rails.root, 'config', 'app_environment_variables.rb')
+load(app_environment_variables) if File.exists?(app_environment_variables)
+
 # Initialize the Rails application.
 Rails.application.initialize!
--- config/initializers/default_setting.rb
+++ config/initializers/default_setting.rb
@@ -3,12 +3,5 @@ class DefaultSetting
   ERDBEERE_LINK = ENV['ERDBEERE_SERVER']
   MUESLI_LINK = ENV['MUESLI_SERVER']
   PROJECT_EMAIL = ENV['PROJECT_EMAIL']
-  KEKS_QUESTION_LINK = 'https://keks.mathi.uni-heidelberg.de/hitme' \
-                       '#hide-options#hide-categories#question='
-  VIDEO_WIDTH = 1620
-  VIDEO_HEIGHT = 1080
-  EMBEDDED_WIDTH = 1080
-  EMBEDDED_HEIGHT = 720
-  AUTHORING_SOFTWARE = 'Camtasia 9'
-  VIDEO_PLAYER = 'TechSmith Smart Player'
+  KEKS_QUESTION_LINK = KEKS_LINK + 'hitme/#hide-options#hide-categories#question='
 end
