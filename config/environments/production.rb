29f417928db9ee358ed44c158a6469f18487e4e5
fosterfarrell9 <28628554+fosterfarrell9@users.noreply.github.com>
2018-09-13 17:39:29 +0200

    change to old secrets.yml instaed of master.key

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,7 +18,7 @@ Rails.application.configure do
   # Requires an encryption key in `ENV["RAILS_MASTER_KEY"]` or
   # `config/secrets.yml.key`.
   # config.read_encrypted_secrets = true
-  config.require_master_key = true
+  config.require_master_key = false
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
