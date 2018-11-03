64ddd8a733c1e8564ca515f20073bd4ae476f6a5
fosterfarrell9 <28628554+fosterfarrell9@users.noreply.github.com>
2018-11-03 11:58:09 +0100

    bugfix exit administration if course cookie is not present

--- app/controllers/administration_controller.rb
+++ app/controllers/administration_controller.rb
@@ -6,7 +6,11 @@ class AdministrationController < ApplicationController
 
   def exit
   	course_id = cookies[:current_course]
-    redirect_to course_path(course_id)
+  	if course_id.present?
+  	  redirect_to course_path(course_id)
+ 			return
+ 		end
+ 		redirect_to root_path
   end
 
   def profile
