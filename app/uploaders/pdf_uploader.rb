9e58613d7cf3eea5abe93923d4a9d21a91f4b654
fosterfarrell9 <28628554+fosterfarrell9@users.noreply.github.com>
2019-02-17 15:44:15 +0100

    refactor pdf uploader

--- app/uploaders/pdf_uploader.rb
+++ app/uploaders/pdf_uploader.rb
@@ -16,6 +16,8 @@ class PdfUploader < Shrine
   # - named destinations
   # - bookmarks with details (created by mampf.sty LATeX package)
   add_metadata do |io, context|
+    # no metadata extraction for the screenshot which is generated during
+    # storing
     unless context[:action] == :store && context[:version] == :screenshot
       Shrine.with_file(io) do |file|
         temp_file = Tempfile.new
@@ -25,20 +27,31 @@ class PdfUploader < Shrine
           return { 'pages' => nil, 'destinations' => nil, 'bookmarks' => nil }
         end
         meta = File.read(temp_file)
+        # extract number of pages from pdftk output
         pages = /NumberOfPages: (\d*)/.match(meta)[1].to_i
+        # extract lines that correspond to MaMpf-Label entries from LaTEX
+        # package mampf.sty
         bookmarks = meta.scan(/BookmarkTitle: MaMpf-Label\|(.*?)\n/).flatten
         result = []
         bookmarks.each do |b|
+          # extract bookmark data
+          # line may look like this:
+          # defn:erster-Tag-der-Schoepfung|Definition|1.1|Erster Tag|1
           data = /(.*?)\|(.*?)\|(.*?)\|(.*?)\|(.*)/.match(b)
-          result.push({ "destination" => data[1], "sort" => data[2],
-                        "label" => data[3], "description" => data[4],
-                        "page" => data[5] })
+          result.push('destination' => data[1], 'sort' => data[2],
+                      'label' => data[3], 'description' => data[4],
+                      'page' => data[5])
         end
+        # extract named destinations using the origami gem
+        # (there may be named destinations not created by the mampf LATeX
+        # which would overlooked if we were to take
+        # result.map { |b| b.destination }
+        # instead
         pdf = Origami::PDF.read(file.path)
         if pdf.present?
           destinations = []
           pdf.each_named_dest do |d|
-          # enforce UTF-8, otherwise there are problems
+            # enforce UTF-8, otherwise there are problems
             destinations.push(d.value.force_encoding('UTF-8'))
           end
           # reject named destinations including spaces or "."
