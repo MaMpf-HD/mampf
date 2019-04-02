eb3d89ddadf36536f4ef0733f23620ba7e886021
fosterfarrell9 <28628554+fosterfarrell9@users.noreply.github.com>
2019-04-02 19:23:46 +0200

    circumvent foreign key constraint error in PG migration

--- db/migrate/20190401132545_drop_questions_remarks_quizzes_tables.rb
+++ db/migrate/20190401132545_drop_questions_remarks_quizzes_tables.rb
@@ -1,8 +1,10 @@
 class DropQuestionsRemarksQuizzesTables < ActiveRecord::Migration[5.2]
   def up
+    remove_reference :answers, :question, index: true, foreign_key: true
     drop_table :questions
     drop_table :remarks
     drop_table :quizzes
+    add_reference :answers, :question, foreign_key: true
   end
 
   def down
