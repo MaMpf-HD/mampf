462293c9160f36aa39e48c9653276e5a7846d336
Tobias Wackenhut <tynsh@tynsh.moe>
2019-05-02 16:39:51 +0200

    write initialisation logs to separate logfile

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,12 +3,12 @@ echo "Initialising mampf"
 cd /usr/src/app
 if ! [ -f completed_initial_run ]
 then
-  echo running: bundle exec rails db:migrate
-  bundle exec rails db:migrate
-  echo running: bundle exec rails assets:precompile
-  bundle exec rails assets:precompile
+  echo running: bundle exec rails db:migrate &> >(tee -a /usr/src/app/log/initialisation.log)
+  bundle exec rails db:migrate &> >(tee -a /usr/src/app/log/initialisation.log)
+  echo running: bundle exec rails assets:precompile &> >(tee -a /usr/src/app/log/initialisation.log)
+  bundle exec rails assets:precompile &> >(tee -a /usr/src/app/log/initialisation.log)
   touch completed_initial_run
 fi
 rm -f tmp/pids/server.pid
 echo "running mampf"
-exec bundle exec rails s -p 3000 -b '0.0.0.0' > >(tee -a /usr/src/app/log/stdout.log) 2> >(tee -a /usr/src/app/log/stderr.log >&2)
+exec bundle exec rails s -p 3000 -b '0.0.0.0' &> >(tee -a /usr/src/app/log/runtime.log)
