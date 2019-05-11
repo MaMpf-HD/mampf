00782c0e13757b5f117131011b781a4fe03a081a
fosterfarrell9 <28628554+fosterfarrell9@users.noreply.github.com>
2019-05-11 05:15:34 +0200

    migrate tag titles to notions

--- db/migrate/20190508131550_migrate_tag_titles_to_notion.rb
+++ db/migrate/20190508131550_migrate_tag_titles_to_notion.rb
@@ -2,8 +2,5 @@ class MigrateTagTitlesToNotion < ActiveRecord::Migration[6.0]
   def change
     add_column :notions, :tag_id, :integer
     add_index :notions, :tag_id
-    Tag.all.each do |t|
-      Notion.create(title: t.title, locale: I18n.default_locale, tag_id: t.id)
-    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_10_233648) do
+ActiveRecord::Schema.define(version: 2019_05_11_031012) do
 
   create_table "announcements", force: :cascade do |t|
     t.integer "lecture_id"
