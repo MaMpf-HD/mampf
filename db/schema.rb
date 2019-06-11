a624830486b70af92b15b167956c30e23d365cc0
fosterfarrell9 <28628554+fosterfarrell9@users.noreply.github.com>
2019-06-11 11:05:50 +0200

    remove tilte backup from tag - migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_10_051931) do
+ActiveRecord::Schema.define(version: 2019_06_11_081201) do
 
   create_table "announcements", force: :cascade do |t|
     t.integer "lecture_id"
@@ -294,7 +294,6 @@ ActiveRecord::Schema.define(version: 2019_06_10_051931) do
   end
 
   create_table "tags", force: :cascade do |t|
-    t.string "title_backup"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
