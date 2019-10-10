b557baf3bff2eccda9e8034f24f0139a9686add4
fosterfarrell9 <28628554+fosterfarrell9@users.noreply.github.com>
2019-10-10 16:55:56 +0200

    add comment on cache busting

--- config/initializers/bust_cache.rb
+++ config/initializers/bust_cache.rb
@@ -1,1 +1,4 @@
+# This is used to prevent problems described in
+# https://ninjasandrobots.com/rails-caching-a-problem-with-etags-and-a-solution
+# see also https://brandonhilkert.com/blog/understanding-the-rails-cache-id-environment-variable/
 ENV["RAILS_CACHE_ID"] = Time.now.to_s
