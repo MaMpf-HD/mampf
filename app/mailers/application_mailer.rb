7220b14bb131ac9caa244551c000de3704beea99
fosterfarrell9 <28628554+fosterfarrell9@users.noreply.github.com>
2019-10-23 11:20:39 +0200

    experiment with change message ids for application mailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
   default from: DefaultSetting::PROJECT_EMAIL
-  default "Message-ID" => "#{Digest::SHA2.hexdigest(Time.now.to_i.to_s)}@#{ENV["MAILSERVER"]}"
+  default "Message-ID" => "#{Digest::SHA2.hexdigest(Time.now.to_i.to_s)}@mathi.uni-heidelberg.de}"
   layout 'mailer'
 end
