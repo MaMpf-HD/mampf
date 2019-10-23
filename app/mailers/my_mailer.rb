28c66fec3f2d6c7a177cbd1918c382eeca9f6390
fosterfarrell9 <28628554+fosterfarrell9@users.noreply.github.com>
2019-10-23 17:27:17 +0200

    randomize message ids

--- app/mailers/my_mailer.rb
+++ app/mailers/my_mailer.rb
@@ -3,5 +3,5 @@ class MyMailer < Devise::Mailer
   include Devise::Controllers::UrlHelpers # Optional. eg. `confirmation_url`
   default template_path: 'devise/mailer' # to make sure that your mailer uses the devise views
   default from: DefaultSetting::PROJECT_EMAIL
-  default "Message-ID" => "<#{self.object_id}.#{Time.now.to_i}@#{ENV['MAILID_DOMAIN']}>"
+  default "Message-ID" => "<#{rand.to_s.split('.')[1]}.#{Time.now.to_i}@#{ENV['MAILID_DOMAIN']}>"
 end
