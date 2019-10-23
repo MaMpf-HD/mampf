e7469d4e15cb4f534cb4dc0f0e8969bda21cfe41
fosterfarrell9 <28628554+fosterfarrell9@users.noreply.github.com>
2019-10-23 17:40:19 +0200

    properly randomize message ids using a lambda

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
   default from: DefaultSetting::PROJECT_EMAIL
-  default "Message-ID" => "<#{rand.to_s.split('.')[1]}.#{Time.now.to_i}@#{ENV['MAILID_DOMAIN']}>"
+  default "Message-ID" => -> {"<#{rand.to_s.split('.')[1]}.#{Time.now.to_i}@#{ENV['MAILID_DOMAIN']}>" }
   layout 'mailer'
 end
