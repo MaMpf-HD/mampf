890e97747e91fbb2137b16ade6c7ae6cfb721677
fosterfarrell9 <28628554+fosterfarrell9@users.noreply.github.com>
2020-03-08 13:51:41 +0100

    remove locale from erdbeere api path

--- app/controllers/examples_controller.rb
+++ app/controllers/examples_controller.rb
@@ -4,7 +4,7 @@ class ExamplesController < ApplicationController
 
 	def show
 		id = params[:id]
-    response = Faraday.get "https://erdbeere-dev.mathi.uni-heidelberg.de/de/api/v1/examples/#{id}"
+    response = Faraday.get "https://erdbeere-dev.mathi.uni-heidelberg.de/api/v1/examples/#{id}"
     json = JSON.parse(response.body)
     @content = json['embedded_html']
     render layout: 'application'
