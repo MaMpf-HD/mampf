b92d2c592967e4fdd448b8aa63cedc94a8c0ae29
Tobias Wackenhut <tobias@wackenhut.at>
2020-04-18 00:02:46 +0200

    revert back to tmpfs and rsync for sharing assets between master and proxy

--- docker/entrypoint-master.sh
+++ docker/entrypoint-master.sh
@@ -7,6 +7,8 @@ then
   bundle exec rails db:migrate
   echo 'running: bundle exec rake sunspot:solr:reindex'
   bundle exec rake sunspot:solr:reindex
+  echo 'copying public assets to volume'
+  rsync -av --delete public/ /public/
   echo 'finished initialisation'
   touch completed_initial_run
 fi
