9c985889da9e25635a765dcc5eaca585f4944705
Tobias Wackenhut <tobias@wackenhut.at>
2020-04-17 20:18:36 +0200

    configure Accel-Redirect for nginx

--- docker/entrypoint-master.sh
+++ docker/entrypoint-master.sh
@@ -2,12 +2,14 @@
 cd /usr/src/app
 if ! [ -f completed_initial_run ]
 then
-  echo 'Initialising mampf master container' &> >(tee -a /usr/src/app/log/initialisation.log)
-  echo running: bundle exec rails db:migrate &> >(tee -a /usr/src/app/log/initialisation.log)
-  bundle exec rails db:migrate &> >(tee -a /usr/src/app/log/initialisation.log)
-  echo running: bundle exec rake sunspot:solr:reindex &> >(tee -a /usr/src/app/log/initialisation.log)
-  bundle exec rake sunspot:solr:reindex &> >(tee -a /usr/src/app/log/initialisation.log)
-  echo 'finished initialisation' &> >(tee -a /usr/src/app/log/initialisation.log)
+  echo 'Initialising mampf master container'
+  echo 'running: bundle exec rails db:migrate'
+  bundle exec rails db:migrate
+  echo 'running: bundle exec rake sunspot:solr:reindex'
+  bundle exec rake sunspot:solr:reindex
+  echo 'copying public assets to volume'
+  rsync -av --delete public/ /public/
+  echo 'finished initialisation'
   touch completed_initial_run
 fi
 echo "running mampf master"
