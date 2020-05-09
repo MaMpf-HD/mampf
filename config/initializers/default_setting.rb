33a2ae0af4ed336a9e5cdc08abc680b6c796b9dd
fosterfarrell9 <28628554+fosterfarrell9@users.noreply.github.com>
2020-05-09 12:53:42 +0200

    add button for sidekiq, update infos on main page

--- config/initializers/default_setting.rb
+++ config/initializers/default_setting.rb
@@ -6,4 +6,5 @@ class DefaultSetting
   URL_HOST_SHORT = ENV['URL_HOST_SHORT']
   RESEARCHGATE_LINK = 'https://www.researchgate.net/project/MaMpf-Mathematische-Medienplattform'
   TOUR_LINK = 'https://mampf.blog/ueber-mampf/'
+  RESOURCES_LINK = 'https://mampf.blog/ressourcen-fur-editorinnen/'
 end
