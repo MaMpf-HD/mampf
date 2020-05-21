1430f4985ec28e46bc6f531ae861c7b92af26c9a
fosterfarrell9 <28628554+fosterfarrell9@users.noreply.github.com>
2020-05-21 16:16:05 +0200

    hotfix migrations that failed in production

--- db/migrate/20200521120400_add_position_to_section_tag_join.rb
+++ db/migrate/20200521120400_add_position_to_section_tag_join.rb
@@ -1,8 +1,8 @@
 class AddPositionToSectionTagJoin < ActiveRecord::Migration[6.0]
   def change
-    add_column :section_tag_joins, :position, :integer
+    add_column :section_tag_joins, :tag_position, :integer
     SectionTagJoin.all.each do |st|
-      st.update_column :position, st.section&.tags_order&.index(st.tag_id)
+      st.update_column :tag_position, st.section&.tags_order&.index(st.tag_id)
     end
   end
 end
--- db/migrate/20200521125454_rename_position_column_in_section_tag_join.rb
+++ db/migrate/20200521125454_rename_position_column_in_section_tag_join.rb
@@ -1,5 +1,4 @@
 class RenamePositionColumnInSectionTagJoin < ActiveRecord::Migration[6.0]
   def change
-		rename_column :section_tag_joins, :position, :tag_position
   end
 end
