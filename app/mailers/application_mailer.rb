c15d991c5ab4272148c3f7a2b584d16d38cb0268
Christian Heusel <christian@heusel.eu>
2020-07-24 00:10:12 +0200

    styled the greeting mail
    Signed-off-by: Christian Heusel <christian@heusel.eu>

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
+  add_template_helper(EmailHelper)
   default from: DefaultSetting::PROJECT_EMAIL
   default "Message-ID" => -> {"<#{rand.to_s.split('.')[1]}.#{Time.now.to_i}@#{ENV['MAILID_DOMAIN']}>" }
   layout 'mailer'
