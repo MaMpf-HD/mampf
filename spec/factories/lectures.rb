116ea962c4469ea859da736bc73317fa377470db
Henrik Reinstädtler <reinstaedtler@stud.uni-heidelberg.de>
2020-09-17 08:33:57 +0200

    Fixes #80
    Validation for sort and correct factory for lectures

--- spec/factories/lectures.rb
+++ spec/factories/lectures.rb
@@ -1,7 +1,13 @@
 FactoryBot.define do
   factory :lecture do
     association :course, factory: [:course, :with_tags]
-    association :teacher
+    association :teacher, factory: :user
+    after(:build) do |user, _evaluator|
+      user.editors << build(:user)
+    end
+    content_mode {['video','manuscript'].sample()}
+    sort {['lecture', 'seminar', 'oberseminar',
+      'proseminar', 'special'].sample }
     association :term
     trait :with_disabled_tags do
       after(:build) { |l| l.disabled_tags = l.course.tags.sample(2) }
