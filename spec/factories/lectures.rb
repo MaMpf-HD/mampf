93d41ef4a63c4e9893705d2fe3cf20c68841de36
Henrik Reinstädtler <reinstaedtler@stud.uni-heidelberg.de>
2020-09-17 09:34:32 +0200

    Adding three working test cases
    1. Create Lecture
    2. Create Module
    3. Subscribe to Module

--- spec/factories/lectures.rb
+++ spec/factories/lectures.rb
@@ -2,15 +2,21 @@ FactoryBot.define do
   factory :lecture do
     association :course, factory: [:course, :with_tags]
     association :teacher, factory: :user
+    released { ['all', 'users', 'subscribers', nil].sample }
     after(:build) do |user, _evaluator|
       user.editors << build(:user)
     end
-    content_mode {['video','manuscript'].sample()}
-    sort {['lecture', 'seminar', 'oberseminar',
-      'proseminar', 'special'].sample }
+    content_mode { ['video', 'manuscript'].sample }
+    sort do
+      ['lecture', 'seminar', 'oberseminar',
+       'proseminar', 'special'].sample
+    end
     association :term
     trait :with_disabled_tags do
       after(:build) { |l| l.disabled_tags = l.course.tags.sample(2) }
+    end
+    trait :published_to_all do
+      released {'all'}
     end
     trait :with_additional_tags do
       after(:build) { |l| l.additional_tags = create_list(:tag, 2) }
