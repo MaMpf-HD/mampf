d3b01ca6a9594fe63c334adaf0f78dd65addd398
Tobias Wackenhut <tobias@wackenhut.at>
2020-09-29 02:11:23 +0200

    Fix docker-development web assembly

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,6 +21,7 @@ then
     touch completed_initial_run
 fi
 rm -f tmp/pids/server.pid
+cp /pdfcomprezzor.wasm public/pdfcomprezzor/pdfcomprezzor.wasm
 echo "running mampf"
 bundle exec sidekiq &
 exec bundle exec rails s -p 3000 -b '0.0.0.0' &> >(tee -a /usr/src/app/log/runtime.log)
