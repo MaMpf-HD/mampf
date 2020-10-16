60e6a006298939f0b41c4810d910a19c68733e7a
fosterfarrell9 <28628554+fosterfarrell9@users.noreply.github.com>
2020-10-16 14:02:38 +0200

    add down method for archived flag migration

--- db/migrate/20201015154231_add_archived_to_user.rb
+++ db/migrate/20201015154231_add_archived_to_user.rb
@@ -1,5 +1,9 @@
 class AddArchivedToUser < ActiveRecord::Migration[6.0]
-  def change
+  def up
     add_column :users, :archived, :boolean
+  end
+
+  def down
+    remove_column :users, :archived, :boolean
   end
 end
