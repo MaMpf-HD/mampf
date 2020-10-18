29a479338bca21385ae7bd354b5e4ab424ba2cff
fosterfarrell9 <28628554+fosterfarrell9@users.noreply.github.com>
2020-10-18 15:53:19 +0200

    revert locals settings (this thime the correct ones)

--- config/environments/docker_development.rb
+++ config/environments/docker_development.rb
@@ -5,13 +5,13 @@ Rails.application.configure do
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
-  config.web_console.development_only = true
+  config.web_console.development_only = false
 
   # Do not eager load code on boot.
   config.eager_load = false
 
   # Show full error reports.
-  config.consider_all_requests_local = false
+  config.consider_all_requests_local = true
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
