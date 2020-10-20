76713af12b238e247ff0e7f124c147b3514797c7
fosterfarrell9 <28628554+fosterfarrell9@users.noreply.github.com>
2020-10-20 17:15:44 +0200

    add more explanations and media info

--- app/helpers/assignments_helper.rb
+++ app/helpers/assignments_helper.rb
@@ -4,4 +4,12 @@ module AssignmentsHelper
     return cancel_edit_assignment_path(assignment) if assignment.persisted?
     cancel_new_assignment_path(params: { lecture: assignment.lecture })
   end
+
+  def has_documents?(assignment)
+    return false unless assignment.medium
+    assignment.medium.video || assignment.medium.manuscript ||
+      assignment.medium.geogebra ||
+      assignment.medium.external_reference_link.present? ||
+      (assignment.medium.sort == 'Quiz' && assignment.medium.quiz_graph)
+    end
 end
