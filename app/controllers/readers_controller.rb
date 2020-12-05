e530b9dc3a5ed022db0541e8acd47f2c31a3fd5c
fosterfarrell9 <28628554+fosterfarrell9@users.noreply.github.com>
2020-12-05 16:04:30 +0100

    add alert after clearing comments

--- app/controllers/readers_controller.rb
+++ app/controllers/readers_controller.rb
@@ -3,10 +3,10 @@ class ReadersController < ApplicationController
 
   def update
     @thread = Commontator::Thread.find_by_id(reader_params[:thread_id])
-	return unless @thread
-	@reader = Reader.find_or_create_by(user: current_user,
-									   thread: @thread)
-	@reader.touch
+    return unless @thread
+    @reader = Reader.find_or_create_by(user: current_user,
+                                       thread: @thread)
+    @reader.touch
     @anything_left = current_user.media_latest_comments.any? do |m|
       (Reader.find_by(user: current_user, thread: m[:thread])
             &.updated_at || (Time.now - 1000.years)) < m[:latest_comment].created_at
@@ -16,16 +16,16 @@ class ReadersController < ApplicationController
 
   def update_all
     threads = current_user.subscribed_commentable_media_with_comments
-    											.map(&:commontator_thread)
+                          .map(&:commontator_thread)
     existing_readers = Reader.where(user: current_user, thread: threads)
     missing_thread_ids = threads.map(&:id) - existing_readers.pluck(:thread_id)
     new_readers = []
     missing_thread_ids.each do |t|
-    	new_readers << Reader.new(thread_id: t, user: current_user)
+      new_readers << Reader.new(thread_id: t, user: current_user)
     end
     Reader.import new_readers
     Reader.where(user: current_user, thread: threads)
-    			.update_all(updated_at: Time.now)
+          .update_all(updated_at: Time.now)
     current_user.update(unread_comments: false)
   end
 
