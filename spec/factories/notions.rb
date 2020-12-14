127afdf6843147e9c45bbf7b81ea831084660218
fosterfarrell9 <28628554+fosterfarrell9@users.noreply.github.com>
2020-12-14 10:31:36 +0100

    repair notions factory

--- spec/factories/notions.rb
+++ spec/factories/notions.rb
@@ -2,11 +2,17 @@ require 'faker'
 
 FactoryBot.define do
   factory :notion, aliases: [:related_notion] do
+    # to get a valid notion, a tag has to be added
+    # in order to avoid loops in the creation of tags (which need a notion)
+    # the adding of a tag is done in the valid_notion factory
     locale { ['de', 'en'].sample }
     title { Faker::Book.title + ' ' +
             Faker::Number.between(from: 1, to: 9999).to_s }
+
     trait :with_tag do
       after(:build) { |t| t.tag = build(:tag) }
     end
+
+    factory :valid_notion, traits: [:with_tag]
   end
 end
