6e10d46065ff3e77e898d53671223ccaa734ce3e
fosterfarrell9 <28628554+fosterfarrell9@users.noreply.github.com>
2020-12-14 14:23:09 +0100

    repair terms factory

--- spec/factories/terms.rb
+++ spec/factories/terms.rb
@@ -4,6 +4,7 @@ FactoryBot.define do
   factory :term do
     season { ['WS', 'SS'].sample }
     year { Faker::Number.between(from: 2000, to: 100000) }
+
     trait :summer do
       season  {'SS'}
     end
