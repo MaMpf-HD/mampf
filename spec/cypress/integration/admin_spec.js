0ea184bf1aa7c00abbba09a396e35e939f6b5b2b
Henrik Reinstädtler <reinstaedtler@stud.uni-heidelberg.de>
2021-01-10 17:15:37 +0100

    Clicker admin tests and Added docker-compose for url_short

--- spec/cypress/integration/admin_spec.js
+++ spec/cypress/integration/admin_spec.js
@@ -15,3 +15,33 @@ describe("Authentication", function () {
         });
     });
 });
+
+describe("Clicker Admin", function () {
+    beforeEach(() => {
+        cy.app("clean");
+        cy.appScenario("admin");
+        cy.visit("/users/sign_in");
+        cy.get('input[type="email"]').type("administrator@mampf.edu");
+        cy.get('input[type="password"]').type("test123456");
+        cy.get('input[type="submit"]').click();
+    });
+
+    it("can create clicker", () => {
+        cy.visit("/administration");
+        cy.get('a[title="Clicker anlegen"]').click();
+        cy.get('input[name="clicker[title]"]').type("ErsterClicker");
+        cy.get("div#new-clicker-area").contains("Speichern").click();
+        cy.contains("ErsterClicker").should("exist");
+    });
+
+    it("can show clicker qr", () => {
+        cy.appFactories([
+            ['create', 'clicker', 'with_editor']
+        ]).then((clickers) => {
+            cy.visit(`/clickers/${clickers[0].id}/edit`);
+            cy.contains("QR-Code zeigen").click();
+            cy.get("li#clickerQRCode").should("exist");
+        });
+
+    });
+});
