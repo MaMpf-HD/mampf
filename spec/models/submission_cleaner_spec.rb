b5554975ad702f57947254dafee065ad80c73221
fosterfarrell9 <28628554+fosterfarrell9@users.noreply.github.com>
2021-03-22 13:52:14 +0100

    fix tests for submission cleaner

--- spec/models/submission_cleaner_spec.rb
+++ spec/models/submission_cleaner_spec.rb
@@ -134,6 +134,54 @@ RSpec.describe SubmissionCleaner, type: :model do
     end
   end
 
+  describe '#clean!' do
+    after :each do
+      @term.destroy
+    end
+
+    it 'does nothing if the date is not right' do
+      @term = FactoryBot.create(:term, year: 2020, season: 'WS')
+      cleaner = FactoryBot.build(:submission_cleaner,
+                                 date: Date.parse('28.3.2021'))
+      cleaner.clean!
+      @term.reload
+      expect(@term.submission_deletion_mail).to be_nil
+    end
+
+    it 'updates submission_deletion_mail column on first day of next term' do
+      @term = FactoryBot.create(:term, year: 2020, season: 'WS')
+      cleaner = FactoryBot.build(:submission_cleaner,
+                                 date: Date.parse('1.4.2021'))
+      cleaner.clean!
+      @term.reload
+      expect(@term.submission_deletion_mail).not_to be_nil
+    end
+
+    it 'updates submission_deletion_reminder column one week later' do
+      @term = FactoryBot.create(:term, year: 2020, season: 'WS',
+                                submission_deletion_mail:
+                                  DateTime.parse('1.4.2021 3:00'))
+      cleaner = FactoryBot.build(:submission_cleaner,
+                                 date: Date.parse('8.4.2021'))
+      cleaner.clean!
+      @term.reload
+      expect(@term.submission_deletion_reminder).not_to be_nil
+    end
+
+    it 'updates submissions_deleted_at column two weeks later' do
+      @term = FactoryBot.create(:term, year: 2020, season: 'WS',
+                                submission_deletion_mail:
+                                  DateTime.parse('1.4.2021 3:00'),
+                                submission_deletion_reminder:
+                                  DateTime.parse('8.4.2021 3:00'))
+      cleaner = FactoryBot.build(:submission_cleaner,
+                                 date: Date.parse('15.4.2021'))
+      cleaner.clean!
+      @term.reload
+      expect(@term.submissions_deleted_at).not_to be_nil
+    end
+  end
+
   context 'with sample submissions' do
 
     before :all do
@@ -163,17 +211,17 @@ RSpec.describe SubmissionCleaner, type: :model do
                                       tutorial: tutorial,
                                       assignment: assignment2)
       @submission3.users << @user4
-      term2 = FactoryBot.create(:term, year: 2021, season: 'SS')
-      lecture2 = FactoryBot.create(:lecture, term: term2)
+      @term2 = FactoryBot.create(:term, year: 2021, season: 'SS')
+      lecture2 = FactoryBot.create(:lecture, term: @term2)
       tutorial2 = FactoryBot.create(:tutorial, :with_tutors, lecture: lecture2)
       assignment21 = FactoryBot.create(:assignment,
                                        lecture: lecture2,
                                        deadline: Date.parse('15.5.2021'))
-      submission21 = FactoryBot.create(:submission,
+      @submission21 = FactoryBot.create(:submission,
                                        tutorial: tutorial2,
                                        assignment: assignment21)
       user5 = FactoryBot.create(:confirmed_user)
-      submission21.users << [@user1, user5]
+      @submission21.users << [@user1, user5]
       lecture3 = FactoryBot.create(:lecture, term: @term)
     end
 
@@ -203,33 +251,7 @@ RSpec.describe SubmissionCleaner, type: :model do
     end
 
     describe '#clean!' do
-      it 'does nothing if the date is not right' do
-        cleaner = FactoryBot.build(:submission_cleaner,
-                                   date: Date.parse('28.3.2021'))
-        cleaner.clean!
-        @term.reload
-        expect(@term.submission_deletion_mail).to be_nil
-      end
-
-      it 'does sends deletion info mail on first day of next term' do
-        cleaner = FactoryBot.build(:submission_cleaner,
-                                   date: Date.parse('1.4.2021'))
-        cleaner.clean!
-        @term.reload
-        expect(@term.submission_deletion_mail).not_to be_nil
-      end
-
-      it 'does sends deletion reminder mail one week later' do
-        @term.submission_deletion_mail = DateTime.parse('1.4.2021 3:00')
-        @term.save
-        cleaner = FactoryBot.build(:submission_cleaner,
-                                   date: Date.parse('8.4.2021'))
-        cleaner.clean!
-        @term.reload
-        expect(@term.submission_deletion_reminder).not_to be_nil
-      end
-
-      it 'destroys submissions and sends an email two weeks later (part 1)' do
+      it 'destroys submissions of the relevant term two weeks later' do
         @term.submission_deletion_mail = DateTime.parse('1.4.2021 3:00')
         @term.submission_deletion_reminder = DateTime.parse('8.4.2021 3:00')
         @term.save
@@ -237,12 +259,8 @@ RSpec.describe SubmissionCleaner, type: :model do
                                    date: Date.parse('15.4.2021'))
         cleaner.clean!
         @term.reload
-        expect(@term.submissions_deleted_at).not_to be_nil
-      end
-
-      it 'destroys submissions and sends an email two weeks later (part 2)' do
-        # test if submission1 - submission 3 were indeed destroyed
-        # (and submission21 is still alive)
+        expect([@term.submissions.to_a, @term2.submissions.to_a])
+          .to eq([[], [@submission21]])
       end
     end
   end
