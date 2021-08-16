51329c12b5395da7ffb841ace874a68413ed0271
Henrik Reinstädtler <reinstaedtler@stud.uni-heidelberg.de>
2021-08-16 11:35:04 +0200

    Upgrade cypress and first mail test

--- spec/cypress/integration/admin_spec.js
+++ spec/cypress/integration/admin_spec.js
@@ -1,3 +1,5 @@
+import { recurse } from 'cypress-recurse'
+
 describe("Authentication", function () {
     beforeEach(() => {
         cy.app("clean");
@@ -27,6 +29,31 @@ describe("Authentication", function () {
             cy.contains("Speichern").click();
             cy.contains("image.png").should("exist");
         });
+    });
+    describe("Simple user",()=>{
+        it("can sign up",()=>{
+            // by now the SMTP server has probably received the email
+            cy.visit('/users/sign_up');
+            cy.get('input[type="email"]').type("joe@mampf.edu");
+            cy.get("input[name='user[password]']").type("sup3r_s3cr3t");
+            cy.get("input[name='user[password_confirmation]']").type("sup3r_s3cr3t");
+            cy.get("#dsgvo-consent").check();
+            cy.wait(100);
+            cy.get("form").contains("Registrieren").click();
+            cy.wait(3000);
+            recurse(() => cy.task('getLastEmail', 'joe@mampf.edu'), Cypress._.isObject, {
+                log: false,
+                delay: 1000,
+                timeout: 20000,
+              })
+            .its('html') // check the HTML email text
+            // what do we do now?
+            .then((html) => {
+            cy.document().invoke('write', html)
+            cy.contains("Account bestätigen").click();
+            cy.contains("Account").should("exist");
+            })
+        })
     });
 });
 
--- spec/cypress/plugins/index.js
+++ spec/cypress/plugins/index.js
@@ -12,10 +12,43 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
+// cypress/plugins/index.js
+/// <reference types="cypress" />
+const ms = require('smtp-tester')
+
 /**
  * @type {Cypress.PluginConfig}
  */
 module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
-};
+  // starts the SMTP server at localhost:7777
+  const port = 1025
+  const mailServer = ms.init(port)
+  console.log('mail server at port %d', port)
+  let lastEmail = {}
+  // process all emails
+  mailServer.bind((addr, id, email) => {
+    console.log('--- email ---')
+    //console.log(addr, id, email)
+    lastEmail[email.headers.to] = email
+  })
+  on('task', {
+    resetEmails(email) {
+      console.log('reset all emails')
+      if (email) {
+        delete lastEmail[email]
+      } else {
+        lastEmail = {}
+      }
+      return null
+    },
+
+    getLastEmail(email) {
+      // cy.task cannot return undefined
+      // thus we return null as a fallback
+      console.log(email)
+      console.log(Object.keys(lastEmail))
+      //console.log(lastEmail[email])
+      return lastEmail[email] || null
+    },
+  })
+}
