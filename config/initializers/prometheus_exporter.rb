63f6c6c327de07e0295ab0ad348cf2d882087988
Henrik Reinstädtler <reinstaedtler@stud.uni-heidelberg.de>
2021-08-27 11:46:22 +0200

    monitoring more infos like GC

--- config/initializers/prometheus_exporter.rb
+++ config/initializers/prometheus_exporter.rb
@@ -1,7 +1,15 @@
 # in config/initializers/prometheus.rb
 if Rails.env != "test"
-    require 'prometheus_exporter/middleware'
+  require 'prometheus_exporter/instrumentation'
+  require 'prometheus_exporter/middleware'
   
     # This reports stats per request like HTTP status and timings
-    Rails.application.middleware.unshift PrometheusExporter::Middleware
+  Rails.application.middleware.unshift PrometheusExporter::Middleware
+    # in config/initializers/prometheus.rb
+  
+
+  # this reports basic process stats like RSS and GC info, type master
+  # means it is instrumenting the master process
+  PrometheusExporter::Instrumentation::Process.start(type: "master")
+
   end
--- config/puma.rb
+++ config/puma.rb
@@ -31,6 +31,17 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 # processes).
 #
 # workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+on_worker_boot do
+    require 'prometheus_exporter/instrumentation'
+    PrometheusExporter::Instrumentation::ActiveRecord.start(
+      custom_labels: { type: "puma_worker" }, #optional params
+      config_labels: [:database, :host] #optional params
+    )
+end
+after_worker_boot do
+    require 'prometheus_exporter/instrumentation'
+    PrometheusExporter::Instrumentation::Puma.start
+end
 
 # Use the `preload_app!` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code
