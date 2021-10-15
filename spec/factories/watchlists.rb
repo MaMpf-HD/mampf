e5bc3b545a5b5822c76d07e4c76f47d9750435f1
Fabian Kontor <fabiankontor@aol.com>
2021-10-15 23:21:57 +0200

    added watchlist tests

--- spec/factories/watchlists.rb
+++ spec/factories/watchlists.rb
@@ -2,5 +2,9 @@ FactoryBot.define do
   factory :watchlist do
     user { nil }
     name { Faker::Movie.title }
+
+    trait :with_user do
+      user { create(:confirmed_user) }
+    end
   end
 end
