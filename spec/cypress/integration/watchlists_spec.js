4afba7904b0816b98b68ed665007f9f357954fcd
Fabian Kontor <fabiankontor@aol.com>
2021-10-27 13:30:04 +0200

    removed footer from layout with sidebar (tutorial tests didnt work with that) added test for adding watchlists in show view

--- spec/cypress/integration/watchlists_spec.js
+++ spec/cypress/integration/watchlists_spec.js
@@ -12,6 +12,64 @@ describe("Watchlists", () => {
             cy.get('input[type="password"]').type("test123456");
             cy.get('input[type="submit"]').click();
         });
+        it("can create and add to watchlist in lecture", () => {
+			cy.appFactories([
+                ["create", "lecture_medium", "with_manuscript", "released"],
+                ["create",
+                    "lecture_user_join", {
+                        user_id: 1,
+                        lecture_id: 1
+                    }
+                ]
+			]).then((data) => {
+				cy.visit(`lectures/${data[0].id}`);
+				cy.get('.nav > :nth-child(6) > .nav-link').click();
+				cy.get('div.text-light > .fa-list').click();
+				cy.get('#openNewWatchlistForm').click();
+				cy.get('#watchlistNameField').type('Lernliste');
+				cy.get('#createWatchlistBtn').click();
+				cy.get('#watchlistEntrySubmitButton').click();
+                cy.get('div.text-light > .fa-list').click();
+                cy.get('#watchlistEntrySubmitButton').click();
+                cy.get('.invalid-feedback').should('exist');
+                cy.wait(200);
+                cy.get('.close > span').click();
+                cy.wait(100);
+                cy.get('#watchlistsIcon').click();
+                cy.get('#card-title').should('exist');
+			});
+        });
+        it("can create and add to watchlist in watchlist view", () => {
+			cy.appFactories([
+			]).then((data) => {
+				cy.get('#watchlistsIcon').click();
+                cy.get('#openNewWatchlistForm').click();
+                cy.get('#watchlistNameField').type('Lernliste');
+                cy.get('#newWatchlistButton').click();
+                cy.get('#watchlistButton').contains('Lernliste');
+			});
+        });
+        it("can use bookmark", () => {
+			cy.appFactories([
+                ["create", "watchlist", {
+                    user_id: 1
+                }],
+                ["create", "watchlist_entry", "with_medium", {
+                    watchlist_id: 1
+                }],
+                ["create",
+                    "lecture_user_join", {
+                        user_id: 1,
+                        lecture_id: 1
+                    }
+                ]
+			]).then((data) => {
+				cy.visit('lectures/1');
+				cy.get('.nav > :nth-child(6) > .nav-link').click();
+                cy.get('.fa-bookmark').click();
+                cy.get('#watchlistButton').contains(`${data[0].name}`).should('exist');
+			});
+        });
         it("can change visibility of watchlist", () => {
             cy.appFactories([
                 ["create", "watchlist", {
@@ -45,55 +103,6 @@ describe("Watchlists", () => {
                 cy.get(':nth-child(3) > .row > .col-12 > :nth-child(2)').contains('Watchlist').should('exist');
             });
         });
-        it("can create and add to watchlist", () => {
-			cy.appFactories([
-                ["create", "lecture_medium", "with_manuscript", "released"],
-                ["create",
-                    "lecture_user_join", {
-                        user_id: 1,
-                        lecture_id: 1
-                    }
-                ]
-			]).then((data) => {
-				cy.visit(`lectures/${data[0].id}`);
-				cy.get('.nav > :nth-child(6) > .nav-link').click();
-				cy.get('div.text-light > .fa-list').click();
-				cy.get('#openNewWatchlistForm').click();
-				cy.get('#watchlistNameField').type('Lernliste');
-				cy.get('#createWatchlistBtn').click();
-				cy.get('#watchlistEntrySubmitButton').click();
-                cy.wait(100);
-                cy.get('div.text-light > .fa-list').click();
-                cy.get('#watchlistEntrySubmitButton').click();
-                cy.get('.invalid-feedback').should('exist');
-                cy.wait(200);
-                cy.get('.close > span').click();
-                cy.wait(100);
-                cy.get('#watchlistsIcon').click();
-                cy.get('#card-title').should('exist');
-			});
-        });
-        it("can use bookmark", () => {
-			cy.appFactories([
-                ["create", "watchlist", {
-                    user_id: 1
-                }],
-                ["create", "watchlist_entry", "with_medium", {
-                    watchlist_id: 1
-                }],
-                ["create",
-                    "lecture_user_join", {
-                        user_id: 1,
-                        lecture_id: 1
-                    }
-                ]
-			]).then((data) => {
-				cy.visit('lectures/1');
-				cy.get('.nav > :nth-child(6) > .nav-link').click();
-                cy.get('.fa-bookmark').click();
-                cy.get('#watchlistButton').contains(`${data[0].name}`).should('exist');
-			});
-        });
         it("can filter watchlist_entries", () => {
             cy.appFactories([
                 ["create", "watchlist", {
@@ -111,6 +120,7 @@ describe("Watchlists", () => {
                 cy.wait(100);
                 cy.get('#allButton').click();
                 cy.get('#watchlistButton').should('exist');
+                cy.get('.active > .page-link').should('not.exist');
             });
         });
         it("can drag and drop watchlist_entry", () => {
