601a76fdc9d518618456fa30bc5c21a726ee2411
Fabian Kontor <fabiankontor@aol.com>
2021-12-09 17:47:17 +0100

    ran bundle update replaced deprecated zeitwerk method

--- config/initializers/preload_medium_sti.rb
+++ config/initializers/preload_medium_sti.rb
@@ -5,5 +5,5 @@ autoloader = Rails.autoloaders.main
 sti_leaves = %w(question quiz remark)
 
 sti_leaves.each do |leaf|
-  autoloader.preload("#{Rails.root}/app/models/#{leaf}.rb")
+  autoloader.on_setup { "#{leaf}" }
 end
