8dad8e9e987a8433971a9a526f53af48f1bb8c0d
Fabian Kontor <fabiankontor@aol.com>
2021-12-10 19:12:28 +0100

    small refactorizations

--- app/controllers/watchlists_controller.rb
+++ app/controllers/watchlists_controller.rb
@@ -1,6 +1,6 @@
 # WatchlistsController
 class WatchlistsController < ApplicationController
-  before_action :sanitize_params, only: [:show, :update_order, :change_visibility]
+  before_action :sanitize_params, only: %i[show update_order change_visibility]
   layout 'application_no_sidebar'
 
   def create
@@ -8,7 +8,7 @@ class WatchlistsController < ApplicationController
     @watchlist.name = params[:watchlist][:name]
     @watchlist.user = current_user
     @watchlist.description = params[:watchlist][:description]
-    @medium = Medium.find_by_id(params[:watchlist][:medium_id])
+    @medium = Medium.find_by(id: params[:watchlist][:medium_id])
     @success = @watchlist.save
     if @medium.blank? && @success
       flash[:notice] = I18n.t('watchlist.creation_success')
@@ -19,11 +19,9 @@ class WatchlistsController < ApplicationController
   end
 
   def update
-    @watchlist = Watchlist.find_by_id(params[:id])
+    @watchlist = Watchlist.find_by(id: params[:id])
     @success = @watchlist.update(params.require(:watchlist).permit(:name, :description))
-    if @success
-      flash[:notice] = I18n.t('watchlist.change_success')
-    end
+    flash[:notice] = I18n.t('watchlist.change_success') if @success
     respond_to do |format|
       format.js
     end
@@ -46,7 +44,7 @@ class WatchlistsController < ApplicationController
   def show
     @watchlists = current_user.watchlists
     if params[:id]
-      @watchlist = Watchlist.find_by_id(params[:id])
+      @watchlist = Watchlist.find_by(id: params[:id])
     # if user calls 'watchlists/show' without watchlist id
     elsif !@watchlists.empty?
       redirect_to watchlist_path(@watchlists.first)
@@ -69,10 +67,10 @@ class WatchlistsController < ApplicationController
       redirect_to :root, alert: I18n.t('controllers.no_watchlist')
       return
     end
-    if !@watchlists.empty? && !@watchlist.watchlist_entries.empty?
-      @watchlist_entries = paginated_results
-      @media = @watchlist_entries.pluck(:medium_id)
-    end
+    return unless !@watchlists.empty? && !@watchlist.watchlist_entries.empty?
+
+    @watchlist_entries = paginated_results
+    @media = @watchlist_entries.pluck(:medium_id)
   end
 
   def sanitize_params
@@ -95,12 +93,13 @@ class WatchlistsController < ApplicationController
   def filter_results
     filter_results = @watchlist.watchlist_entries
     return filter_results unless params[:reverse]
+
     filter_results.reverse
   end
 
   def add_to_watchlist
     @watchlists = current_user.watchlists
-    @medium = Medium.find_by_id(params[:id])
+    @medium = Medium.find_by(id: params[:id])
     render 'watchlists/show_add_modal'
   end
 
@@ -109,15 +108,19 @@ class WatchlistsController < ApplicationController
   end
 
   def change_watchlist
-    @watchlist = Watchlist.find_by_id(params[:id])
+    @watchlist = Watchlist.find_by(id: params[:id])
     render 'watchlists/show_change_modal'
   end
 
   def update_order
     if params[:reverse]
-      params[:order].reverse.each_with_index { |id, index| WatchlistEntry.update(id, medium_position: index) }
+      params[:order].reverse.each_with_index do |id, index|
+        WatchlistEntry.update(id, medium_position: index)
+      end
     else
-      params[:order].each_with_index { |id, index| WatchlistEntry.update(id, medium_position: index) }
+      params[:order].each_with_index do |id, index|
+        WatchlistEntry.update(id, medium_position: index)
+      end
     end
   end
 
@@ -126,6 +129,6 @@ class WatchlistsController < ApplicationController
   end
 
   def check_ownership
-    Watchlist.find_by_id(parmas[:id]).ownedBy(current_user)
+    Watchlist.find_by(id: parmas[:id]).ownedBy(current_user)
   end
 end
