c14c2dedf6755c4ee4370f886ed9c0d84441b85c
Tobias Wackenhut <tobias@wackenhut.at>
2022-03-04 13:54:30 +0100

    Fix metrics locations and simplify nginx.tmpl further

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,5 +8,5 @@ rm -f tmp/pids/server.pid
 cp /pdfcomprezzor.wasm public/pdfcomprezzor/pdfcomprezzor.wasm
 echo "running mampf"
 bundle exec sidekiq &
-prometheus_exporter -b 0.0.0.0 -p 9394 -a lib/collectors/mampf_collector.rb &> /usr/src/app/log/prometheus_exporter.log &!
+prometheus_exporter --label "{\"instance\": \"${HOSTNAME}\"}" -b 0.0.0.0 -p 9394 -a lib/collectors/mampf_collector.rb &!
 exec bundle exec rails s -p 3000 -b '0.0.0.0' &> >(tee -a /usr/src/app/log/runtime.log)
