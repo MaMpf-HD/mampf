38e4c7c929469483b4345b4bf7217137567e4cea
Tobias Wackenhut <tobias@wackenhut.at>
2022-03-06 16:36:23 +0100

    Add master container to monitoring

--- docker/production/entrypoint-worker.sh
+++ docker/production/entrypoint-worker.sh
@@ -3,5 +3,5 @@ cd /usr/src/app
 echo "Ensuring no stale server pid file is present"
 rm -f tmp/pids/server.pid
 echo "running mampf"
-prometheus_exporter --label "{\"instance\": \"${HOSTNAME}\"}" -b 0.0.0.0 -p 9394 -a lib/collectors/mampf_collector.rb &> /usr/src/app/log/prometheus_exporter.log &!
+prometheus_exporter --label "{\"container\": \"${HOSTNAME}\"}" -b 0.0.0.0 -p 9394 -a lib/collectors/mampf_collector.rb &> /usr/src/app/log/prometheus_exporter.log &!
 exec bundle exec rails s -p 3000 -b '0.0.0.0'
--- entrypoint.sh
+++ entrypoint.sh
@@ -8,5 +8,5 @@ rm -f tmp/pids/server.pid
 cp /pdfcomprezzor.wasm public/pdfcomprezzor/pdfcomprezzor.wasm
 echo "running mampf"
 bundle exec sidekiq &
-prometheus_exporter --label "{\"instance\": \"${HOSTNAME}\"}" -b 0.0.0.0 -p 9394 -a lib/collectors/mampf_collector.rb &!
+prometheus_exporter --label "{\"container\": \"${HOSTNAME}\"}" -b 0.0.0.0 -p 9394 -a lib/collectors/mampf_collector.rb &!
 exec bundle exec rails s -p 3000 -b '0.0.0.0' &> >(tee -a /usr/src/app/log/runtime.log)
