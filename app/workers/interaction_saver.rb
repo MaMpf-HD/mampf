d6a1798f1129bc2463f6d21bd4c9d3cfeddd42da
fosterfarrell9 <28628554+fosterfarrell9@users.noreply.github.com>
2022-03-10 17:57:05 +0100

    correct indentation

--- app/workers/interaction_saver.rb
+++ app/workers/interaction_saver.rb
@@ -3,8 +3,8 @@ class InteractionSaver
 
   def perform(session_id, full_path, referrer, study_participant)
     referrer_url = if referrer.to_s.include?(ENV['URL_HOST'])
-                      referrer.to_s.remove(ENV['URL_HOST'])
-                                   .remove('https://').remove('http://')
+                     referrer.to_s.remove(ENV['URL_HOST'])
+                                  .remove('https://').remove('http://')
                    end
     Interaction.create(session_id: Digest::SHA2.hexdigest(session_id).first(10),
                        full_path: full_path,
