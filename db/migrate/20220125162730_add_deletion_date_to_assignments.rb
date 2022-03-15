cd6adcb32587d3b103774eb6361400c0b0e7a383
Fabian Kontor <fabiankontor@aol.com>
2022-03-15 13:04:33 +0100

    made last few migrations reversible

--- db/migrate/20220125162730_add_deletion_date_to_assignments.rb
+++ db/migrate/20220125162730_add_deletion_date_to_assignments.rb
@@ -1,8 +1,13 @@
 class AddDeletionDateToAssignments < ActiveRecord::Migration[6.1]
-  def change
+  def up
     add_column :assignments,
                :deletion_date,
                :date, null: false, default: (Term.active&.end_date || (Date.today + 180.days)) + 15.days
-    remove_column :assignments, :protected, :boolean, :default => false
+    remove_column :assignments, :protected, :boolean
+  end
+
+  def down
+    remove_column :assignments, :deletion_date, :date
+    add_column :assignments, :protected, :boolean, default: false
   end
 end
