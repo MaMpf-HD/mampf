500e8d159a7bf6d4c10ee39c8fb005a18c54ad22
Fabian Kontor <fabiankontor@aol.com>
2022-08-25 23:27:38 +0200

    rework hash mail look

--- app/mailers/mathi_mailer.rb
+++ app/mailers/mathi_mailer.rb
@@ -1,11 +1,10 @@
 class MathiMailer < ApplicationMailer
   default from: DefaultSetting::PROJECT_EMAIL
-  layout false
-
+  
   def ghost_email(user)
     return if user.ghost_hash.nil?
-    @email = user.email
+    @name = user.name
     @hash = user.ghost_hash
-    mail(to: user.email, subject: "Ghost:#{@email}")
+    mail(to: user.email, subject: t('mailer.hash_mail_subject'))
   end
 end
