76c64909b14b2889a996c3d399a9aa920f88b2c1
Fabian Kontor <fabiankontor@aol.com>
2022-09-29 13:46:55 +0200

    upgrade solr from 8.11.1 to 9.0.0 and redis from alpine to 7.0 fixed user cleaner test

--- spec/factories/user_cleaners.rb
+++ spec/factories/user_cleaners.rb
@@ -5,6 +5,7 @@ FactoryBot.define do
       after(:build) do |c|
         user = FactoryBot.create(:confirmed_user, email: Faker::Internet.email)
         user.update(ghost_hash: Digest::SHA256.hexdigest(Time.now.to_i.to_s))
+        c.email_dict = {}
         c.hash_dict = { user.email => user.ghost_hash }
       end
     end
