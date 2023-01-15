3792ed82f0d2ca152a1113abdacb5e86d32d2764
Frodo161 <110454463+Frodo161@users.noreply.github.com>
2023-01-15 13:54:29 +0100

    Update lectures_controller.rb
    If a lecture had no editors the old code would perform actions on a nil object causing an error.
    Now the relevant passage in the update method is surrounded by an if-statement which first checks, if the given array of editor ids is nil and skips the passage in this case.

--- app/controllers/lectures_controller.rb
+++ app/controllers/lectures_controller.rb
@@ -26,20 +26,23 @@ class LecturesController < ApplicationController
   end
 
   def update
-    # removes the empty String "" in the NEW array of editor ids
-    # and converts it into an array of integers
-    all_ids = lecture_params[:editor_ids].map(&:to_i) - [0]
-    old_ids = @lecture.editor_ids
-    new_ids = all_ids - old_ids
+    editor_ids = lecture_params[:editor_ids]
+    if editor_ids != nil
+      # removes the empty String "" in the NEW array of editor ids
+      # and converts it into an array of integers
+      all_ids = editor_ids.map(&:to_i) - [0]
+      old_ids = @lecture.editor_ids
+      new_ids = all_ids - old_ids
 
-    # returns an array of Users that match the given ids
-    recipients = User.where(id: new_ids)
+      # returns an array of Users that match the given ids
+      recipients = User.where(id: new_ids)
 
-    recipients.each do |r|
-      NotificationMailer.with(recipient: r,
-                              locale: r.locale,
-                              lecture: @lecture)
-                        .new_editor_email.deliver_later
+      recipients.each do |r|
+        NotificationMailer.with(recipient: r,
+                                locale: r.locale,
+                                lecture: @lecture)
+                          .new_editor_email.deliver_later
+      end
     end
     
     @lecture.update(lecture_params)
