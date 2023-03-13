e2b3fdf8089e9f97b7d6432b03ae5b30b9956d67
Frodo161 <marlon.kocher@web.de>
2023-03-13 20:38:56 +0100

    Replaced the standard color picker of the annotation form by a selection of 15 colors.

--- app/helpers/annotations_helper.rb
+++ app/helpers/annotations_helper.rb
@@ -28,4 +28,40 @@ module AnnotationsHelper
       return 'Other'
     end
   end
+  
+  def annotation_color(int)
+    case int
+    when 1
+      return '#DB2828'
+    when 2
+      return '#F2711C'
+    when 3
+      return '#FBBD08'
+    when 4
+      return '#B5CC18'
+    when 5
+      return '#21BA45'
+    when 6
+      return '#00B5AD'
+    when 7
+      return '#2185D0'
+    when 8
+      return '#6435C9'
+    when 9
+      return '#A333C8'
+    when 10
+      return '#E03997'
+    when 11
+      return '#d05d41'
+    when 12
+      return '#924129'
+    when 13
+      return '#444444'
+    when 14
+      return '#999999'
+    when 15
+      return '#eeeeee'
+    end
+  end
+  
 end
