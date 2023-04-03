d525217cc960d4aaba41bb0adcaeabeca7e47ba7
Splines <dominic-plein@gmx.de>
2023-04-03 10:55:48 +0200

    Add announcements to landing page
    (right now only for /users/sign_in route)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,13 @@
 class SessionsController < Devise::SessionsController
 
+  def initialize
+    @announcements = Announcement
+      .where(on_main_page: true, lecture: nil)
+      .pluck(:details)
+      .join
+    super
+  end
+
   # remove devise's flash message for succesful sign_in
   def create
     super
