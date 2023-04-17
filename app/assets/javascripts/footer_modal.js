4f35b95b7b12270fdd6c97fefa5a0ceae110fdcb
Splines <dominic-plein@gmx.de>
2023-04-17 10:49:17 +0200

    Remove "#sponsors" slug when closing modal

--- app/assets/javascripts/footer_modal.js
+++ app/assets/javascripts/footer_modal.js
@@ -1,5 +1,9 @@
-$(document).ready(function () {
+$(document).ready(() => {
     if (window.location.hash == "#sponsors") {
         $('#sponsors').modal('show');
     }
+
+    $('#sponsors').on('hide.bs.modal', () => {
+        window.history.pushState(null, null, location.pathname);
+    });
 });
