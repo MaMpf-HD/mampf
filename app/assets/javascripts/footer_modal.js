b43337451b8b73e7629e14dad1f5732da69637e4
Splines <dominic-plein@gmx.de>
2023-04-17 20:55:57 +0200

    Move onclick event to js file

--- app/assets/javascripts/footer_modal.js
+++ app/assets/javascripts/footer_modal.js
@@ -1,7 +1,11 @@
-document.addEventListener("turbolinks:load", function () {
+document.addEventListener("turbolinks:load", () => {
     if (window.location.hash == "#sponsors") {
         $('#sponsors').modal('show');
     }
+
+    $('#sponsorsLink').click(function (event) {
+        window.history.pushState(null, null, '#sponsors');
+    });
 
     $('#sponsors').on('hide.bs.modal', () => {
         window.history.pushState(null, null, location.pathname);
