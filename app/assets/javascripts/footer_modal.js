3b6109af90aeaf147722b5b3aabaaef321fd303a
Splines <dominic-plein@gmx.de>
2023-04-18 22:13:59 +0200

    Don't use browser history for modal

--- app/assets/javascripts/footer_modal.js
+++ app/assets/javascripts/footer_modal.js
@@ -2,12 +2,4 @@ document.addEventListener("turbolinks:load", () => {
     if (window.location.hash == "#sponsors") {
         $('#sponsors').modal('show');
     }
-
-    $('#sponsorsLink').click(function (event) {
-        window.history.pushState(null, null, '#sponsors');
-    });
-
-    $('#sponsors').on('hide.bs.modal', () => {
-        window.history.pushState(null, null, location.pathname);
-    });
 });
