9c6659505ef036c78415dc099edf606fd0ffd668
Splines <dominic-plein@gmx.de>
2023-06-05 00:18:01 +0200

    Add Feedback modal view and corresponding controller
    First working version, of course still a lot to improve from here.

---
+++ app/controllers/feedbacks_controller.rb
@@ -0,0 +1,23 @@
+class FeedbacksController < ApplicationController
+  authorize_resource except: [:create]
+
+  def create
+    feedback = Feedback.new(feedback_params)
+    feedback.user_id = current_user.id
+    successfully_saved = feedback.save
+    flash.now[:status_msg] = if successfully_saved
+      'Feedback successfully sent.'
+    else
+      'Something went wrong.'
+    end
+    @errors = feedback.errors
+    # # redirect_to :root, alert: @errors.full_messages.join(', ')
+    respond_to(&:js)
+  end
+
+  private
+
+    def feedback_params
+      params.require(:feedback).permit(:title, :feedback)
+    end
+end
