75ca523874da6c66ee809a02bb9a8c498be87ddd
Splines <dominic-plein@gmx.de>
2023-07-12 00:31:35 +0200

    Do not use not working jQuery version of datetimepicker

--- app/assets/javascripts/datetimepicker.js
+++ app/assets/javascripts/datetimepicker.js
@@ -1,13 +1,17 @@
 // see https://getdatepicker.com
-$('#assignment-picker').tempusDominus({
-    display: {
-        sideBySide: true
-    },
-    localization: {
-        startOfTheWeek: 1,
-        format: 'yyyy-MM-dd HH:mm',
-        hourCycle: 'h23'
+var datetimePicker = new tempusDominus.TempusDominus(
+    document.getElementById('assignment-picker'),
+    {
+        display: {
+            sideBySide: true,
+        },
+        localization: {
+            startOfTheWeek: 1,
+            format: 'yyyy-MM-dd HH:mm',
+            hourCycle: 'h23',
+        }
     }
+);
 });
 
 $('#assignment-picker-input').on('click', () => {
