4b696891800b14e05acb7d5edc9889efa48b5b33
Splines <dominic-plein@gmx.de>
2023-09-12 21:59:04 +0200

    Get rid of null id field in JSON output
    Also reordered the fields to be prettier.

--- app/controllers/news_popups_controller.rb
+++ app/controllers/news_popups_controller.rb
@@ -1,7 +1,7 @@
 class NewsPopupsController < ApplicationController
   def index
     respond_to do |format|
-      format.json { render json: unseen_news_popups }
+      format.json { render json: unseen_news_popups.to_json(except: :id) }
     end
   end
 
@@ -11,7 +11,7 @@ class NewsPopupsController < ApplicationController
       HasSeenNewsPopup
         .where(user: current_user)
         .left_outer_joins(:news_popup)
-        .select('news_popups.active', 'news_popups.name')
+        .select('news_popups.name', 'news_popups.active')
         .where(news_popups: { active: true })
     end
 end
