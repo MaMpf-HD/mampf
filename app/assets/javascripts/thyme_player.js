af59d7a776b4e03d06b2b86d582b1f111e411ac8
Frodo161 <marlon.kocher@web.de>
2023-09-13 12:44:08 +0200

    Optical and structural improvements of code and fix of updating annotations after submitting the annotation form.

--- app/assets/javascripts/thyme_player.js
+++ app/assets/javascripts/thyme_player.js
@@ -81,7 +81,6 @@ function setupHypervideo() {
   if (video === null) {
     return;
   }
-  document.body.style.backgroundColor = 'black';
   const chaptersElement = $('#video track[kind="chapters"]').get(0);
   const metadataElement = $('#video track[kind="metadata"]').get(0);
 
@@ -330,19 +329,31 @@ function setupHypervideo() {
 };
 
 $(document).on('turbolinks:load', function() {
+  /*
+    VIDEO INITIALIZATION
+   */
+  // exit script if the current page has no thyme player
   const thymeContainer = document.getElementById('thyme-container');
-  // no need for thyme if no thyme container on the page
-  if (thymeContainer === null) {
+  if (thymeContainer === null || $('#video').get(0) === null) {
     return;
   }
-  // Video
+
+  // background color
+  document.body.style.backgroundColor = 'black';
+
+  // initialize attributes
+  const thyme = document.getElementById('thyme');
   const video = document.getElementById('video');
   thymeAttributes.video = video;
-  const thyme = document.getElementById('thyme');
-  // initialize medium id
   thymeAttributes.mediumId = thyme.dataset.medium;
-  // initialize marker bar
   thymeAttributes.markerBarId = 'markers';
+
+
+
+
+  /*
+    COMPONENTS
+   */
   // Buttons
   annotationsToggle = new AnnotationsToggle('annotations-toggle');
   annotationsToggle.add();
@@ -365,21 +376,11 @@ $(document).on('turbolinks:load', function() {
   const iaClose = document.getElementById('ia-close');
   const backButton = document.getElementById('back-button');
 
-  // Manage large and small display
-  function largeDisplayFunc() {
-    video.style.width = '82%';
-    if (iaButton.dataset.status === 'false') {
-      iaButton.innerHTML = 'remove_from_queue';
-      $('#caption').hide();
-      $('#annotation-caption').hide();
-      video.style.width = '100%';
-      $('#video-controlBar').css('width', '100%');
-      $(window).trigger('resize');
-    }
-  }
-  const elements = [$('#caption'), $('#annotation-caption'), $('#video-controlBar')];
-  const displayManager = new DisplayManager(elements, largeDisplayFunc);
 
+
+  /*
+    ANNOTATION FUNCTIONALITY
+   */
   // annotation manager and area
   function colorFunc(annotation) {
     return annotation.color;
@@ -429,6 +430,55 @@ $(document).on('turbolinks:load', function() {
   annotationArea.onShow = onShow;
   annotationArea.onHide = onHide;
 
+  // Update annotations after submitting the annotations form
+  $(document).on('click', '#submit-button', function() {
+    /* NOTE:
+       Updating might take some time on the backend,
+       so I added a slight delay.
+       I couldn't think of an easy way to let the script
+       wait for the update to complete (as with the delete button),
+       but it might be possible! */
+    setTimeout(function() {
+      annotationManager.updateAnnotations();
+    }, 500);
+  });
+
+  // Update annotations after deleting an annotation
+  $(document).on('click', '#delete-button', function() {
+    const annotationId = Number(document.getElementById('annotation_id').textContent);
+    $.ajax(Routes.annotation_path(annotationId), {
+      type: 'DELETE',
+      dataType: 'json',
+      data: {
+        annotationId: annotationId
+      },
+      success: function() {
+        annotationManager.updateAnnotations();
+        $('#annotation-close-button').click();
+      }
+    });
+  });
+
+  
+
+  /*
+    RESIZE
+   */
+  // Manage large and small display
+  function largeDisplayFunc() {
+    video.style.width = '82%';
+    if (iaButton.dataset.status === 'false') {
+      iaButton.innerHTML = 'remove_from_queue';
+      $('#caption').hide();
+      $('#annotation-caption').hide();
+      video.style.width = '100%';
+      $('#video-controlBar').css('width', '100%');
+      $(window).trigger('resize');
+    }
+  }
+  const elements = [$('#caption'), $('#annotation-caption'), $('#video-controlBar')];
+  const displayManager = new DisplayManager(elements, largeDisplayFunc);
+
   // resizes the thyme container to the window dimensions, taking into account
   // whether the interactive area is displayed or hidden
   function resizeContainer() {
@@ -441,6 +491,16 @@ $(document).on('turbolinks:load', function() {
     }
   };
 
+
+
+  /*
+    KEYBOARD SHORTCUTS
+   */
+  thymeKeyShortcuts.addGeneralShortcuts();
+  thymeKeyShortcuts.addPlayerShortcuts();
+
+
+
   // detect IE/edge and inform user that they are not suppported if necessary,
   // only use browser player
   if (document.documentMode || /Edge/.test(navigator.userAgent)) {
@@ -450,7 +510,6 @@ $(document).on('turbolinks:load', function() {
     $('#video-controlBar').hide();
     video.style.width = '100%';
     video.controls = true;
-    document.body.style.backgroundColor = 'black';
     resizeContainer();
     window.onresize = resizeContainer;
     return;
@@ -473,33 +532,6 @@ $(document).on('turbolinks:load', function() {
   video.addEventListener('mouseover', showControlBar, false);
   video.addEventListener('mousemove', showControlBar, false);
   video.addEventListener('touchstart', showControlBar, false);
-
-  // Update annotations after submitting the annotations form
-  $(document).on('click', '#submit-button', function() {
-    /* NOTE:
-       Updating might take some time on the backend,
-       so I added a slight delay.
-       I couldn't think of an easy way to let the script
-       wait for the update to complete (as with the delete button),
-       but it might be possible! */
-    setTimeout(annotationManager.updateAnnotations(), 500);
-  });
-
-  // Update annotations after deleting an annotation
-  $(document).on('click', '#delete-button', function() {
-    const annotationId = Number(document.getElementById('annotation_id').textContent);
-    $.ajax(Routes.annotation_path(annotationId), {
-      type: 'DELETE',
-      dataType: 'json',
-      data: {
-        annotationId: annotationId
-      },
-      success: function() {
-        annotationManager.updateAnnotations();
-        $('#annotation-close-button').click();
-      }
-    });
-  });
 
   // Event handler for interactive area activation button
   iaButton.addEventListener('click', function() {
@@ -542,7 +574,4 @@ $(document).on('turbolinks:load', function() {
     showControlBar();
   });
 
-  // Add keyboard shortcuts from thyme/key.js
-  thymeKeyShortcuts.addGeneralShortcuts();
-  thymeKeyShortcuts.addPlayerShortcuts();
 });
