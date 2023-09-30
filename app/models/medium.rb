99af7440fab184d42c859d43005cdd5f4aa4af72
Frodo161 <marlon.kocher@web.de>
2023-09-30 17:04:03 +0200

    Set default value of visible_for_teacher to false, whenever the annotation_status doesn't equal 1 (before it was only set to false if the annotation_status equalled -1).

--- app/models/medium.rb
+++ app/models/medium.rb
@@ -1127,8 +1127,8 @@ class Medium < ApplicationRecord
   end
   
   def get_annotations_status
-  	return annotations_status if annotations_status != 0
-	lecture.annotations_status
+    return annotations_status if annotations_status != 0
+    lecture.annotations_status
   end
   
   def annotations_visible?(user)
