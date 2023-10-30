326476309b2ae03411f7e3f328f5046ca1212f0e
Splines <dominic-plein@gmx.de>
2023-10-30 16:10:35 +0100

    Validate file size in backend

--- app/uploaders/correction_uploader.rb
+++ app/uploaders/correction_uploader.rb
@@ -6,4 +6,11 @@ class CorrectionUploader < Shrine
   plugin :determine_mime_type, analyzer: :marcel
   plugin :upload_endpoint, max_size: 30 * 1024 * 1024 # 30 MB
   plugin :default_storage, cache: :submission_cache, store: :submission_store
+  plugin :validation_helpers
+
+  Attacher.validate do
+    # Reject empty file uploads
+    # at least 1 byte
+    validate_min_size 1, message: I18n.t('submission.upload_failure_empty_file')
+  end
 end
