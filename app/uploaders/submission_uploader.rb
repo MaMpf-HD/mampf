6a8deabd811c4ab2dedcf28831326b0d81928e12
Splines <37160523+Splines@users.noreply.github.com>
2023-10-31 17:14:18 +0100

    Prevent upload of empty submission corrections (#552)
    * Prevent upload of empty files
    
    * Add console log for empty file
    
    * Add more specific error message for empty files
    
    * Validate file size in backend
    
    * Improve locale strings
    
    * Undo automatic linting
    
    * Undo unwanted changes
    
    * Reset whitespaces
    
    * Fix multiple files stats for bulk upload
    
    * Provide info about which file is empty to user

--- app/uploaders/submission_uploader.rb
+++ app/uploaders/submission_uploader.rb
@@ -7,4 +7,11 @@ class SubmissionUploader < Shrine
   plugin :upload_endpoint, max_size: 20 * 1024 * 1024 # 20 MB
   plugin :default_storage, cache: :submission_cache, store: :submission_store
   plugin :restore_cached_data
+  plugin :validation_helpers
+
+  Attacher.validate do
+    # Reject empty file uploads
+    # at least 1 byte
+    validate_min_size 1, message: I18n.t('submission.upload_failure_empty_file')
+  end
 end
