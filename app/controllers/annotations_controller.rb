ef91b49b7839940defe18586cc86b86509e7b3f7
Splines <dominic-plein@gmx.de>
2023-11-14 02:15:41 +0100

    Update modal background according to annotation color
    When we create a new annotation, a random color is chosen.
    TODO: Maybe do this random choice already in the backend,
    not just in the frontend.

--- app/controllers/annotations_controller.rb
+++ app/controllers/annotations_controller.rb
@@ -8,6 +8,7 @@ class AnnotationsController < ApplicationController
     @total_seconds = params[:total_seconds]
     @medium_id = params[:medium_id]
     @posted = false
+    @is_new_annotation = true
 
     render :edit
   end
@@ -29,6 +30,8 @@ class AnnotationsController < ApplicationController
     # we have to call the "get_comment" method in order to get
     # the text.
     @annotation.comment = @annotation.get_comment
+
+    @is_new_annotation = false
   end
 
   def create
