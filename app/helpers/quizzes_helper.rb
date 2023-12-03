dfd8b364b09874ff4107ad6518eba2535109fea9
Splines <dominic-plein@gmx.de>
2023-12-03 11:41:08 +0100

    Disable `Rails/HelperInstanceVariable` in `quizzes_helper.rb`
    This is since we only use the instance variable to provide a *default*
    value for some params, so the helper methods can still be reused.

--- app/helpers/quizzes_helper.rb
+++ app/helpers/quizzes_helper.rb
@@ -1,23 +1,25 @@
 # Quizzes Helper
 module QuizzesHelper
-  def answer_id(a_id, progress = @quiz_round.progress, # rubocop:todo Rails/HelperInstanceVariable
-                vertex = @quiz_round.vertex) # rubocop:todo Rails/HelperInstanceVariable
+  # rubocop:disable Rails/HelperInstanceVariable
+  def answer_id(a_id, progress = @quiz_round.progress,
+    vertex = @quiz_round.vertex)
     "r" + progress.to_s + "q" + vertex[:id].to_s + "a" + a_id.to_s
   end
 
-  def quiz_id(q_id = @quiz_round.quiz.id) # rubocop:todo Rails/HelperInstanceVariable
+  def quiz_id(q_id = @quiz_round.quiz.id)
     "quiz" + q_id.to_s
   end
-
-  def result_id(a_id, progress = @quiz_round.progress, # rubocop:todo Rails/HelperInstanceVariable
-                vertex = @quiz_round.vertex) # rubocop:todo Rails/HelperInstanceVariable
+  
+  def result_id(a_id, progress = @quiz_round.progress,
+    vertex = @quiz_round.vertex)
     "result" + progress.to_s + "q" + vertex[:id].to_s + "a" + a_id.to_s
   end
-
-  def cross_id(a_id, progress = @quiz_round.progress, # rubocop:todo Rails/HelperInstanceVariable
-               vertex = @quiz_round.vertex) # rubocop:todo Rails/HelperInstanceVariable
+  
+  def cross_id(a_id, progress = @quiz_round.progress,
+    vertex = @quiz_round.vertex)
     "cross" + progress.to_s + "q" + vertex[:id].to_s + "a" + a_id.to_s
   end
+  # rubocop:enable Rails/HelperInstanceVariable
 
   def vertices_labels_no_end(quiz)
     special = [[I18n.t("admin.quiz.undefined"), 0]]
