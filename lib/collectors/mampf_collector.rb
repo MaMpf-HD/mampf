2758444a5383540e5c7669abbc5de490d7b78e6b
Splines <dominic-plein@gmx.de>
2023-12-03 09:53:39 +0100

    Fix Layout/IndentationConsistency (manual)

--- lib/collectors/mampf_collector.rb
+++ lib/collectors/mampf_collector.rb
@@ -13,25 +13,19 @@ class MampfCollector < PrometheusExporter::Server::TypeCollector
   def metrics
     user_count_gauge = PrometheusExporter::Metric::Gauge.new("user_count",
                                                              "number of users in the app")
-      user_count_gauge.observe User.count # rubocop:todo Layout/IndentationConsistency
-      # rubocop:todo Layout/IndentationConsistency
-      medium_count_gauge = PrometheusExporter::Metric::Gauge.new("uploaded_medium_count",
-                                                                 "number of media")
-      # rubocop:enable Layout/IndentationConsistency
-      medium_count_gauge.observe Medium.count # rubocop:todo Layout/IndentationConsistency
-      # rubocop:todo Layout/IndentationConsistency
-      tag_count_gauge = PrometheusExporter::Metric::Gauge.new("tag_count", "number of tags")
-      # rubocop:enable Layout/IndentationConsistency
-      tag_count_gauge.observe Tag.count # rubocop:todo Layout/IndentationConsistency
-      # rubocop:todo Layout/IndentationConsistency
-      submissions_count_gauge = PrometheusExporter::Metric::Gauge.new("submissions_count",
-                                                                      "number of submissions")
-      # rubocop:enable Layout/IndentationConsistency
-      # rubocop:todo Layout/IndentationConsistency
-      submissions_count_gauge.observe Submission.count
-      # rubocop:enable Layout/IndentationConsistency
-      # rubocop:todo Layout/IndentationConsistency
-      [user_count_gauge, medium_count_gauge, tag_count_gauge, submissions_count_gauge]
-    # rubocop:enable Layout/IndentationConsistency
+    user_count_gauge.observe User.count
+
+    medium_count_gauge = PrometheusExporter::Metric::Gauge.new("uploaded_medium_count",
+                                                               "number of media")
+    medium_count_gauge.observe Medium.count
+
+    tag_count_gauge = PrometheusExporter::Metric::Gauge.new("tag_count", "number of tags")
+    tag_count_gauge.observe Tag.count
+
+    submissions_count_gauge = PrometheusExporter::Metric::Gauge.new("submissions_count",
+                                                                    "number of submissions")
+    submissions_count_gauge.observe Submission.count
+
+    [user_count_gauge, medium_count_gauge, tag_count_gauge, submissions_count_gauge]
   end
 end
