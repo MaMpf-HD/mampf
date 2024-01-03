70dbfed655db6da9abc563d2d9c4fc40ec6d26b5
Splines <dominic-plein@gmx.de>
2024-01-03 14:50:30 +0100

    Ignore sprocket syntax in cable.js

--- app/assets/javascripts/cable.js
+++ app/assets/javascripts/cable.js
@@ -1,9 +1,12 @@
 // Action Cable provides the framework to deal with WebSockets in Rails.
 // You can generate new channels where WebSocket features live using the `rails generate channel` command.
 //
+// disable eslint
+/* eslint-disable */
 //= require action_cable
 //= require_self
 //= require_tree ./channels
+/* eslint-enable */
 
 (function() {
   this.App || (this.App = {});
