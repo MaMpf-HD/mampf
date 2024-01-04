2020bdd6d646ae01607d108da9034dd84fb17f41
Splines <37160523+Splines@users.noreply.github.com>
2024-01-04 12:47:39 +0100

    Enforce new ESLint rules for all .js and .js.erb files (#575)
    * Add cypress rules to ESLint & ignore some patterns
    
    * Allow usage of tempusDominus global variable
    
    * Ignore JS files with Sprocket syntax
    
    * Further improve rules, e.g. allow common globals
    
    * Ignore sprocket syntax in cable.js
    
    * Autofix all `.js` and `.js.erb` files
    
    Command used:
    `yarn run eslint --fix .`
    
    Still 47 problems (27 errors, 20 warnings) after this.
    
    * Fix variables in turbolink fix
    
    * Prepend unused variables with "_"
    
    * Get rid of unused widget variable
    
    * Fix specs comment tab alignment

--- spec/cypress/support/on-rails.js
+++ spec/cypress/support/on-rails.js
@@ -1,37 +1,37 @@
 // CypressOnRails: dont remove these command
-Cypress.Commands.add('appCommands', function (body) {
+Cypress.Commands.add("appCommands", function (body) {
   cy.log("APP: " + JSON.stringify(body));
   return cy.request({
-    method: 'POST',
+    method: "POST",
     url: "/__cypress__/command",
     body: JSON.stringify(body),
     log: true,
-    failOnStatusCode: true
+    failOnStatusCode: true,
   }).then((response) => {
     return response.body;
   });
 });
 
-Cypress.Commands.add('app', function (name, command_options) {
-  return cy.appCommands({name: name, options: command_options}).then((body) => {
+Cypress.Commands.add("app", function (name, command_options) {
+  return cy.appCommands({ name: name, options: command_options }).then((body) => {
     return body[0];
   });
 });
 
-Cypress.Commands.add('appScenario', function (name, options = {}) {
-  return cy.app('scenarios/' + name, options);
+Cypress.Commands.add("appScenario", function (name, options = {}) {
+  return cy.app("scenarios/" + name, options);
 });
 
-Cypress.Commands.add('appEval', function (code) {
-  return cy.app('eval', code);
+Cypress.Commands.add("appEval", function (code) {
+  return cy.app("eval", code);
 });
 
-Cypress.Commands.add('appFactories', function (options) {
-  return cy.app('factory_bot', options);
+Cypress.Commands.add("appFactories", function (options) {
+  return cy.app("factory_bot", options);
 });
 
-Cypress.Commands.add('appFixtures', function (options) {
-  cy.app('activerecord_fixtures', options);
+Cypress.Commands.add("appFixtures", function (options) {
+  cy.app("activerecord_fixtures", options);
 });
 
 Cypress.Commands.add("dragTo", { prevSubject: "element" }, (subject, targetEl) => {
@@ -46,13 +46,13 @@ Cypress.Commands.add("dragTo", { prevSubject: "element" }, (subject, targetEl) =
 // });
 
 // comment this out if you do not want to attempt to log additional info on test fail
-Cypress.on('fail', (err, runnable) => {
+Cypress.on("fail", (err, runnable) => {
   // allow app to generate additional logging data
   Cypress.$.ajax({
-    url: '/__cypress__/command',
-    data: JSON.stringify({name: 'log_fail', options: {error_message: err.message, runnable_full_title: runnable.fullTitle() }}),
+    url: "/__cypress__/command",
+    data: JSON.stringify({ name: "log_fail", options: { error_message: err.message, runnable_full_title: runnable.fullTitle() } }),
     async: false,
-    method: 'POST'
+    method: "POST",
   });
 
   throw err;
