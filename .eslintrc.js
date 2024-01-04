99a95d1e3b6178d35a4710eb7624cbda845ace56
Splines <37160523+Splines@users.noreply.github.com>
2024-01-04 12:47:39 +0100

    Enforce new ESLint rules for all .js and .js.erb files (#575)
    * Add cypress rules to ESLint & ignore some patterns
    
    * Allow usage of tempusDominus global variable
    
    * Ignore JS files with Sprocket syntax
    
    * Further improve rules, e.g. allow common globals
    
    * Ignore sprocket syntax in cable.js
    
    * Autofix all `.js` and `.js.erb` files
    
    Command used:
    `yarn run eslint --fix .`
    
    Still 47 problems (27 errors, 20 warnings) after this.
    
    * Fix variables in turbolink fix
    
    * Prepend unused variables with "_"
    
    * Get rid of unused widget variable
    
    * Fix specs comment tab alignment

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,12 +12,42 @@ const stylistic = require("@stylistic/eslint-plugin");
 
 const customizedStylistic = stylistic.configs.customize({
   "indent": 2,
-  "quotes": "double",
   "jsx": false,
   "quote-props": "always",
   "semi": "always",
   "brace-style": "1tbs",
 });
+
+const cypressRules = {
+  "cypress/no-assigning-return-values": "error",
+  "cypress/no-unnecessary-waiting": "off", // TODO: fix this issue
+  "cypress/assertion-before-screenshot": "warn",
+  "cypress/no-force": "warn",
+  "cypress/no-async-tests": "error",
+  "cypress/no-pause": "error",
+};
+
+const ignoreFilesWithSprocketRequireSyntax = [
+  "app/assets/javascripts/application.js",
+  "app/assets/config/manifest.js",
+  "app/assets/javascripts/edit_clicker_assets.js",
+  "app/assets/javascripts/show_clicker_assets.js",
+  "app/assets/javascripts/geogebra_assets.js",
+  "vendor/assets/javascripts/thredded_timeago.js",
+];
+
+const customGlobals = {
+  TomSelect: "readable",
+  bootstrap: "readable",
+
+  // Rails globals
+  Routes: "readable",
+  App: "readable",
+  ActionCable: "readable",
+
+  // Common global methods
+  initBootstrapPopovers: "readable",
+};
 
 module.exports = {
   root: true,
@@ -26,18 +56,32 @@ module.exports = {
     sourceType: "module",
   },
   env: {
-    node: true,
-    browser: true,
-    jquery: true,
+    "node": true,
+    "browser": true,
+    "jquery": true,
+    "cypress/globals": true,
   },
   extends: [
     "eslint:recommended",
     // Allow linting of ERB files, see https://github.com/Splines/eslint-plugin-erb
     "plugin:erb/recommended",
   ],
-  plugins: ["@stylistic", "erb"],
+  globals: customGlobals,
+  plugins: ["@stylistic", "erb", "cypress"],
   rules: {
     ...customizedStylistic.rules,
-    "no-unused-vars": "warn",
+    "no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    ...cypressRules,
+    // see https://github.com/eslint-stylistic/eslint-stylistic/issues/254
+    "@stylistic/quotes": ["error", "double", { avoidEscape: true }],
   },
+  ignorePatterns: [
+    "node_modules/",
+    "pdfcomprezzor/",
+    "tmp/",
+    "public/packs/",
+    "public/packs-test/",
+    "public/uploads/",
+    ...ignoreFilesWithSprocketRequireSyntax,
+  ],
 };
