ca14be21b20de125052d8fbffd7d147a3b126d99
Splines <dominic-plein@gmx.de>
2024-01-25 20:08:09 +0100

    Fix linting in feedback.js

--- app/assets/javascripts/feedback.js
+++ app/assets/javascripts/feedback.js
@@ -1,57 +1,58 @@
-$(document).on('turbolinks:load', () => {
-    if (!shouldRegisterFeedback()) {
-        return;
-    }
-    registerToasts();
-    registerSubmitButtonHandler();
-    registerFeedbackBodyValidator();
+$(document).on("turbolinks:load", () => {
+  if (!shouldRegisterFeedback()) {
+    return;
+  }
+  registerToasts();
+  registerSubmitButtonHandler();
+  registerFeedbackBodyValidator();
 });
 
-SUBMIT_FEEDBACK_ID = '#submit-feedback';
+const SUBMIT_FEEDBACK_ID = "#submit-feedback";
 
-TOAST_OPTIONS = {
-    animation: true,
-    autohide: true,
-    delay: 6000 // autohide after ... milliseconds
+const TOAST_OPTIONS = {
+  animation: true,
+  autohide: true,
+  delay: 6000, // autohide after ... milliseconds
 };
 
 function shouldRegisterFeedback() {
-    return $(SUBMIT_FEEDBACK_ID).length > 0;
+  return $(SUBMIT_FEEDBACK_ID).length > 0;
 }
 
 function registerToasts() {
-    const toastElements = document.querySelectorAll('.toast');
-    const toastList = [...toastElements].map(toast => {
-        new bootstrap.Toast(toast, TOAST_OPTIONS);
-    });
+  const toastElements = document.querySelectorAll(".toast");
+  [...toastElements].map((toast) => {
+    new bootstrap.Toast(toast, TOAST_OPTIONS);
+  });
 }
 
 function registerSubmitButtonHandler() {
-    const submitButton = $('#submit-feedback-form-btn');
+  const submitButton = $("#submit-feedback-form-btn");
 
-    // Invoke the hidden submit button inside the actual Rails form
-    $('#submit-feedback-form-btn-outside').click(() => {
-        submitButton.click();
-    });
+  // Invoke the hidden submit button inside the actual Rails form
+  $("#submit-feedback-form-btn-outside").click(() => {
+    submitButton.click();
+  });
 
-    // Submit form by pressing Ctrl + Enter
-    document.addEventListener('keydown', (event) => {
-        const isModalOpen = $(SUBMIT_FEEDBACK_ID).is(':visible');
-        if (isModalOpen && event.ctrlKey && event.key == "Enter") {
-            submitButton.click();
-        }
-    });
+  // Submit form by pressing Ctrl + Enter
+  document.addEventListener("keydown", (event) => {
+    const isModalOpen = $(SUBMIT_FEEDBACK_ID).is(":visible");
+    if (isModalOpen && event.ctrlKey && event.key == "Enter") {
+      submitButton.click();
+    }
+  });
 }
 
 function registerFeedbackBodyValidator() {
-    const feedbackBody = document.getElementById('feedback_feedback');
-    feedbackBody.addEventListener('input', () => {
-        if (feedbackBody.validity.tooShort) {
-            const tooShortMessage = feedbackBody.dataset.tooShortMessage;
-            feedbackBody.setCustomValidity(tooShortMessage);
-        } else {
-            // render input valid, so that form will submit
-            feedbackBody.setCustomValidity('');
-        }
-    });
+  const feedbackBody = document.getElementById("feedback_feedback");
+  feedbackBody.addEventListener("input", () => {
+    if (feedbackBody.validity.tooShort) {
+      const tooShortMessage = feedbackBody.dataset.tooShortMessage;
+      feedbackBody.setCustomValidity(tooShortMessage);
+    }
+    else {
+      // render input valid, so that form will submit
+      feedbackBody.setCustomValidity("");
+    }
+  });
 }
