aaf9d37ef2e2e46a3bcfde3e9afa1b7e7b4c4109
Splines <dominic-plein@gmx.de>
2024-01-25 20:09:46 +0100

    Fix RuboCop errors

--- app/mailers/feedback_mailer.rb
+++ app/mailers/feedback_mailer.rb
@@ -5,11 +5,11 @@ class FeedbackMailer < ApplicationMailer
   # Mail to the MaMpf developers including the new feedback of a user.
   def new_user_feedback_email
     @feedback = params[:feedback]
-    reply_to_mail = @feedback.can_contact ? @feedback.user.email : ''
+    reply_to_mail = @feedback.can_contact ? @feedback.user.email : ""
     subject = "Feedback: #{@feedback.title}"
     mail(to: DefaultSetting::FEEDBACK_EMAIL,
          subject: subject,
-         content_type: 'text/plain',
+         content_type: "text/plain",
          reply_to: reply_to_mail)
   end
 end
