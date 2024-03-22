d792ed92602476c0c7bab0126b643d6be9febe64
Splines <dominic-plein@gmx.de>
2024-03-22 11:40:18 +0100

    Fix nulldb error

--- config/initializers/commontator.rb
+++ config/initializers/commontator.rb
@@ -319,7 +319,7 @@ Rails.application.config.to_prepare do
   # Load the Commontator extensions only if we are not in the assets:precompile
   # step where no db connection is available. See the production Dockerfile.
   db_adapter = ENV.fetch("DB_ADAPTER", nil)
-  if [nil, "nulldb"].include?(db_adapter)
+  if db_adapter == "nulldb"
     Rails.logger.info("DB_ADAPTER env var is #{db_adapter}. Skipping Commontator extensions.")
     next
   end
