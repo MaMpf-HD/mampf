97ea0dac29e40f6338d8a45af03ae6711e90ade0
Splines <dominic-plein@gmx.de>
2024-04-06 20:43:13 +0200

    Remove unused app environment variables usage
    The file `config/app_environment_variables.rb` does not exist in our
    codebase anymore.

--- config/environment.rb
+++ config/environment.rb
@@ -1,9 +1,5 @@
 # Load the Rails application.
 require_relative "application"
 
-# Load the app's custom environment variables here, so that they are loaded before environments/*.rb
-app_environment_variables = Rails.root.join("config/app_environment_variables.rb").to_s
-load(app_environment_variables) if File.exist?(app_environment_variables)
-
 # Initialize the Rails application.
 Rails.application.initialize!
