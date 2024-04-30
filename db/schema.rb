807833aa38aabd94ee1c04a43da784071593b249
Splines <dominic-plein@gmx.de>
2024-05-01 00:07:03 +0200

    Update active record schema version to 7.1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_04_22_200000) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_22_200000) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
