58b28c610b4e69d0e98e6c525b966d4466dd7091
Splines <dominic-plein@gmx.de>
2024-05-02 01:07:40 +0200

    Try to use `delete_all` instead of `destroy_all`
    This may be a fix for the issue `Can't modify frozen hash` we get in the
    pipeline during the unit tests.

--- spec/requests/media_spec.rb
+++ spec/requests/media_spec.rb
@@ -15,7 +15,7 @@ end
 RSpec.describe("Media", type: :request) do
   describe "#search_by" do
     before do
-      Medium.destroy_all
+      Medium.delete_all
 
       @medium1 = FactoryBot.create(:medium, :with_teachable, :with_editors, :released,
                                    sort: "Nuesse", description: "Erstes Medium")
