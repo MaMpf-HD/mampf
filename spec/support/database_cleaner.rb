f8d187232a5e38b3719e718fa20f26856f987420
Splines <dominic-plein@gmx.de>
2024-05-02 01:51:12 +0200

    Improve database cleaner config according to docs

--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -1,17 +1,13 @@
+# See the docs here: https://www.rubydoc.info/github/DatabaseCleaner/database_cleaner
 RSpec.configure do |config|
   config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
     DatabaseCleaner.clean_with(:truncation, except: ["ar_internal_metadata"])
   end
 
-  config.before(:each) do
-    DatabaseCleaner.strategy = :transaction
-  end
-
-  config.before(:each) do
-    DatabaseCleaner.start
-  end
-
-  config.after(:each) do
-    DatabaseCleaner.clean
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
   end
 end
