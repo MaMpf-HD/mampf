cf08e5b4b126e0410bfb754e803e77c5f5c09233
Splines <dominic-plein@gmx.de>
2024-05-07 10:51:34 +0200

    Use before() & after() instead of around() in DB cleaner

--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -2,12 +2,14 @@
 RSpec.configure do |config|
   config.before(:suite) do
     DatabaseCleaner.strategy = :transaction
-    DatabaseCleaner.clean_with(:truncation, except: ["ar_internal_metadata"])
+    DatabaseCleaner.clean_with(:truncation)
   end
 
-  config.around(:each) do |example|
-    DatabaseCleaner.cleaning do
-      example.run
-    end
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
   end
 end
