c9b157b5b832a59e46aa9ee247bf1babc1a5d443
Splines <dominic-plein@gmx.de>
2024-05-15 01:30:22 +0200

    Only load lectures_admin js related code when needed
    We also perform an early return if we no erdbeere examples are searched for, i.e.
    when the element is not yet visible on the page.

--- app/assets/javascripts/lectures.js
+++ app/assets/javascripts/lectures.js
@@ -1,14 +1,10 @@
-function disableExceptOrganizational() {
-  $("#lecture-organizational-warning").show();
-  $(".fa-edit").hide();
-  $(".new-in-lecture").hide();
-  $('[data-bs-toggle="collapse"]').prop("disabled", true).removeClass("clickable");
-};
-
 // Load example data (erdbeere) for structures
 function loadExampleStructures() {
   const structuresBody = $("#erdbeereStructuresBody");
   const lectureId = structuresBody.data("lecture");
+  if (lectureId === undefined) {
+    return;
+  }
   const loading = structuresBody.data("loading");
   structuresBody.empty().append(loading);
   $.ajax(Routes.edit_structures_path(lectureId), {
@@ -17,41 +13,14 @@ function loadExampleStructures() {
   });
 };
 
-/**
- * Takes care of the URL hashes, such that each bootstrab tab gets assigned
- * its own URL hash, e.g. "#assignments".
- *
- * This is necessary to be able to share the URL with a specific tab open.
- * It also allows to stay on the same tab after a page reload
- * (which is done when an edit action is canceled).
- *
- * Find out more details in this guide:
- * https://webdesign.tutsplus.com/how-to-add-deep-linking-to-the-bootstrap-4-tabs-component--cms-31180t
- */
-function configureUrlHashesForBootstrapTabs() {
-  if (location.hash) {
-    const hrefXPathIdentifier = `button[href="${location.hash}"]`;
-    $(`#lecture-nav-pills ${hrefXPathIdentifier}`).tab("show");
-  }
+function disableExceptOrganizational() {
+  $("#lecture-organizational-warning").show();
+  $(".fa-edit").hide();
+  $(".new-in-lecture").hide();
+  $('[data-bs-toggle="collapse"]').prop("disabled", true).removeClass("clickable");
+};
 
-  $('#lecture-nav-pills button[role="tab"]').on("focus", function () {
-    const hash = $(this).attr("href");
-    const urlWithoutHash = location.href.split("#")[0];
-    const newUrl = `${urlWithoutHash}${hash}`;
-    history.replaceState(null, null, newUrl);
-  });
-}
-
-$(document).on("ready turbolinks:load", function () {
-  let structures;
-  initBootstrapPopovers();
-  configureUrlHashesForBootstrapTabs();
-
-  // Reinitialize the masonry grid system when the lecture content is shown
-  $("#lecture-nav-content").on("focus click", () => {
-    initMasonryGridSystem();
-  });
-
+$(document).on("turbolinks:load", function () {
   $("#delete-forum").on("click", () => {
     const sureToDeleteMsg = $("#delete-forum").data("sureToDelete");
     const reallyDelete = confirm(sureToDeleteMsg);
@@ -355,6 +324,7 @@ $(document).on("ready turbolinks:load", function () {
     }
   });
 
+  let structures;
   loadExampleStructures();
 
   const $lectureStructures = $("#lectureStructuresInfo");
---
+++ app/assets/javascripts/lectures_admin.js
@@ -0,0 +1,37 @@
+/**
+ * Takes care of the URL hashes, such that each bootstrab tab gets assigned
+ * its own URL hash, e.g. "#assignments".
+ *
+ * This is necessary to be able to share the URL with a specific tab open.
+ * It also allows to stay on the same tab after a page reload
+ * (which is done when an edit action is canceled).
+ *
+ * Find out more details in this guide:
+ * https://webdesign.tutsplus.com/how-to-add-deep-linking-to-the-bootstrap-4-tabs-component--cms-31180t
+ */
+function configureUrlHashesForBootstrapTabs() {
+  $('#lecture-nav-pills button[role="tab"]').on("focus", function () {
+    const hash = $(this).attr("href");
+    const urlWithoutHash = location.href.split("#")[0];
+    const newUrl = `${urlWithoutHash}${hash}`;
+    history.replaceState(null, null, newUrl);
+  });
+
+  if (location.hash) {
+    const hrefXPathIdentifier = `button[href="${location.hash}"]`;
+    $(`#lecture-nav-pills ${hrefXPathIdentifier}`).tab("show");
+  }
+  else {
+    $("#lecture-nav-content").focus();
+  }
+}
+
+$(document).on("ready turbolinks:load", function () {
+  initBootstrapPopovers();
+  configureUrlHashesForBootstrapTabs();
+
+  // Reinitialize the masonry grid system when the lecture content is shown
+  $("#lecture-nav-content").on("focus click", () => {
+    initMasonryGridSystem();
+  });
+});
