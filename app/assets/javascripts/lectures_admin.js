98e80d8ce405360ceb409016a040a52bff85a303
Splines <dominic-plein@gmx.de>
2024-05-28 23:54:44 +0200

    Fix weird masonry grid system bug

--- app/assets/javascripts/lectures_admin.js
+++ app/assets/javascripts/lectures_admin.js
@@ -35,7 +35,7 @@ $(document).on("ready turbolinks:load", function () {
   navigateToActiveNavTab();
 
   // Reinitialize the masonry grid system when the lecture content is shown
-  $("#lecture-nav-content").on("focus click", () => {
+  $("#lecture-nav-content").on("click", () => {
     initMasonryGridSystem();
   });
 });
