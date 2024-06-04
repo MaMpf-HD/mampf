c4305f594700b434e8a1bc42a8f4a58d6cec6fd4
Splines <dominic-plein@gmx.de>
2024-06-05 00:52:29 +0200

    Improve comment for `after_authentication` hook

--- config/initializers/after_sign_in.rb
+++ config/initializers/after_sign_in.rb
@@ -1,9 +1,10 @@
 # Callback documentation for Warden (used by Devise):
 # https://github.com/wardencommunity/warden/wiki/Callbacks#after_authentication
 Warden::Manager.after_authentication do |user, _auth, _opts|
-  # See the UserCleaner class for more information
+  # User might have not logged in for a long time, which is why they have a
+  # deletion date set. If the user logs in, we unset this date to prevent the
+  # user from being deleted. See the UserCleaner class for more information.
+  # In case this callback does not work, a safety net is provided by the method
+  # `unset_deletion_date_for_recently_active_users` in the UserCleaner class.
   user.deletion_date = nil
-
-  # TODO: Add Cypress integration tests for this behavior. For now,
-  # test this behavior manually, e.g on localhost:5050 via pgadmin.
 end
