eda3d7b8ef1b6d184b380a11d5c78d7cfb86f5d7
Splines <dominic-plein@gmx.de>
2024-06-11 19:11:59 +0200

    Use `7.1` as Rails version specifier in migrations

--- db/migrate/20240530200000_add_deletion_date_to_user.rb
+++ db/migrate/20240530200000_add_deletion_date_to_user.rb
@@ -1,4 +1,4 @@
-class AddDeletionDateToUser < ActiveRecord::Migration[7.0]
+class AddDeletionDateToUser < ActiveRecord::Migration[7.1]
   def change
     add_column :users, :deletion_date, :date, null: true, default: nil
   end
--- db/migrate/20240605200000_remove_ghost_hash_from_user.rb
+++ db/migrate/20240605200000_remove_ghost_hash_from_user.rb
@@ -1,4 +1,4 @@
-class RemoveGhostHashFromUser < ActiveRecord::Migration[7.0]
+class RemoveGhostHashFromUser < ActiveRecord::Migration[7.1]
   def change
     remove_column :users, :ghost_hash, :string
   end
