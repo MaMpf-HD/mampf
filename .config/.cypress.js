0dd2c651664ead4eef3184b285b8da1adb240b2d
Splines <dominic-plein@gmx.de>
2024-06-19 00:53:12 +0200

    Remove e2e configuration

--- .config/.cypress.js
+++ .config/.cypress.js
@@ -1,11 +1,6 @@
 module.exports = {
   e2e: {
-    baseUrl: "http://localhost:3000",
-    defaultCommandTimeout: 10000,
-    projectId: "mampf-cypress",
-    retries: {
-      runMode: 2,
-      openMode: 0,
-    },
+    // Add configuration here
+    // Base URL is set via Docker environment variable
   },
 };
