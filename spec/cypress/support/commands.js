13c56a74b052cca03313b19e16b3e8781a648967
Splines <dominic-plein@gmx.de>
2024-06-26 23:34:26 +0200

    Rename cypress route subjects

--- spec/cypress/support/commands.js
+++ spec/cypress/support/commands.js
@@ -2,7 +2,7 @@
 import BackendCaller from "./backend_caller";
 
 Cypress.Commands.add("cleanDatabase", () => {
-  return BackendCaller.callCypressRoute("database_cleaner", "Database cleaner", {});
+  return BackendCaller.callCypressRoute("database_cleaner", "cy.cleanDatabase()", {});
 });
 
 beforeEach(() => {
@@ -13,7 +13,7 @@ Cypress.Commands.add("createUser", (role) => {
   if (!["admin", "editor", "teacher", "generic"].includes(role)) {
     throw new Error(`Invalid role: ${role}`);
   }
-  return BackendCaller.callCypressRoute("user_creator", "FactoryBot.createUser()", { role: role });
+  return BackendCaller.callCypressRoute("user_creator", "cy.createUser()", { role: role });
 });
 
 Cypress.Commands.add("login", (user) => {
