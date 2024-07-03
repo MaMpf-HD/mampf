cabb828279ace19422cc92cf8ad6c1ed264f1497
Splines <dominic-plein@gmx.de>
2024-07-03 19:49:15 +0200

    Wait for turbolinks:load (shining pointer)

--- app/assets/javascripts/cards.js
+++ app/assets/javascripts/cards.js
@@ -1,5 +1,7 @@
-$(".mampf-card").on("mousemove", function (event) {
-  const { x, y } = this.getBoundingClientRect();
-  this.style.setProperty("--x", event.clientX - x);
-  this.style.setProperty("--y", event.clientY - y);
+$(document).on("turbolinks:load", function () {
+  $(".mampf-card").on("mousemove", function (event) {
+    const { x, y } = this.getBoundingClientRect();
+    this.style.setProperty("--x", event.clientX - x);
+    this.style.setProperty("--y", event.clientY - y);
+  });
 });
