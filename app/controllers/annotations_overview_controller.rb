4e1b88978304c3bda7d4acc8687639f01e3672b0
Splines <dominic-plein@gmx.de>
2024-08-04 12:45:43 +0200

    Refactor annotations overview controller

--- app/controllers/annotations_overview_controller.rb
+++ app/controllers/annotations_overview_controller.rb
@@ -3,19 +3,22 @@ class AnnotationsOverviewController < ApplicationController
 
   def show
     user_annotations = Annotation.where(user_id: current_user.id).map do |annotation|
-      lecture_name = annotation.medium.teachable.lecture.title
-      link = helpers.video_link_timed(annotation.medium_id, annotation.timestamp)
-      link += "&ann=#{annotation.id}"
       {
         category: annotation.category,
         text: annotation.comment_optional,
-        link: link,
+        link: annotation_open_in_thyme_player_link(annotation),
         color: annotation.color,
         updated_at: annotation.updated_at,
-        lecture: lecture_name
+        lecture: annotation.medium.teachable.lecture.title
       }
     end
     @annotations_by_lecture = user_annotations.group_by { |annotation| annotation[:lecture] }
     render "annotations/annotations_overview"
+  end
+
+  def annotation_open_in_thyme_player_link(annotation)
+    link = helpers.video_link_timed(annotation.medium_id, annotation.timestamp)
+    link += "&ann=#{annotation.id}"
+    link
   end
 end
