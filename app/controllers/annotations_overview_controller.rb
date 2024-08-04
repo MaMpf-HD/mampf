b6f15f9cc3b5782009ec9b1254e10654db17ce2a
Splines <dominic-plein@gmx.de>
2024-08-04 02:41:39 +0200

    Group annotations by lecture

--- app/controllers/annotations_overview_controller.rb
+++ app/controllers/annotations_overview_controller.rb
@@ -2,7 +2,9 @@ class AnnotationsOverviewController < ApplicationController
   layout "application_no_sidebar_with_background"
 
   def show
-    @user_annotations = Annotation.where(user_id: current_user.id).map do |annotation|
+    user_annotations = Annotation.where(user_id: current_user.id).map do |annotation|
+      lecture_name = annotation.medium.teachable.lecture.title
+
       link = helpers.video_link_timed(annotation.medium_id, annotation.timestamp)
       link += "&ann=#{annotation.id}"
       {
@@ -10,9 +12,13 @@ class AnnotationsOverviewController < ApplicationController
         text: annotation.comment_optional,
         link: link,
         color: annotation.color,
-        updated_at: annotation.updated_at
+        updated_at: annotation.updated_at,
+        lecture: lecture_name
       }
     end
+
+    @annotations_by_lecture = user_annotations.group_by { |annotation| annotation[:lecture] }
+
     render "annotations/annotations_overview"
   end
 end
