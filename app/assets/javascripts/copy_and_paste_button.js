eb1d17d70370ab1c15b1188639168344a1992210
fosterfarrell9 <28628554+fosterfarrell9@users.noreply.github.com>
2024-08-04 15:48:59 +0200

    Create separate file for copy and paste button code and decaffeinate

---
+++ app/assets/javascripts/copy_and_paste_button.js
@@ -0,0 +1,20 @@
+$(document).on("turbolinks:load", function () {
+  var clipboard = new Clipboard(".clipboard-btn");
+
+  $(document).on("click", ".clipboard-button", function () {
+    $(".token-clipboard-popup").removeClass("show");
+    var id = $(this).data("id");
+    $('.token-clipboard-popup[data-id="' + id + '"]').addClass("show");
+
+    var restoreClipboardButton = function () {
+      $('.token-clipboard-popup[data-id="' + id + '"]').removeClass("show");
+    };
+
+    setTimeout(restoreClipboardButton, 1500);
+  });
+});
+
+// clean up for turbolinks
+$(document).on("turbolinks:before-cache", function () {
+  $(document).off("click", ".clipboard-button");
+});
