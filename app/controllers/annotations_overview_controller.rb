e6cf4bbdc205dbc51b54c359563f5c30954840cf
Splines <dominic-plein@gmx.de>
2024-08-04 16:11:04 +0200

    Refactor annotations overview controller

--- app/controllers/annotations_overview_controller.rb
+++ app/controllers/annotations_overview_controller.rb
@@ -2,22 +2,18 @@ class AnnotationsOverviewController < ApplicationController
   layout "application_no_sidebar_with_background"
 
   def show
-    user_annotations = Annotation.where(user_id: current_user.id).map do |annotation|
-      link = { link: annotation_open_link(annotation) }
-      extract_relevant_information(annotation).merge(link)
-    end
+    user_annotations = Annotation.where(user_id: current_user.id)
+                                 .map { |a| extract_relevant_information(a) }
     @annotations_by_lecture = user_annotations.group_by { |annotation| annotation[:lecture] }
     # TODO: don't include lecture key in the hash anymore after grouping
     # Maybe we can even gruop in the SQL query directly?
 
     @show_students_annotations = current_user.teachable_editor_or_teacher?
     if @show_students_annotations
-      student_annotations = Annotation.where(medium_id: medium_ids_for_teacher_or_editor,
-                                             visible_for_teacher: true)
-                                      .map do |annotation|
-        link = { link: annotation_open_link(annotation, in_feedback_player: true) }
-        extract_relevant_information(annotation).merge(link)
-      end
+      student_annotations =
+        Annotation.where(medium_id: medium_ids_for_teacher_or_editor,
+                         visible_for_teacher: true)
+                  .map { |a| extract_relevant_information(a, is_shared: true) }
       @student_annotations_by_lecture = student_annotations.group_by do |annotation|
         annotation[:lecture]
       end
@@ -26,28 +22,28 @@ class AnnotationsOverviewController < ApplicationController
     render "annotations/annotations_overview"
   end
 
-  def extract_relevant_information(annotation)
-    {
-      category: annotation.category,
-      text: annotation.comment_optional,
-      color: annotation.color,
-      updated_at: annotation.updated_at,
-      lecture: annotation.medium.teachable.lecture.title
-    }
-  end
-
   def medium_ids_for_teacher_or_editor
     lectures = current_user.given_lectures + current_user.edited_lectures
     lectures.map(&:media_with_inheritance).flatten.pluck(:id)
   end
 
-  def annotation_open_link(annotation, in_feedback_player: false)
-    link = if in_feedback_player
+  def extract_relevant_information(annotation, is_shared: false)
+    {
+      category: annotation.category,
+      text: annotation.comment_optional,
+      color: annotation.color,
+      updated_at: annotation.updated_at,
+      lecture: annotation.medium.teachable.lecture.title,
+      link: annotation_open_link(annotation, is_shared)
+    }
+  end
+
+  def annotation_open_link(annotation, is_shared)
+    link = if is_shared
       helpers.feedback_video_link_timed(annotation.medium_id, annotation.timestamp)
     else
       helpers.video_link_timed(annotation.medium_id, annotation.timestamp)
     end
-
     link += "&ann=#{annotation.id}"
     link
   end
