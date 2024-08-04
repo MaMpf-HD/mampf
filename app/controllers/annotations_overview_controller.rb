a6f6e2dc4733d0b96b58c277c31b4cebddb99122
Splines <dominic-plein@gmx.de>
2024-08-04 03:02:43 +0200

    Delete unnecessary whitespace

--- app/controllers/annotations_overview_controller.rb
+++ app/controllers/annotations_overview_controller.rb
@@ -4,7 +4,6 @@ class AnnotationsOverviewController < ApplicationController
   def show
     user_annotations = Annotation.where(user_id: current_user.id).map do |annotation|
       lecture_name = annotation.medium.teachable.lecture.title
-
       link = helpers.video_link_timed(annotation.medium_id, annotation.timestamp)
       link += "&ann=#{annotation.id}"
       {
@@ -16,9 +15,7 @@ class AnnotationsOverviewController < ApplicationController
         lecture: lecture_name
       }
     end
-
     @annotations_by_lecture = user_annotations.group_by { |annotation| annotation[:lecture] }
-
     render "annotations/annotations_overview"
   end
 end
