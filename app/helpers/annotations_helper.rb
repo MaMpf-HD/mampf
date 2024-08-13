28f023291b04127f0b97428f17c27aabcff79af5
Splines <dominic-plein@gmx.de>
2024-08-13 15:42:13 +0200

    Extract border color to helper method

--- app/helpers/annotations_helper.rb
+++ app/helpers/annotations_helper.rb
@@ -8,4 +8,12 @@ module AnnotationsHelper
     link += "&ann=#{annotation.id}"
     link
   end
+
+  def annotation_index_border_color(annotation, is_student_annotation)
+    # The border color of annotation cards shared by students, will be set
+    # via JS according to the color of the annotation CATEGORY.
+    return "" if is_student_annotation
+
+    "border-color: #{annotation[:color]}"
+  end
 end
