6d09e9e6b98467477ccc1cb4cfc9805555c05527
fosterfarrell9 <28628554+fosterfarrell9@users.noreply.github.com>
2024-08-21 19:44:12 +0200

    Fix lecture edit page if active term is not set (#683)
    * Add missing case that Term.active is nil
    
    * Add unit tests for lecture#stale?
    
    * Make lecture#older_than? method private
    
    * Add cypress test for lecture edit page
    
    * Make better use of .then()
    
    Co-authored-by: Splines <37160523+Splines@users.noreply.github.com>
    
    * Add docstring
    
    * Remove obsolete line
    
    * Rename for consistency
    
    * Add missing punctuation to docstring
    
    ---------
    
    Co-authored-by: Splines <37160523+Splines@users.noreply.github.com>
    Co-authored-by: Splines <dominic-plein@gmx.de>

---
+++ spec/cypress/e2e/lectures_spec.cy.js
@@ -0,0 +1,15 @@
+import FactoryBot from "../support/factorybot";
+
+describe("Lecture edit page", () => {
+  it("shows content tab button", function () {
+    cy.createUserAndLogin("teacher").then((teacher) => {
+      FactoryBot.create("lecture", "with_teacher_by_id",
+        { teacher_id: teacher.id }).as("lecture");
+    });
+
+    cy.then(() => {
+      cy.visit(`/lectures/${this.lecture.id}/edit`);
+      cy.getBySelector("content-tab-btn").should("be.visible");
+    });
+  });
+});
