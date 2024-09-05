1c2a3cd757be2a0ec0308c7ee776821606515697
Splines <dominic-plein@gmx.de>
2024-09-05 19:02:16 +0200

    Improve voucher model specs

--- spec/models/voucher_spec.rb
+++ spec/models/voucher_spec.rb
@@ -20,38 +20,22 @@ RSpec.describe(Voucher, type: :model) do
     end
 
     describe "#add_expiration_datetime" do
-      let(:voucher) { build(:voucher, role: role, created_at: Time.zone.now) }
-
-      context "when the voucher is for a speaker" do
-        let(:role) { :speaker }
-
-        it "sets the expiration date to SPEAKER_EXPIRATION_DAYS from created_at" do
-          voucher.save
-          expect(voucher.expires_at).to(
-            eq(voucher.created_at + Voucher::SPEAKER_EXPIRATION_DAYS.days)
-          )
+      def expiration_days(role)
+        case role
+        when :speaker
+          Voucher::SPEAKER_EXPIRATION_DAYS
+        when :tutor
+          Voucher::TUTOR_EXPIRATION_DAYS
+        else
+          Voucher::DEFAULT_EXPIRATION_DAYS
         end
       end
 
-      context "when the voucher is for a tutor" do
-        let(:role) { :tutor }
-
-        it "sets the expiration date to TUTOR_EXPIRATION_DAYS from created_at" do
+      it "sets the expiration date correctly based on the role" do
+        Voucher::ROLE_HASH.each_key do |role|
+          voucher = build(:voucher, lecture: lecture, role: role)
           voucher.save
-          expect(voucher.expires_at).to(
-            eq(voucher.created_at + Voucher::TUTOR_EXPIRATION_DAYS.days)
-          )
-        end
-      end
-
-      context "when the voucher is for another role" do
-        let(:role) { :teacher }
-
-        it "sets the expiration date to DEFAULT_EXPIRATION_DAYS from created_at" do
-          voucher.save
-          expect(voucher.expires_at).to(
-            eq(voucher.created_at + Voucher::DEFAULT_EXPIRATION_DAYS.days)
-          )
+          expect(voucher.expires_at).to eq(voucher.created_at + expiration_days(role).days)
         end
       end
     end
@@ -70,10 +54,11 @@ RSpec.describe(Voucher, type: :model) do
     end
 
     describe "#ensure_speaker_vouchers_only_for_seminars" do
-      context "whe the lecture is a seminar" do
+      context "when the lecture is a seminar" do
         let(:voucher) { build(:voucher, :speaker, lecture: seminar) }
 
         it "does not add an error" do
+          expect(voucher).to be_valid
           expect(voucher.save).to be_truthy
           expect(voucher.errors[:role]).to be_empty
         end
@@ -95,20 +80,24 @@ RSpec.describe(Voucher, type: :model) do
 
   describe "scopes" do
     describe ".active" do
-      let!(:active_voucher) { FactoryBot.create(:voucher) }
-      let!(:expired_voucher) { FactoryBot.create(:voucher, :expired) }
-      let!(:invalidated_voucher) { FactoryBot.create(:voucher, :invalidated) }
-
       it "includes vouchers that are not expired and not invalidated" do
+        active_voucher = FactoryBot.create(:voucher)
         expect(Voucher.active).to include(active_voucher)
       end
 
       it "excludes vouchers that are expired" do
+        expired_voucher = FactoryBot.create(:voucher, :expired)
         expect(Voucher.active).not_to include(expired_voucher)
       end
 
       it "excludes vouchers that are invalidated" do
+        invalidated_voucher = FactoryBot.create(:voucher, :invalidated)
         expect(Voucher.active).not_to include(invalidated_voucher)
+      end
+
+      it "excludes vouchers that are both expired and invalidated" do
+        voucher = FactoryBot.create(:voucher, :expired, :invalidated)
+        expect(Voucher.active).not_to include(voucher)
       end
     end
   end
