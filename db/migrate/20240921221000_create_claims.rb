b741b1f463fe25fad3fcdcc860e1c70c81e71861
Splines <dominic-plein@gmx.de>
2024-09-21 23:04:51 +0200

    Delete duplicate migration & update new migration dates/schema

--- db/migrate/20240921221000_create_claims.rb
+++ db/migrate/20240921221000_create_claims.rb
@@ -3,7 +3,6 @@ class CreateClaims < ActiveRecord::Migration[7.1]
     create_table :claims do |t|
       t.references :redemption, null: false, foreign_key: true
       t.references :claimable, polymorphic: true, null: false
-
       t.timestamps
     end
   end
