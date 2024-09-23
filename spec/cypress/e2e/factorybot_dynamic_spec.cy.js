d473edd02a2de12be5aeb82325bc5ba4059b8875
Splines <dominic-plein@gmx.de>
2024-09-24 00:34:12 +0200

    Include temporary FactoryBot dynamic method assignment test

---
+++ spec/cypress/e2e/factorybot_dynamic_spec.cy.js
@@ -0,0 +1,17 @@
+import FactoryBot from "../support/factorybot";
+
+describe("FactoryBot dynamic", () => {
+  beforeEach(function () {
+    cy.createUser("teacher").as("teacher");
+    cy.createUserAndLogin("generic").as("user");
+    cy.then(() => {
+      FactoryBot.create("lecture", { teacher_id: this.teacher.id }).as("lecture");
+    });
+  });
+
+  it("Instance methods", function () {
+    cy.log(this.lecture);
+    cy.log(`Type of lecture: ${typeof this.lecture}`);
+    cy.log(this.lecture.finalllllly());
+  });
+});
