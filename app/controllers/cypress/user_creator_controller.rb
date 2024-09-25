939bb92435dd79d1727614723ec2733ccbae286c
Splines <dominic-plein@gmx.de>
2024-09-25 14:28:12 +0200

    Allow creation of multiple users of same role
    e.g. multiple generic users

--- app/controllers/cypress/user_creator_controller.rb
+++ app/controllers/cypress/user_creator_controller.rb
@@ -10,8 +10,10 @@ module Cypress
 
       role = params[:role]
       is_admin = (role == "admin")
+      random_hash = SecureRandom.hex(6)
 
-      user = User.create(name: "#{role} Cypress", email: "#{role}@mampf.cypress",
+      user = User.create(name: "#{role} Cypress #{random_hash}",
+                         email: "#{role}-#{random_hash}@mampf.cypress",
                          password: "cypress123", consents: true, admin: is_admin,
                          locale: I18n.default_locale)
       user.confirm
