c427f16d15f7220498730c421f59c4c13ba5c156
Splines <dominic-plein@gmx.de>
2024-09-25 14:27:50 +0200

    Add Cypress tests to test array as argument

--- spec/cypress/e2e/meta/factory_bot_spec.cy.js
+++ spec/cypress/e2e/meta/factory_bot_spec.cy.js
@@ -2,11 +2,30 @@ import FactoryBot from "../../support/factorybot";
 
 describe("FactoryBot.create()", () => {
   beforeEach(function () {
+    cy.createUserAndLogin("teacher").as("teacher");
+  });
+
+  it("allows to call create() with array as argument", function () {
+    cy.createUser("generic").as("tutor1");
+    cy.createUser("generic").as("tutor2");
+    FactoryBot.create("lecture", { teacher_id: this.teacher.id }).as("lecture");
+
+    cy.then(() => {
+      // here we pass in an array as argument to tutor_ids
+      FactoryBot.create("tutorial",
+        { lecture_id: this.lecture.id, tutor_ids: [this.tutor1.id, this.tutor2.id] },
+      );
+    });
+  });
+});
+
+describe("FactoryBot.create().call", () => {
+  beforeEach(function () {
     cy.createUser("teacher").as("teacher");
     cy.createUserAndLogin("generic");
   });
 
-  it("allows to call instance method after assigning them to an alias", function () {
+  it("allows to call instance methods after assigning them to an alias", function () {
     FactoryBot.create("lecture", { teacher_id: this.teacher.id }).as("lecture");
 
     cy.then(() => {
@@ -17,7 +36,7 @@ describe("FactoryBot.create()", () => {
     });
   });
 
-  it("allows to call instance methods directly (without alias)", function () {
+  it("allows to call instance methods directly (without an alias)", function () {
     FactoryBot.create("lecture", { teacher_id: this.teacher.id }).then((lecture) => {
       // via return value of FactoryBot.create() directly (no alias intermediate)
       lecture.call.long_title().then((res) => {
