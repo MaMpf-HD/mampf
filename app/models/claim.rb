57530f29f6eb5df03b92698def74723eacd9e7ae
Splines <dominic-plein@gmx.de>
2024-10-08 17:49:34 +0200

    Move Claim model to voucher folder

--- app/models/claim.rb
+++
@@ -1,8 +0,0 @@
-# Claim class
-# claims store what is being taken over by the user when they redeem a voucher
-# (e.g. a tutorial or a talk)
-
-class Claim < ApplicationRecord
-  belongs_to :redemption
-  belongs_to :claimable, polymorphic: true
-end
