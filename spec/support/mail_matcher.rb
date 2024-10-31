c25aa28b7b6fcfc07c6755cd95aa3a847bbc01f7
Splines <dominic-plein@gmx.de>
2024-10-31 01:54:14 +0100

    Use just one file for mail matchers

--- spec/support/mail_matcher.rb
+++ spec/support/mail_matcher.rb
@@ -1,3 +1,18 @@
+RSpec::Matchers.define(:enqueue_mail_with_params) do |mailer, method, params|
+  supports_block_expectations
+
+  match do |block|
+    expect do
+      block.call
+    end.to have_enqueued_mail(mailer, method)
+      .with(hash_including(params: hash_including(params)))
+  end
+
+  failure_message do |_block|
+    "expected to enqueue #{mailer}.#{method} with #{params}, but instead enqueued: #{enqueued_jobs}"
+  end
+end
+
 RSpec::Matchers.define(:include_in_html_body) do |expected|
   # ignore \r and \n in the comparison
   mail_body_stripped = ->(mail) { mail.html_part.body.decoded.gsub(/[\r\n]/, "") }
