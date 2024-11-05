f7d9d79c79cddf9405e566210736e6e6ff6bcd63
Splines <dominic-plein@gmx.de>
2024-11-05 20:13:50 +0100

    Add tests for teacher selection dropdown

--- spec/cypress/e2e/lectures_spec.cy.js
+++ spec/cypress/e2e/lectures_spec.cy.js
@@ -13,3 +13,46 @@ describe("Lecture edit page", () => {
     });
   });
 });
+
+describe("Lecture people edit page", () => {
+  beforeEach(function () {
+    cy.createUser("generic").as("user");
+    cy.createUserAndLogin("teacher").as("teacher");
+    cy.then(() => {
+      FactoryBot.create("lecture", { teacher_id: this.teacher.id }).as("lecture");
+    });
+    cy.then(() => {
+      cy.wrap(`/lectures/${this.lecture.id}/edit#people`).as("lecturePeopleUrl");
+    });
+  });
+
+  context("when logged in as teacher", () => {
+    it.only("does not show element to select teacher", function () {
+      cy.visit(this.lecturePeopleUrl);
+      cy.getBySelector("teacher-admin-select").should("not.exist");
+      cy.getBySelector("teacher-info").should("exist");
+    });
+  });
+
+  context("when logged in as admin", () => {
+    beforeEach(function () {
+      cy.createUserAndLogin("admin").as("admin");
+    });
+
+    it("shows element to select teacher", function () {
+      cy.logout();
+      cy.createUserAndLogin("admin").as("admin");
+      cy.visit(this.lecturePeopleUrl);
+
+      cy.getBySelector("teacher-admin-select").should("be.visible");
+      cy.getBySelector("teacher-admin-select").type("cy");
+
+      cy.getBySelector("teacher-admin-select").should("contain", this.user.name_in_tutorials);
+      cy.getBySelector("teacher-admin-select").should("contain", this.user.email);
+      cy.getBySelector("teacher-admin-select").should("contain", this.teacher.name_in_tutorials);
+      cy.getBySelector("teacher-admin-select").should("contain", this.teacher.email);
+      cy.getBySelector("teacher-admin-select").should("contain", this.admin.name_in_tutorials);
+      cy.getBySelector("teacher-admin-select").should("contain", this.admin.email);
+    });
+  });
+});
