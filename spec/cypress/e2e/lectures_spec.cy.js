8b2de36511f898c4f60c200b93913ea3d5e00380
Splines <dominic-plein@gmx.de>
2024-11-05 20:43:10 +0100

    Write test for editor dropdown selection

--- spec/cypress/e2e/lectures_spec.cy.js
+++ spec/cypress/e2e/lectures_spec.cy.js
@@ -27,32 +27,53 @@ describe("Lecture people edit page", () => {
   });
 
   context("when logged in as teacher", () => {
-    it.only("does not show element to select teacher", function () {
+    it("does not show element to select teacher", function () {
       cy.visit(this.lecturePeopleUrl);
+
       cy.getBySelector("teacher-admin-select").should("not.exist");
       cy.getBySelector("teacher-info").should("exist");
+    });
+
+    it("prohibits searching for arbitrary users in the editor dropdown", function () {
+      cy.visit(this.lecturePeopleUrl);
+
+      cy.getBySelector("editor-select").click();
+      cy.getBySelector("editor-select").type("cy");
+
+      cy.getBySelector("editor-select").should("not.contain", this.user.name_in_tutorials);
+      cy.getBySelector("editor-select").should("not.contain", this.user.email);
     });
   });
 
   context("when logged in as admin", () => {
     beforeEach(function () {
+      cy.logout();
       cy.createUserAndLogin("admin").as("admin");
     });
 
-    it("shows element to select teacher", function () {
-      cy.logout();
-      cy.createUserAndLogin("admin").as("admin");
+    function shouldContainAllUsers(selector, context) {
+      cy.getBySelector(selector).should("contain", context.user.name_in_tutorials);
+      cy.getBySelector(selector).should("contain", context.user.email);
+      cy.getBySelector(selector).should("contain", context.teacher.name_in_tutorials);
+      cy.getBySelector(selector).should("contain", context.teacher.email);
+      cy.getBySelector(selector).should("contain", context.admin.name_in_tutorials);
+      cy.getBySelector(selector).should("contain", context.admin.email);
+    }
+
+    it("allows searching for arbitrary users to assign them as teachers", function () {
       cy.visit(this.lecturePeopleUrl);
 
       cy.getBySelector("teacher-admin-select").should("be.visible");
       cy.getBySelector("teacher-admin-select").type("cy");
+      shouldContainAllUsers("teacher-admin-select", this);
+    });
 
-      cy.getBySelector("teacher-admin-select").should("contain", this.user.name_in_tutorials);
-      cy.getBySelector("teacher-admin-select").should("contain", this.user.email);
-      cy.getBySelector("teacher-admin-select").should("contain", this.teacher.name_in_tutorials);
-      cy.getBySelector("teacher-admin-select").should("contain", this.teacher.email);
-      cy.getBySelector("teacher-admin-select").should("contain", this.admin.name_in_tutorials);
-      cy.getBySelector("teacher-admin-select").should("contain", this.admin.email);
+    it.only("allows searching for arbitrary users to assign them as editors", function () {
+      cy.visit(this.lecturePeopleUrl);
+
+      cy.getBySelector("editor-select").click();
+      cy.getBySelector("editor-select").type("cy");
+      shouldContainAllUsers("editor-select", this);
     });
   });
 });
