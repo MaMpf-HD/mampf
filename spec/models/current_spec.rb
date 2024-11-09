cc82b9447fbdcb1a85aaeea620f13c9cd26fbf6f
Splines <dominic-plein@gmx.de>
2024-11-09 23:11:52 +0100

    Remove current_user= test as method was removed

--- spec/models/current_spec.rb
+++ spec/models/current_spec.rb
@@ -9,11 +9,4 @@ RSpec.describe(Current) do
       expect(Current.user).to eq(:user)
     end
   end
-
-  describe "current_user= via ApplicationController" do
-    it "sets the current user" do
-      ApplicationController.current_user = :user
-      expect(Current.user).to eq(:user)
-    end
-  end
 end
