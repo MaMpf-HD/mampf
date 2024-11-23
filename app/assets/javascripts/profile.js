8093267b24e9442be7e3a021be408cf19d76f114
Splines <dominic-plein@gmx.de>
2024-11-23 14:21:43 +0100

    Remove left-over console log

--- app/assets/javascripts/profile.js
+++ app/assets/javascripts/profile.js
@@ -36,7 +36,6 @@ $(document).ready(function () {
   });
 
   $("#request-data-btn").on("click", function () {
-    console.log("pressed");
     const toast = $("#request-data-toast");
     bootstrap.Toast.getOrCreateInstance(toast).show();
   });
