f7bcb3c433787317ba7d9044a073cc56c2ebdb99
Splines <dominic-plein@gmx.de>
2024-12-03 18:07:48 +0100

    Add test for module settings (allow to subscribe to a lecture)

--- spec/cypress/e2e/profile_spec.cy.js
+++ spec/cypress/e2e/profile_spec.cy.js
@@ -115,28 +115,48 @@ describe("Account settings", () => {
   });
 });
 
-describe("Module settings", () => {
+describe.only("Module settings", () => {
   beforeEach(function () {
-    cy.createUserAndLogin("generic").as("user");
+    cy.wrap("Happy course").as("courseName");
+    cy.wrap("Happy division").as("divisionName");
 
-    FactoryBot.create("course").as("course1");
-    FactoryBot.create("course").as("course2");
+    cy.then(() => {
+      FactoryBot.create("division", { name: this.divisionName }).as("division");
+    });
+
+    cy.then(() => {
+      FactoryBot.create("course", "with_division",
+        { title: this.courseName, division_id: this.division.id }).as("course");
+    });
 
     cy.then(() => {
       FactoryBot.create("lecture", "released_for_all",
-        { course_id: this.course1.id }).as("lecture1");
-      FactoryBot.create("lecture", "released_for_all",
-        { course_id: this.course2.id }).as("lecture2");
+        { course_id: this.course.id }).as("lecture");
     });
-    // cy.createUser("teacher").as("teacher");
-    // FactoryBot.create("lecture_with_sparse_toc", "released_for_all").as("lecture");
   });
 
-  it("todo", function () {
-    cy.visit(PROFILE_PAGE);
+  it("allows to subscribe to a lecture", function () {
+    this.lecture.call.teacher().as("teacher");
+    this.lecture.call.term().as("term");
 
-    cy.logout();
     cy.createUserAndLogin("admin").as("admin");
     cy.visit(PROFILE_PAGE);
+
+    cy.getBySelector("courses-accordion").find("button:visible").first().click();
+    cy.getBySelector("courses-accordion").should("contain", this.divisionName);
+    cy.contains(this.courseName).click();
+
+    this.lecture.call.term_teacher_info().as("lectureName");
+    cy.then(() => {
+      cy.contains(this.lectureName).click();
+      cy.contains(this.lectureName).parent().find("input").should("be.checked");
+      cy.getBySelector("profile-change-submit").click();
+    });
+
+    cy.then(() => {
+      cy.visit("/main/start");
+      cy.getBySelector("subscribed-inactive-lectures-collapse").contains(this.courseName);
+      cy.getBySelector("subscribed-inactive-lectures-collapse").contains(this.teacher.name);
+    });
   });
 });
