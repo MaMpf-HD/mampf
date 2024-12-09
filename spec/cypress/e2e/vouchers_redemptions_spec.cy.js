1d80266f00453d076f49fd0f666cbe227b8e5b62
Splines <dominic-plein@gmx.de>
2024-12-09 13:54:44 +0100

    Wait for delete account request to have finished

--- spec/cypress/e2e/vouchers_redemptions_spec.cy.js
+++ spec/cypress/e2e/vouchers_redemptions_spec.cy.js
@@ -236,7 +236,10 @@ describe("User & Redemption deletion", () => {
     cy.visit("/profile/edit");
     cy.getBySelector("delete-account-btn").click();
     cy.getBySelector("delete-account-pwd-field").type(this.user.password);
+
+    cy.intercept("POST", "/users*").as("deleteUserRequest");
     cy.getBySelector("delete-account-confirm-btn").click();
+    cy.wait("@deleteUserRequest");
 
     cy.then(() => {
       helpers.logoutAndLoginAsTeacher(this);
