0002f39821726357fcfc5df9c5e18f598dfc4535
Florian <florianbuerckel@gmail.com>
2024-12-15 17:48:59 +0100

    rebased dev into vignette
    Signed-off-by: Florian <florianbuerckel@gmail.com>

--- spec/models/user_cleaner_spec_BACKUP_140121.rb
+++ spec/models/user_cleaner_spec_BACKUP_140121.rb
@@ -4,18 +4,15 @@ RSpec.describe(UserCleaner, type: :model) do
   # Non-generic users are either admins, teachers or editors
   let(:user_admin) do
     return FactoryBot.create(:confirmed_user, deletion_date: Date.current - 1.day, admin: true)
-    return FactoryBot.create(:confirmed_user, deletion_date: Date.current - 1.day, admin: true)
   end
 
   let(:user_teacher) do
-    user_teacher = FactoryBot.create(:confirmed_user, deletion_date: Date.current - 1.day)
     user_teacher = FactoryBot.create(:confirmed_user, deletion_date: Date.current - 1.day)
     FactoryBot.create(:lecture, teacher: user_teacher)
     return user_teacher
   end
 
   let(:user_editor) do
-    user_editor = FactoryBot.create(:confirmed_user, deletion_date: Date.current - 1.day)
     user_editor = FactoryBot.create(:confirmed_user, deletion_date: Date.current - 1.day)
     FactoryBot.create(:lecture, editors: [user_editor])
     return user_editor
@@ -35,60 +32,12 @@ RSpec.describe(UserCleaner, type: :model) do
                           confirmation_sent_date: 7.months.ago, current_sign_in_at: nil)
         expect(UserCleaner.new.inactive_users.count).to eq(1)
       end
-    context "when user is confirmed" do
-      it "counts users without current_sign_in_at date as inactive" do
-        # but only if also the confirmation date is older than the threshold
-        FactoryBot.create(:confirmed_user, :with_confirmation_sent_date,
-                          confirmation_sent_date: 5.months.ago, current_sign_in_at: nil)
-        FactoryBot.create(:confirmed_user, :with_confirmation_sent_date,
-                          confirmation_sent_date: 7.months.ago, current_sign_in_at: nil)
-        expect(UserCleaner.new.inactive_users.count).to eq(1)
-      end
 
       it("counts users with current_sign_in_at date older than threshold as inactive") do
         FactoryBot.create(:confirmed_user, current_sign_in_at: 7.months.ago)
         expect(UserCleaner.new.inactive_users.count).to eq(1)
       end
-      it("counts users with current_sign_in_at date older than threshold as inactive") do
-        FactoryBot.create(:confirmed_user, current_sign_in_at: 7.months.ago)
-        expect(UserCleaner.new.inactive_users.count).to eq(1)
-      end
 
-      it "does not count users with current_sign_in_at date younger than threshold as inactive" do
-        FactoryBot.create(:confirmed_user, current_sign_in_at: 5.months.ago)
-        expect(UserCleaner.new.inactive_users.count).to eq(0)
-      end
-    end
-
-    context "when user is not confirmed yet" do
-      def test_non_confirmed_user(confirmation_sent_date, expected_inactive_users_count)
-        user = FactoryBot.create(:user, :with_confirmation_sent_date,
-                                 confirmation_sent_date: confirmation_sent_date,
-                                 current_sign_in_at: nil)
-        FactoryBot.create(:user, :with_confirmation_sent_date,
-                          confirmation_sent_date: confirmation_sent_date,
-                          current_sign_in_at: 5.months.ago)
-        FactoryBot.create(:user, :with_confirmation_sent_date,
-                          confirmation_sent_date: confirmation_sent_date,
-                          current_sign_in_at: 7.months.ago)
-
-        expect(user.confirmed_at).to be_nil
-        expect(user.confirmation_sent_at).to eq(confirmation_sent_date)
-
-        expect(UserCleaner.new.inactive_users.count).to eq(expected_inactive_users_count)
-      end
-
-      context "when registration was recently" do
-        it "does not count user as inactive regardless of value of last_sign_in_date" do
-          test_non_confirmed_user(5.days.ago, 0)
-        end
-      end
-
-      context "when registration was long ago" do
-        it "counts users as inactive regardless of value of last_sign_in_date" do
-          test_non_confirmed_user(7.months.ago, 3)
-        end
-      end
       it "does not count users with current_sign_in_at date younger than threshold as inactive" do
         FactoryBot.create(:confirmed_user, current_sign_in_at: 5.months.ago)
         expect(UserCleaner.new.inactive_users.count).to eq(0)
@@ -134,19 +83,13 @@ RSpec.describe(UserCleaner, type: :model) do
         inactive_user2 = FactoryBot.create(:user, :with_confirmation_sent_date,
                                            confirmation_sent_date: 7.months.ago)
         active_user = FactoryBot.create(:confirmed_user, current_sign_in_at: 5.months.ago)
-        inactive_user = FactoryBot.create(:confirmed_user, current_sign_in_at: 7.months.ago)
-        inactive_user2 = FactoryBot.create(:user, :with_confirmation_sent_date,
-                                           confirmation_sent_date: 7.months.ago)
-        active_user = FactoryBot.create(:confirmed_user, current_sign_in_at: 5.months.ago)
 
         UserCleaner.new.set_deletion_date_for_inactive_users
         inactive_user.reload
         inactive_user2.reload
-        inactive_user2.reload
         active_user.reload
 
         expect(inactive_user.deletion_date).to eq(Date.current + 40.days)
-        expect(inactive_user2.deletion_date).to eq(Date.current + 40.days)
         expect(inactive_user2.deletion_date).to eq(Date.current + 40.days)
         expect(active_user.deletion_date).to be_nil
       end
@@ -155,8 +98,6 @@ RSpec.describe(UserCleaner, type: :model) do
         max_deletions = 3
         UserCleaner::MAX_DELETIONS_PER_RUN = max_deletions
 
-        FactoryBot.create_list(:confirmed_user, max_deletions + 2,
-                               current_sign_in_at: 7.months.ago)
         FactoryBot.create_list(:confirmed_user, max_deletions + 2,
                                current_sign_in_at: 7.months.ago)
 
@@ -174,31 +115,18 @@ RSpec.describe(UserCleaner, type: :model) do
         user2 = FactoryBot.create(:user, :with_confirmation_sent_date,
                                   confirmation_sent_date: 7.months.ago,
                                   deletion_date: Date.current + 44.days)
-        user = FactoryBot.create(:confirmed_user, current_sign_in_at: 7.months.ago,
-                                                  deletion_date: Date.current + 42.days)
-        user2 = FactoryBot.create(:user, :with_confirmation_sent_date,
-                                  confirmation_sent_date: 7.months.ago,
-                                  deletion_date: Date.current + 44.days)
 
         UserCleaner.new.set_deletion_date_for_inactive_users
         user.reload
         user2.reload
-        user2.reload
 
         expect(user.deletion_date).to eq(Date.current + 42.days)
-        expect(user2.deletion_date).to eq(Date.current + 44.days)
         expect(user2.deletion_date).to eq(Date.current + 44.days)
       end
     end
 
     it "unassigns a deletion date from recently active users" do
       deletion_date = Date.current + 5.days
-      user_inactive = FactoryBot.create(:confirmed_user, deletion_date: deletion_date,
-                                                         current_sign_in_at: 7.months.ago)
-      user_inactive2 = FactoryBot.create(:confirmed_user, deletion_date: deletion_date,
-                                                          current_sign_in_at: 6.months.ago - 1.day)
-      user_active = FactoryBot.create(:confirmed_user, deletion_date: deletion_date,
-                                                       current_sign_in_at: 2.days.ago)
       user_inactive = FactoryBot.create(:confirmed_user, deletion_date: deletion_date,
                                                          current_sign_in_at: 7.months.ago)
       user_inactive2 = FactoryBot.create(:confirmed_user, deletion_date: deletion_date,
@@ -222,9 +150,6 @@ RSpec.describe(UserCleaner, type: :model) do
       user_past1 = FactoryBot.create(:confirmed_user, deletion_date: Date.current - 1.day)
       user_past2 = FactoryBot.create(:confirmed_user, deletion_date: Date.current - 1.year)
       user_present = FactoryBot.create(:confirmed_user, deletion_date: Date.current)
-      user_past1 = FactoryBot.create(:confirmed_user, deletion_date: Date.current - 1.day)
-      user_past2 = FactoryBot.create(:confirmed_user, deletion_date: Date.current - 1.year)
-      user_present = FactoryBot.create(:confirmed_user, deletion_date: Date.current)
 
       UserCleaner.new.delete_users_according_to_deletion_date!
 
@@ -236,8 +161,6 @@ RSpec.describe(UserCleaner, type: :model) do
     it "does not delete users with a deletion date in the future" do
       user_future1 = FactoryBot.create(:confirmed_user, deletion_date: Date.current + 1.day)
       user_future2 = FactoryBot.create(:confirmed_user, deletion_date: Date.current + 1.year)
-      user_future1 = FactoryBot.create(:confirmed_user, deletion_date: Date.current + 1.day)
-      user_future2 = FactoryBot.create(:confirmed_user, deletion_date: Date.current + 1.year)
 
       UserCleaner.new.delete_users_according_to_deletion_date!
 
@@ -247,13 +170,11 @@ RSpec.describe(UserCleaner, type: :model) do
 
     it "does not delete users without a deletion date" do
       user = FactoryBot.create(:confirmed_user, deletion_date: nil)
-      user = FactoryBot.create(:confirmed_user, deletion_date: nil)
       UserCleaner.new.delete_users_according_to_deletion_date!
       expect(User.where(id: user.id)).to exist
     end
 
     it "deletes only generic users" do
-      user_generic = FactoryBot.create(:confirmed_user, deletion_date: Date.current - 1.day)
       user_generic = FactoryBot.create(:confirmed_user, deletion_date: Date.current - 1.day)
       user_admin
       user_teacher
@@ -271,7 +192,11 @@ RSpec.describe(UserCleaner, type: :model) do
   describe("mails") do
     context "when setting a deletion date" do
       it "enqueues a deletion warning mail (40 days)" do
+<<<<<<< HEAD
+        user = FactoryBot.create(:confirmed_user, current_sign_in_at: 7.months.ago)
+=======
         FactoryBot.create(:confirmed_user, current_sign_in_at: 7.months.ago)
+>>>>>>> 2dc4bd6b (Deal with registration edge cases in UserCleaner (#693) (#716))
 
         expect do
           UserCleaner.new.set_deletion_date_for_inactive_users
@@ -292,7 +217,11 @@ RSpec.describe(UserCleaner, type: :model) do
 
     context "when a deletion date is assigned" do
       def test_enqueues_additional_deletion_warning_mails(num_days)
+<<<<<<< HEAD
+        user = FactoryBot.create(:confirmed_user, deletion_date: Date.current + num_days.days)
+=======
         FactoryBot.create(:confirmed_user, deletion_date: Date.current + num_days.days)
+>>>>>>> 2dc4bd6b (Deal with registration edge cases in UserCleaner (#693) (#716))
 
         expect do
           UserCleaner.new.send_additional_warning_mails
@@ -307,7 +236,6 @@ RSpec.describe(UserCleaner, type: :model) do
       end
 
       it "does not enqueue an additional deletion warning mail for 40 days" do
-        FactoryBot.create(:confirmed_user, deletion_date: Date.current + 40.days)
         FactoryBot.create(:confirmed_user, deletion_date: Date.current + 40.days)
 
         expect do
@@ -328,7 +256,11 @@ RSpec.describe(UserCleaner, type: :model) do
 
     context "when a user is finally deleted" do
       it "enqueues a deletion mail" do
+<<<<<<< HEAD
+        user = FactoryBot.create(:confirmed_user, deletion_date: Date.current - 1.day)
+=======
         FactoryBot.create(:confirmed_user, deletion_date: Date.current - 1.day)
+>>>>>>> 2dc4bd6b (Deal with registration edge cases in UserCleaner (#693) (#716))
 
         expect do
           UserCleaner.new.delete_users_according_to_deletion_date!
@@ -341,12 +273,15 @@ RSpec.describe(UserCleaner, type: :model) do
   describe("#pending_deletion_mail") do
     let(:user_de) { FactoryBot.create(:confirmed_user, locale: "de") }
     let(:user_en) { FactoryBot.create(:confirmed_user, locale: "en") }
-    let(:user_de) { FactoryBot.create(:confirmed_user, locale: "de") }
-    let(:user_en) { FactoryBot.create(:confirmed_user, locale: "en") }
 
     def test_subject_line(num_days)
       user = FactoryBot.create(:confirmed_user)
+<<<<<<< HEAD
+      mailer = UserCleanerMailer
+               .pending_deletion_email(user.email, user.locale, num_days)
+=======
       mailer = UserCleanerMailer.with(user: user).pending_deletion_email(num_days)
+>>>>>>> 2dc4bd6b (Deal with registration edge cases in UserCleaner (#693) (#716))
       expect(mailer.subject).to include(num_days.to_s)
     end
 
@@ -386,8 +321,6 @@ RSpec.describe(UserCleaner, type: :model) do
   end
 
   describe("#deletion_mail") do
-    let(:user_de) { FactoryBot.create(:confirmed_user, locale: "de") }
-    let(:user_en) { FactoryBot.create(:confirmed_user, locale: "en") }
     let(:user_de) { FactoryBot.create(:confirmed_user, locale: "de") }
     let(:user_en) { FactoryBot.create(:confirmed_user, locale: "en") }
 
