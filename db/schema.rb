12ef9b53572773889ebeb54e04d14f64391c7592
Florian <florianbuerckel@gmail.com>
2024-12-15 17:39:54 +0100

    add import statement of trix in application.js
    Signed-off-by: Florian <florianbuerckel@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_12_04_132407) do
+ActiveRecord::Schema[7.1].define(version: 2024_12_15_132228) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -101,6 +101,12 @@ ActiveRecord::Schema[7.1].define(version: 2024_12_04_132407) do
     t.date "deletion_date", default: "2200-01-01", null: false
     t.index ["lecture_id"], name: "index_assignments_on_lecture_id"
     t.index ["medium_id"], name: "index_assignments_on_medium_id"
+  end
+
+  create_table "blogs", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "chapters", force: :cascade do |t|
