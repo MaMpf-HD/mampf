b220ab9f38719e6170cda4c826b5f680088a3663
Florian <florianbuerckel@gmail.com>
2025-01-05 18:12:44 +0100

    ensure correct post request for dynamic form
    Signed-off-by: Florian <florianbuerckel@gmail.com>

--- app/assets/javascripts/vignettes/question_type.js
+++ app/assets/javascripts/vignettes/question_type.js
@@ -1,5 +1,5 @@
 document.addEventListener("turbolinks:load", () => {
-  const questionTypeDropdown = document.getElementById("question_type");
+  const questionTypeDropdown = document.getElementById("question-type");
   if (questionTypeDropdown) {
     questionTypeDropdown.addEventListener("change", function (event) {
       // Disable all fields by default
@@ -17,5 +17,40 @@ document.addEventListener("turbolinks:load", () => {
         }
       }
     });
+
+    questionTypeDropdown.dispatchEvent(new Event("change"));
   };
+  const optionsContainer = document.getElementById("options-container");
+
+  // Handle dynamic addition of options
+  const addOptionButton = document.getElementById("add-option");
+  const optionTemplate = document.getElementById("new-option-template");
+  if (addOptionButton) {
+    addOptionButton.addEventListener("click", (e) => {
+      const optionTemplateHTML = optionTemplate.innerHTML;
+      const uniqueId = new Date().getTime().toString();
+      const newBlockHTML = optionTemplateHTML.replace(/NEW_RECORD/g, uniqueId);
+
+      optionsContainer.insertAdjacentHTML("beforeend", newBlockHTML);
+    });
+  }
+
+  // Handle removal of options
+  if (optionsContainer) {
+    optionsContainer.addEventListener("click", (e) => {
+      console.log(e.target);
+      if (e.target.className == "remove-option") {
+        e.preventDefault();
+        const optionBlock = e.target.parentElement;
+        if (optionBlock.className == "option-field") {
+          const destroyField = optionBlock.querySelector("input[type='hidden'][name*='_destroy']");
+          console.log(destroyField);
+          if (destroyField) {
+            destroyField.value = "1";
+            optionBlock.style.display = "none";
+          }
+        }
+      }
+    });
+  }
 });
