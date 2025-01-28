2d929a63f78789be433ac8ff7537f9960179cfa2
Splines <dominic-plein@gmx.de>
2025-01-28 16:35:38 +0100

    Remove custom filename for test-ci & specify NODE_ENV correctly

--- config/webpack/test-ci.js
+++ config/webpack/test-ci.js
@@ -2,5 +2,5 @@ process.env.NODE_ENV = process.env.NODE_ENV || "test-ci";
 
 const environment = require("./environment");
 const config = environment.toWebpackConfig();
-config.output.filename = "js/[name]-[hash].js";
+
 module.exports = config;
