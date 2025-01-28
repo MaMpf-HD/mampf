a24bf0befeb9011bec523762044b4b20d74f61b1
Splines <dominic-plein@gmx.de>
2025-01-28 14:30:50 +0100

    Try to manually set compile to false in webpacker test config

--- config/webpack/test.js
+++ config/webpack/test.js
@@ -1,6 +1,11 @@
 process.env.NODE_ENV = process.env.NODE_ENV || "development";
 
 const environment = require("./environment");
+
+if (process.env.CI) {
+  environment.config.set("compile", false);
+}
+
 const config = environment.toWebpackConfig();
 config.output.filename = "js/[name]-[hash].js";
 module.exports = config;
