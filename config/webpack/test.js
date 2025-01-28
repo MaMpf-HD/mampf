46ac47c755e53938ac27ca6648dbf95c304c3979
Splines <dominic-plein@gmx.de>
2025-01-28 14:46:16 +0100

    Set compile to false manually

--- config/webpack/test.js
+++ config/webpack/test.js
@@ -1,15 +1,6 @@
 process.env.NODE_ENV = process.env.NODE_ENV || "development";
 
 const environment = require("./environment");
-
-if (process.env.CI) {
-  environment.config.set("compile", false);
-  console.log("In CI/CD environment, we precompile assets in the build step");
-}
-else {
-  console.log("In test environment, we don't precompile assets");
-}
-
 const config = environment.toWebpackConfig();
 config.output.filename = "js/[name]-[hash].js";
 module.exports = config;
