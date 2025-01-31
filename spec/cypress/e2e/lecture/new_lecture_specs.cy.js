ce8111c804c944012b9e2b4e10b3e78c05188152
Splines <dominic-plein@gmx.de>
2025-01-31 17:38:22 +0100

    Outsource Tom Select to new Cypress Command
    Also use random names for the course

--- spec/cypress/e2e/lecture/new_lecture_specs.cy.js
+++ spec/cypress/e2e/lecture/new_lecture_specs.cy.js
@@ -3,7 +3,8 @@ import FactoryBot from "../../support/factorybot";
 describe("New lecture (via admin index page)", () => {
   beforeEach(function () {
     cy.createUserAndLogin("admin").as("admin");
-    FactoryBot.create("course", "with_division", { title: "Curves" }).as("course");
+
+    FactoryBot.create("course").as("course");
     cy.then(() => {
       FactoryBot.create("term").as("term");
     });
@@ -13,19 +14,9 @@ describe("New lecture (via admin index page)", () => {
     cy.visit("/administration");
     cy.getBySelector("new-lecture-button-admin-index").click();
 
-    cy.getBySelector("new-lecture-course-select-div").as("wrapperDiv");
-
-    cy.then(() => {
-      cy.get("@wrapperDiv").find(".ts-control").find("input").click();
-      const name = "Curves";
-      cy.get("@wrapperDiv").find("select").find("option").contains(name).should("exist");
-      cy.get("div.ts-dropdown").contains(name).as("dropdownItem");
-      cy.then(() => {
-        cy.get("@dropdownItem").should("have.attr", "data-selectable");
-        cy.get("@dropdownItem").click();
-      });
+    cy.getBySelector("new-lecture-course-select-div").then(($wrapperDiv) => {
+      cy.wrap($wrapperDiv).selectTom(this.course.title);
     });
-
     cy.getBySelector("new-lecture-submit").click();
 
     const successMessage = this.admin.locale === "de" ? "erfolgreich" : "successfully";
