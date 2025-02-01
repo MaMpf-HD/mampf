25cebc2364e9f06e7a2900ec576175300083892c
Splines <dominic-plein@gmx.de>
2025-02-01 03:58:05 +0100

    Unify test-ci and test

--- config/webpack/test-ci.js
+++
@@ -1,6 +0,0 @@
-process.env.NODE_ENV = process.env.NODE_ENV || "test-ci";
-
-const environment = require("./environment");
-const config = environment.toWebpackConfig();
-
-module.exports = config;
--- config/webpack/test.js
+++ config/webpack/test.js
@@ -1,7 +1,14 @@
-// use the dev server setup for local testing
-process.env.NODE_ENV = "docker_development";
+// https://github.com/rails/webpacker/issues/2488
+// https://github.com/rails/webpacker/blob/5-x-stable/docs/webpack.md
+// https://github.com/rails/webpacker/issues/2654#issuecomment-660974489
 
 const environment = require("./environment");
 const config = environment.toWebpackConfig();
-config.output.filename = "js/[name]-[hash].js";
+
+// locally, we want to use the dev server setup
+const in_ci = process.env.IN_CI_CD_WEBPACKER === "true";
+if (!in_ci) {
+  config.output.filename = "js/[name]-[hash].js";
+}
+
 module.exports = config;
