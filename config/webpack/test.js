03af6f7b7d9059bbc719ec0b6984097c056e3149
Splines <dominic-plein@gmx.de>
2025-02-01 04:22:47 +0100

    Use [name]-[hash] pattern also in CI and set NODE_ENV

--- config/webpack/test.js
+++ config/webpack/test.js
@@ -1,14 +1,7 @@
 // https://github.com/rails/webpacker/issues/2488
 // https://github.com/rails/webpacker/blob/5-x-stable/docs/webpack.md
 // https://github.com/rails/webpacker/issues/2654#issuecomment-660974489
-
 const environment = require("./environment");
 const config = environment.toWebpackConfig();
-
-// locally, we want to use the dev server setup
-const in_ci = process.env.IN_CI_CD_WEBPACKER === "true";
-if (!in_ci) {
-  config.output.filename = "js/[name]-[hash].js";
-}
-
+config.output.filename = "js/[name]-[hash].js";
 module.exports = config;
