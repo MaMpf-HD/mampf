4382a8b2a90b4031cdcb4890fa5b358267fa5d2b
fosterfarrell9 <28628554+fosterfarrell9@users.noreply.github.com>
2025-02-22 20:42:46 +0100

    Add csv gem in order to silence deprecation warning

--- Gemfile
+++ Gemfile
@@ -19,6 +19,7 @@ gem "clipboard-rails", "~> 1.7"
 gem "coffee-rails", "~> 5.0" # CoffeeScript for .coffee assets and views
 gem "commontator", "~> 7.0.1"
 gem "coveralls", "~> 0.7", require: false
+gem "csv", "~> 3.3"
 gem "dalli", "~> 3.2" # caching to memcached in production
 gem "devise", "~> 4.9"
 gem "devise-bootstrap-views", "~> 1.1"
--- app/models/interaction.rb
+++ app/models/interaction.rb
@@ -2,7 +2,6 @@ class Interaction < InteractionsRecord
   scope :created_between, lambda { |start_date, end_date|
                             where(created_at: start_date.beginning_of_day..end_date.end_of_day)
                           }
-  require "csv"
 
   def self.to_csv
     attributes = ["id", "session_id", "created_at", "full_path", "referrer_url",
