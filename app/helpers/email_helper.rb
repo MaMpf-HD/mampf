7e14a3c09524f10b54cb889d7307595017a7c0be
fosterfarrell9 <28628554+fosterfarrell9@users.noreply.github.com>
2025-02-22 22:01:35 +0100

    Enforce Rubocop rules

--- app/helpers/email_helper.rb
+++ app/helpers/email_helper.rb
@@ -1,6 +1,6 @@
 module EmailHelper
-  def email_image_tag(image, **options)
+  def email_image_tag(image, **)
     attachments.inline[image] = Rails.root.join("public/#{image}").read
-    image_tag(attachments[image].url, **options)
+    image_tag(attachments[image].url, **)
   end
 end
