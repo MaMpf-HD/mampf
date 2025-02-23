debbb4a9f8c82523f32ae3117ab038abc204268a
Splines <dominic-plein@gmx.de>
2025-02-23 17:27:09 +0100

    Use object-oriented model for slide statistics

--- app/assets/javascripts/vignettes/take_questionnaire.js
+++ app/assets/javascripts/vignettes/take_questionnaire.js
@@ -1,16 +1,19 @@
+var VIGNETTE_FORM_ID = "#vignettes-answer-form";
+function shouldRegisterVignette() {
+  return $(VIGNETTE_FORM_ID).length > 0;
+}
+
 $(document).on("turbolinks:load", () => {
   if (!shouldRegisterVignette()) {
     return;
   }
+
   registerSubmitHandler();
   registerTextAnswerValidator();
+
+  const stats = new VignetteSlideStatistics();
+  registerStatisticsHandler(stats);
 });
-
-var VIGNETTE_FORM_ID = "#vignettes-answer-form";
-
-function shouldRegisterVignette() {
-  return $(VIGNETTE_FORM_ID).length > 0;
-}
 
 function registerSubmitHandler() {
   $(VIGNETTE_FORM_ID).submit((event) => {
@@ -64,84 +67,107 @@ function validateTextAnswer() {
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-// TODO
+// Statistics
 ////////////////////////////////////////////////////////////////////////////////
 
-document.addEventListener("turbolinks:load", () => {
-  const infoSlideButtons = document.querySelectorAll(".info-slide-button");
-  const closeInfoSlideButtons = document.querySelectorAll(".close-info-slide-button");
-  const infoSlideContainers = document.querySelectorAll(".info-slide-container");
-  const slideContainer = document.getElementById("current-slide-container");
+class VignetteSlideStatistics {
+  slideStartTime = new Date();
+  totalSlideTime = 0;
 
-  const timeOnSlideField = document.getElementById("time-on-slide-field");
-  const timeOnInfoSlidesField = document.getElementById("time-on-info-slides-field");
-  const infoSlidesAccessCountField = document.getElementById("info-slides-access-count-field");
+  infoSlideAccessCounts = {};
+  infoSlideStartTime = null;
+  infoSlideTimes = {};
 
-  const form = document.querySelector("form");
+  increaseInfoSlideAccessCount(index) {
+    this.infoSlideAccessCounts[index] ??= 0;
+    this.infoSlideAccessCounts[index]++;
+  }
 
-  let SlideStartTime = new Date();
-  let InfoSlideStartTime = null;
+  freezeSlideTime() {
+    if (this.slideStartTime === null) {
+      console.error("Attempted to freeze slide time when it was already frozen");
+      return;
+    }
+    this.totalSlideTime += (Date.now() - this.slideStartTime);
+    this.slideStartTime = null;
+  }
 
-  var activeInfoSlideIndex = null;
+  unfreezeSlideTime() {
+    this.slideStartTime = Date.now();
+  }
 
-  // Time in seconds
-  let totalSlideTime = 0;
-  let infoSlideTimes = {};
+  startInfoSlideTimer() {
+    this.infoSlideStartTime = Date.now();
+  }
 
-  let infoSlidesAccessCount = {};
+  stopInfoSlideTimer(index) {
+    if (this.infoSlideStartTime === null) {
+      console.error("Attempted to stop info slide timer when it was already stopped");
+      return;
+    }
+    const timeOnInfoSlide = (Date.now() - this.infoSlideStartTime);
+    this.infoSlideTimes[index] = (this.infoSlideTimes[index] || 0.0) + timeOnInfoSlide;
+    this.infoSlideStartTime = null;
+  }
 
-  infoSlideButtons.forEach((button) => {
-    button.addEventListener("click", () => {
-      const index = button.getAttribute("data-index");
-      activeInfoSlideIndex = index;
+  postProcessTimes() {
+    for (let key in this.infoSlideTimes) {
+      this.infoSlideTimes[key] = Math.floor(this.infoSlideTimes[key] / 1000);
+    }
+    this.totalSlideTime = Math.floor(this.totalSlideTime / 1000);
+  }
+}
 
-      // Hide all info slides and the slide
-      infoSlideContainers.forEach(container => container.style.display = "none");
-      slideContainer.style.display = "none";
-      // Make selected info slide visible
-      document.getElementById(`info-slide-container-${index}`).style.display = "block";
-
-      // Increase access count on selected info slide
-      infoSlidesAccessCount[index] = (infoSlidesAccessCount[index] || 0) + 1;
-
-      totalSlideTime += (Date.now() - SlideStartTime);
-      SlideStartTime = null;
-
-      InfoSlideStartTime = Date.now();
+function registerStatisticsHandler(stats) {
+  // Info Slide - Opening
+  const openInfoSlideButtons = $(".open-info-slide-btn");
+  if (!openInfoSlideButtons) {
+    return;
+  }
+  openInfoSlideButtons.each(function () {
+    const index = $(this).attr("data-info-slide-index");
+    $(this).click(() => {
+      stats.freezeSlideTime();
+      stats.increaseInfoSlideAccessCount(index);
+      stats.startInfoSlideTimer();
     });
   });
 
-  closeInfoSlideButtons.forEach((button) => {
-    button.addEventListener("click", () => {
-      activeInfoSlideIndex = null;
-      const index = button.getAttribute("data-index");
-      document.getElementById(`info-slide-container-${index}`).style.display = "none";
-      slideContainer.style.display = "block";
-
-      const timeSpentOnInfoSlide = (Date.now() - InfoSlideStartTime);
-      infoSlideTimes[index] = (timeSpentOnInfoSlide[index] || 0) + timeSpentOnInfoSlide;
-      InfoSlideStartTime = null;
-
-      SlideStartTime = Date.now();
+  // Info Slide - Closing
+  const infoSlideModals = $(".vignette-info-slide-modal");
+  if (!infoSlideModals) {
+    console.error("No info slide modals found");
+    return;
+  }
+  infoSlideModals.each(function () {
+    $(this).on("hidden.bs.modal", function () {
+      const index = $(this).attr("data-info-slide-index");
+      console.log(`Modal with index ${index} closed`);
+      stats.stopInfoSlideTimer(index);
+      stats.unfreezeSlideTime();
     });
   });
 
-  form.addEventListener("submit", () => {
-    event.preventDefault();
-  });
+  // Form Submission
+  $(VIGNETTE_FORM_ID).submit(() => {
+    // Take rest of time into account
+    if (stats.slideStartTime) {
+      stats.freezeSlideTime();
+    }
 
-  $(document).on("submit", VIGNETTE_FORM_ID, () => {
-    if (SlideStartTime) {
-      totalSlideTime += (Date.now() - SlideStartTime);
-    }
-    else if (activeInfoSlideIndex) {
-      infoSlideTimes[activeInfoSlideIndex] += (Date.now() - InfoSlideStartTime);
-    }
-    for (let key in infoSlideTimes) {
-      infoSlideTimes[key] = Math.floor(infoSlideTimes[key] / 1000);
-    }
-    timeOnSlideField.value = Math.floor(totalSlideTime / 1000);
-    timeOnInfoSlidesField.value = JSON.stringify(infoSlideTimes);
-    infoSlidesAccessCountField.value = JSON.stringify(infoSlidesAccessCount);
+    stats.postProcessTimes();
+
+    console.log("Vignette statistics:");
+    console.log("Total slide time: " + stats.totalSlideTime);
+    console.log("Info slide times: " + JSON.stringify(stats.infoSlideTimes));
+    console.log("Info slide access counts: " + JSON.stringify(stats.infoSlideAccessCounts));
+
+    // Transfer results to hidden form-fields
+    const timeOnSlideField = $("#time-on-slide-field");
+    const timeOnInfoSlidesField = $("#time-on-info-slides-field");
+    const infoSlidesAccessCountField = $("#info-slides-access-count-field");
+    timeOnSlideField.value = stats.totalSlideTime;
+    timeOnInfoSlidesField.value = JSON.stringify(stats.infoSlideTimes);
+    infoSlidesAccessCountField.value = JSON.stringify(stats.infoSlideAccessCounts);
   });
-});
+}
