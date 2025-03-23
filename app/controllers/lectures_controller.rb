039a14c14c9f5b859e280a314274ea771507b44e
Florian <florianbuerckel@gmail.com>
2025-03-23 13:30:16 +0100

    prevent site leaving with unsaved changes

--- app/controllers/lectures_controller.rb
+++ app/controllers/lectures_controller.rb
@@ -27,7 +27,7 @@ class LecturesController < ApplicationController
                                                   &.last_seen_at || @lecture.updated_at,
                               @lecture.forum&.updated_at || @lecture.updated_at].max)
       @lecture = Lecture.includes(:teacher, :term, :editors, :users,
-                                  :vignettes, :announcements, :imported_media,
+                                  :vignettes_questionnaires, :announcements, :imported_media,
                                   course: [:editors],
                                   media: [:teachable, :tags],
                                   lessons: [media: [:tags]],
