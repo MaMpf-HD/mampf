f3983964e621ac7bba5420023afd43a85089025f
Florian <florianbuerckel@gmail.com>
2025-03-28 00:40:23 +0100

    synchronize date of vignettes migrations

--- db/migrate/20250325195638_add_visible_to_vignettes_questionnaires.rb
+++
@@ -1,5 +0,0 @@
-class AddVisibleToVignettesQuestionnaires < ActiveRecord::Migration[7.1]
-  def change
-    add_column :vignettes_questionnaires, :editable, :boolean, default: true
-  end
-end
--- db/migrate/20250327121828_add_total_slide_time_to_vignettes_slide_statistics.rb
+++
@@ -1,5 +0,0 @@
-class AddTotalSlideTimeToVignettesSlideStatistics < ActiveRecord::Migration[7.1]
-  def change
-    add_column :vignettes_slide_statistics, :total_time_on_slide, :integer
-  end
-end
