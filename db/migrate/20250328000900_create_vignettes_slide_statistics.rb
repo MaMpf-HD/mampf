0d00539782da06ce80e265e805a2f669fd380572
Florian <florianbuerckel@gmail.com>
2025-03-28 01:17:14 +0100

    add total_time_on_slide to create migration

--- db/migrate/20250328000900_create_vignettes_slide_statistics.rb
+++ db/migrate/20250328000900_create_vignettes_slide_statistics.rb
@@ -4,6 +4,7 @@ class CreateVignettesSlideStatistics < ActiveRecord::Migration[7.1]
       t.references :user, null: false, foreign_key: true
       t.references :vignettes_answer, foreign_key: { to_table: :vignettes_answers }
       t.integer :time_on_slide
+      t.integer :total_time_on_slide
       t.text :time_on_info_slides
       t.text :info_slides_access_count
       t.text :info_slides_first_access_time
