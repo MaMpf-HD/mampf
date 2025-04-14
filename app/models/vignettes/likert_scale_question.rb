619d113d6ad322f8b97b6b264296987a6b2a5c58
Florian <florianbuerckel@gmail.com>
2025-04-14 12:20:00 +0200

    change likert labels

--- app/models/vignettes/likert_scale_question.rb
+++ app/models/vignettes/likert_scale_question.rb
@@ -19,22 +19,22 @@ module Vignettes
     end
 
     LIKERT_LABELS_EN = {
-      strongly_disagree: "Not at all",
-      disagree: "very little",
-      agree: "somewhat",
-      strongly_agree: "To greater extent"
+      strongly_disagree: "No alignment at all",
+      disagree: "Little alignment",
+      agree: "Quite some alignment",
+      strongly_agree: "Complete alignment"
     }.freeze
     LIKERT_LABELS_DE = {
-      strongly_disagree: "Überhaupt nicht",
-      disagree: "Sehr wenig",
-      agree: "Etwas",
-      strongly_agree: "In größerem Umfang"
+      strongly_disagree: "Überhaupt keine Übereinstimmung",
+      disagree: "Geringe Übereinstimmung",
+      agree: "Ziemlich große Übereinstimmung",
+      strongly_agree: "Vollständige Übereinstimmung"
     }.freeze
     LIKERT_LABELS_NL = {
-      strongly_disagree: "helmaal niet",
-      disagree: "zeer weinig",
-      agree: "enigszins",
-      strongly_agree: "in grotere mate"
+      strongly_disagree: "Helemaal geen overeenstemming",
+      disagree: "Weinig overeenstemming",
+      agree: "Wogal wat overeenstemming",
+      strongly_agree: "Volledige overeenstemming"
     }.freeze
   end
 end
