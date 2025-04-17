08d8ac1d139a7088ba8f13f50f54ae3146a7f7d0
Florian <florianbuerckel@gmail.com>
2025-04-17 15:00:43 +0200

    delete more empty spec files

--- spec/factories/multiple_choice_answers.rb
+++
@@ -1,5 +0,0 @@
-FactoryBot.define do
-  factory :multiple_choice_answer do
-    return
-  end
-end
--- spec/factories/options.rb
+++
@@ -1,6 +0,0 @@
-FactoryBot.define do
-  factory :option do
-    text { "MyString" }
-    question { nil }
-  end
-end
