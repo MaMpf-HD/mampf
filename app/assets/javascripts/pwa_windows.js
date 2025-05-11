369bd3e28bd47ecfa347eb9cebaac460f648db11
Splines <info@splines.me>
2025-05-11 02:44:26 +0200

    Add docstring to custom link handler

--- app/assets/javascripts/pwa_windows.js
+++ app/assets/javascripts/pwa_windows.js
@@ -1,8 +1,21 @@
-// Add global logic for handling anchor tags with target="_blank" using jQuery and event delegation
-// https://stackoverflow.com/q/68993320/
-// https://web.dev/learn/pwa/windows#open_new_windows
-// https://github.com/WICG/manifest-incubations/blob/gh-pages/tabbed-mode-explainer.md
-document.addEventListener("turbolinks:load", function () {
+/**
+ * Opens any link with target="_blank" in a new window when the app is running
+ * as a Progressive Web App (PWA).
+ *
+ * Without this, on Desktop clients, these links would instead open in the
+ * default browser, and thus the user would be taken out of the MaMpf app.
+ *
+ * Notice that on mobile devices, this opening of new windows might not work.
+ * Oftentimes, a dummy in-app browser is opened instead, which is also fine.
+ *
+ * Related:
+ * - Original problem: https://stackoverflow.com/q/68993320/
+ * - Workaround with new in-app window (what we use here):
+ *   https://web.dev/learn/pwa/windows#open_new_windows
+ * - Other workaround using "tabbed" mode, but that one is currently experimental:
+ *   https://github.com/WICG/manifest-incubations/blob/gh-pages/tabbed-mode-explainer.md
+ */
+$(document).on("turbolinks:load", function () {
   $(document).on("click", "a[target='_blank']", function (event) {
     const url = $(this).attr("href");
     const isPWA = window.matchMedia("(display-mode: standalone)").matches
@@ -15,6 +28,6 @@ document.addEventListener("turbolinks:load", function () {
     event.preventDefault();
     const width = window.screen.width / 2;
     const height = window.screen.height / 2;
-    window.open(url, "MaMpf external", `width=${width},height=${height}`);
+    window.open(url, "MaMpf Media View", `width=${width},height=${height}`);
   });
 });
