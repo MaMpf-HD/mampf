56528a4a68b9952f22afe73a9faa06b0eb11d1f1
Splines <info@splines.me>
2025-05-21 15:37:04 +0200

    Reset module type back to nil

--- config/initializers/js_routes.rb
+++ config/initializers/js_routes.rb
@@ -2,6 +2,6 @@
 # Once we update our asset pipeline, update the js-routes config here accordingly.
 # https://github.com/railsware/js-routes/tree/main?tab=readme-ov-file#sprockets-deprecated
 JsRoutes.setup do |config|
-  config.module_type = :umd
+  config.module_type = nil
   config.namespace = "Routes"
 end
