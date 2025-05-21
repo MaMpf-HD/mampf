61394c5ba804a48c05877c949cff1a4b98d8bc1e
Splines <info@splines.me>
2025-05-21 15:35:56 +0200

    Upgrade js-routes to v2.3.5 (not working yet)
    See issue https://github.com/railsware/js-routes/issues/332

---
+++ config/initializers/js_routes.rb
@@ -0,0 +1,7 @@
+# TODO: This is the config for the deprecated Sprockets configuration.
+# Once we update our asset pipeline, update the js-routes config here accordingly.
+# https://github.com/railsware/js-routes/tree/main?tab=readme-ov-file#sprockets-deprecated
+JsRoutes.setup do |config|
+  config.module_type = :umd
+  config.namespace = "Routes"
+end
