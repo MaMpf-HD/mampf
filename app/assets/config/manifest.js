c8567a517d155e61f94be8d9de3f0592b1e6faac
Splines <info@splines.me>
2025-07-06 19:00:40 +0200

    Remove Sprockets (first migration step)
    See https://github.com/rails/propshaft/blob/main/UPGRADING.md#3-migrate-from-sprockets-to-propshaft
    
    Did not yet replace all asset_helpers (`image_url`, `font_url`) with standard `url`s.
    
    And did also not include Propshaft since I want to see what Vite can do
    for us first. Maybe we don't even need Propshaft after all?

--- app/assets/config/manifest.js
+++
@@ -1,8 +0,0 @@
-//= link_tree ../images
-//= link_directory ../javascripts/vignettes .js
-//= link_directory ../javascripts .js
-//= link_directory ../stylesheets .css
-//= link commontator/manifest.js
-//= link thredded_katex.css
-//= link monotile/geometry.js
-//= link monotile/hat.js
