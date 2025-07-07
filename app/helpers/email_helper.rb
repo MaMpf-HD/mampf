4da0215b02a4be2e2f7b217697e831ba3ec7c6b2
Splines <info@splines.me>
2025-07-07 04:43:08 +0200

    Use vite_image_tag instead of image_tag & correct paths
    This has to be revised again as I'm not sure yet how vite_image_tag
    deals with absolute URLs passed to it.
    
    For commontator, we have to leave `image_tag`, such that the
    sprockets assets pipeline is used.

--- app/helpers/email_helper.rb
+++ app/helpers/email_helper.rb
@@ -1,6 +1,6 @@
 module EmailHelper
   def email_image_tag(image, **)
     attachments.inline[image] = Rails.root.join("public/#{image}").read
-    image_tag(attachments[image].url, **)
+    vite_image_tag(attachments[image].url, **)
   end
 end
