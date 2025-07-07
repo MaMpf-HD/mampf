e20ba8a8a9a3ae0663e98e1253a8470305aca1e3
Splines <info@splines.me>
2025-07-07 20:19:10 +0200

    Remove console.logs()

--- app/frontend/entrypoints/application.js
+++ app/frontend/entrypoints/application.js
@@ -1,20 +1,3 @@
-console.log("Vite ⚡️ Rails");
-console.log("Visit the guide for more information: ", "https://vite-ruby.netlify.app/guide/rails");
-
-// Example: Load Rails libraries in Vite.
-//
-// import * as Turbo from '@hotwired/turbo'
-// Turbo.start()
-//
-// import ActiveStorage from '@rails/activestorage'
-// ActiveStorage.start()
-//
-// // Import all channels.
-// const channels = import.meta.globEager('./**/*_channel.js')
-
-// Example: Import a stylesheet in app/frontend/index.css
-// import '~/index.css'
-
 import "~/entrypoints/jQueryGlobal";
 
 // Bootstrap must be imported AFTER jQuery for bootstrap jQuery plugins to work,
--- app/frontend/entrypoints/jQueryGlobal.js
+++ app/frontend/entrypoints/jQueryGlobal.js
@@ -1,3 +1,2 @@
 import jQuery from "jquery";
 Object.assign(window, { $: jQuery, jQuery });
-console.log("jQuery has been assigned to window.$ and window.jQuery");
