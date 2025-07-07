8361787201eddaca719622f5281b40bcf6d8409c
Splines <info@splines.me>
2025-07-07 19:44:30 +0200

    Setup js_routes for use with Vite

--- config/vite.rb
+++ config/vite.rb
@@ -2,6 +2,12 @@
 # which references:
 # https://github.com/ElMassimo/vite_ruby/discussions/159#discussioncomment-1992049
 
+puts "Recompiling js-routes before starting Vite Dev Server" # rubocop:disable Rails/Output
+require "rake"
+require_relative "../config/application"
+Rails.application.load_tasks
+Rake::Task["js:routes"].invoke
+
 ViteRuby.env["GEM_PATHS"] = Gem.loaded_specs.to_h do |name, gem|
   ["gems/#{name}", gem.full_gem_path]
 end.to_json
