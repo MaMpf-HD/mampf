8555f2078a2dca8b660abc20d1068b19d6ab89df
Splines <info@splines.me>
2025-07-09 19:57:57 +0200

    Fix RuboCop lint errors

--- app/helpers/talks_helper.rb
+++ app/helpers/talks_helper.rb
@@ -18,7 +18,7 @@ module TalksHelper
   end
 
   def speaker_icon_class(talk)
-    return "bi bi-person-fill" unless talk.speakers.count > 1
+    return "bi bi-person-fill" unless talk.speakers.many?
 
     "bi bi-people-fill"
   end
