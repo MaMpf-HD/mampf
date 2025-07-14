33214938ca67ec4bc8f7ee330d73b061dbf68158
fosterfarrell9 <28628554+fosterfarrell9@users.noreply.github.com>
2025-07-14 18:21:17 +0200

    Update Interactions Schema to Rails 8 (#812)

--- db/interactions_schema.rb
+++ db/interactions_schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2020_06_13_112557) do
+ActiveRecord::Schema[8.0].define(version: 2020_06_13_112557) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension "pg_catalog.plpgsql"
 
   create_table "consumptions", force: :cascade do |t|
     t.integer "medium_id"
