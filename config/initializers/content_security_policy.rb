4361f1c8b7b71850d78bfce63d568a86d6999133
Splines <info@splines.me>
2025-07-16 16:48:33 +0200

    Fix line too long

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -13,7 +13,10 @@
 #     policy.script_src  :self, :https
 
 # Allow @vite/client to hot reload javascript changes in development
-# policy.script_src *policy.script_src, :unsafe_eval, "http://#{ ViteRuby.config.host_with_port }" if Rails.env.development?
+# if Rails.env.development?
+#   policy.script_src(*policy.script_src, :unsafe_eval,
+#                     "http://#{ViteRuby.config.host_with_port}")
+# end
 
 # You may need to enable this in production as well depending on your setup.
 # policy.script_src *policy.script_src, :blob if Rails.env.test?
