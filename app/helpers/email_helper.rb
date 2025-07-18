95c8b130a3a4b0a5ed6431551b99f2eecea63e2e
Splines <info@splines.me>
2025-07-18 16:48:26 +0200

    Don't use vite_image_tag for email attachment urls

--- app/helpers/email_helper.rb
+++ app/helpers/email_helper.rb
@@ -1,6 +1,6 @@
 module EmailHelper
   def email_image_tag(image, **)
     attachments.inline[image] = Rails.root.join("public/#{image}").read
-    vite_image_tag(attachments[image].url, **)
+    image_tag(attachments[image].url, **)
   end
 end
