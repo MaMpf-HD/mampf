0189d8084c97dec6338960f622dc11dfb22d8d03
fosterfarrell9 <28628554+fosterfarrell9@users.noreply.github.com>
2025-07-25 13:00:11 +0200

    Implement tag search functionality with course and fulltext filters

--- app/models/concerns/filterable_by_fulltext.rb
+++ app/models/concerns/filterable_by_fulltext.rb
@@ -1,14 +1,19 @@
 module FilterableByFulltext
   extend ActiveSupport::Concern
 
+  included do
+    class_attribute :fulltext_parameter, default: :fulltext
+  end
+
   class_methods do
     private
 
       # Assumes the model has a pg_search_scope named :search_by_title
       def apply_fulltext_filter(scope, params)
-        return scope if params[:fulltext].blank?
+        search_term = params[fulltext_parameter]
+        return scope if search_term.blank?
 
-        scope.search_by_title(params[:fulltext]).with_pg_search_rank
+        scope.search_by_title(search_term).with_pg_search_rank
       end
   end
 end
