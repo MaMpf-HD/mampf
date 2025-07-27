f4539c5a7956fc19763b0f66efc3166585005dba
fosterfarrell9 <28628554+fosterfarrell9@users.noreply.github.com>
2025-07-27 10:47:14 +0200

    Refactor apply_ordering method in ModelSearch for clarity and efficiency

--- app/search/model_search.rb
+++ app/search/model_search.rb
@@ -26,13 +26,20 @@ class ModelSearch
       # if fulltext is given then pg_search's .with_pg_search_rank already added
       # the order by rank
       return scope if @fulltext_param && params[@fulltext_param].present?
+      # Exit early if the model doesn't define a default order
+      return scope unless model_class.respond_to?(:default_search_order)
 
-      if model_class.respond_to?(:default_search_order)
-        order_expression = model_class.default_search_order
-        return scope.order(order_expression) if order_expression.present?
+      order_expression = model_class.default_search_order
+      # Exit early if the default order expression is blank
+      return scope if order_expression.blank?
+
+      # If a distinct clause is present, we must also select the order expression
+      # to avoid a PG::InvalidColumnReference error.
+      if scope.distinct_value
+        scope = scope.select(model_class.arel_table[Arel.star],
+                             order_expression)
       end
 
-      # If no other ordering was applied, return the original scope
-      scope
+      scope.order(order_expression)
     end
 end
