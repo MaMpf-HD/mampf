d72ddb7be6a514968ec3421ae5f0204af6c73eeb
Splines <info@splines.me>
2025-08-03 11:22:15 +0200

    Rework migration timestamps

--- db/interactions_schema.rb
+++ db/interactions_schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_20_124006) do
+ActiveRecord::Schema[8.0].define(version: 2025_08_03_000002) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
