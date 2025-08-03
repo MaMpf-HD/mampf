97f5f7ef1e6c1793d9f428fa065f2bd7d6cf2583
Splines <info@splines.me>
2025-08-03 20:50:55 +0200

    Account for icon fill in sidebar

--- app/frontend/js/lecture_sidebar.controller.js
+++ app/frontend/js/lecture_sidebar.controller.js
@@ -13,13 +13,50 @@ export default class extends Controller {
     this.active_link = null;
   }
 
+  /**
+   * Sets the active link in the sidebar when clicked, but not on the initial
+   * page load.
+   */
   setActive(event) {
+    this.removeActiveStyling();
+    this.removeIconFill();
+    this.setActiveLink(event);
+    this.fillActiveIcon();
+  }
+
+  removeActiveStyling() {
     const navLinks = document.querySelectorAll(".nav-link");
     navLinks.forEach((link) => {
       link.classList.remove(ACTIVE_ITEM_CSS_CLASS);
     });
+  }
 
+  removeIconFill() {
+    const icons = document.querySelectorAll(".nav-link i");
+    icons.forEach((icon) => {
+      const classList = icon.classList;
+      const lastClass = classList[classList.length - 1];
+      if (lastClass.endsWith("-fill")) {
+        classList.remove(lastClass);
+        classList.add(lastClass.replace(/-fill$/, ""));
+      }
+    });
+  }
+
+  setActiveLink(event) {
     this.active_link = event.target.closest("li");
     this.active_link.classList.add(ACTIVE_ITEM_CSS_CLASS);
+  }
+
+  fillActiveIcon() {
+    const icon = this.active_link.querySelector("i");
+    if (!icon) return;
+
+    const classList = icon.classList;
+    const lastClass = classList[classList.length - 1];
+    if (!lastClass.endsWith("-fill")) {
+      classList.remove(lastClass);
+      classList.add(lastClass + "-fill");
+    }
   }
 }
