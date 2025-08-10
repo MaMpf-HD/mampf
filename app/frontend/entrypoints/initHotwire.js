329f91440c421a3dcc68ff82220bf1f001403c3a
Splines <info@splines.me>
2025-08-10 21:05:55 +0200

    Reword turbo fix for content missing

--- app/frontend/entrypoints/initHotwire.js
+++ app/frontend/entrypoints/initHotwire.js
@@ -9,7 +9,5 @@ import "@hotwired/turbo-rails";
 // These two fixes were originally used with Turbolinks.
 // They might not be needed with Turbo anymore.
 import "~/js/_turbo_fix_bootstrap_modal";
-import "~/js/_turbo_fix_selectize";
-// This fixes the missing frame issue when breaking out of a frame
-// See the discussion in https://github.com/hotwired/turbo/issues/257
 import "~/js/_turbo_fix_frame_missing";
+import "~/js/_turbo_fix_selectize";
