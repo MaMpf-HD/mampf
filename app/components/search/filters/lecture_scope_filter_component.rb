5c5d5a8df17a4a9fd72cb079bd5571e92ccd5cea
fosterfarrell9 <28628554+fosterfarrell9@users.noreply.github.com>
2025-08-17 12:53:20 +0200

    Enhance rendering logic and integrate with media search form

--- app/components/search/filters/lecture_scope_filter_component.rb
+++ app/components/search/filters/lecture_scope_filter_component.rb
@@ -1,68 +1,76 @@
 module Search
   module Filters
-    class LectureScopeFilterComponent < ViewComponent::Base
-      attr_reader :form, :context
+    class LectureScopeFilterComponent < Search::MultiSelectComponent
+      def initialize(context: "media", **)
+        super(
+          name: :lectures,
+          label: I18n.t("basics.lectures"),
+          help_text: I18n.t("search.media.lectures"),
+          collection: [], # Will be populated in before_render
+          all_toggle_name: nil, # We don't want the default "All" checkbox
+          column_class: "col-6 col-lg-4",
+          context: context,
+          **
+        )
 
-      def initialize(context: "media")
-        super()
-        @context = context
+        # These options match what's in the original partial
+        options.reverse_merge!(
+          multiple: true,
+          class: "pl-4 selectize",
+          disabled: true,
+          required: true
+        )
       end
 
-      def with_form(form)
-        @form = form
-        self
+      # Load collection just in time (helpers available now)
+      def before_render
+        super
+        @collection = helpers.add_prompt(Lecture.select)
       end
 
-      def call
-        content_tag(:div, class: "col-6 col-lg-4 mb-3 form-group") do
-          concat(form.label(:teachable_ids, I18n.t("basics.lectures"), class: "form-label"))
-          concat(helpers.helpdesk(I18n.t("search.media.lectures"), true))
+      # We don't want the all checkbox for this component
+      def skip_all_checkbox?
+        true
+      end
 
-          # "All" option
-          concat(content_tag(:div, class: "form-check mb-2") do
-            form.radio_button(:lecture_option, "0", checked: true, class: "form-check-input") +
-            form.label(:lecture_option,
-                       I18n.t("search.media.lecture_options.all"),
-                       value: "0",
-                       class: "form-check-label")
-          end)
+      def render_lecture_options
+        content_tag(:div, class: "mt-2") do
+          safe_join([
+                      # "Own selection" option
+                      content_tag(:div, class: "form-check mb-2") do
+                        form.radio_button(:lecture_option, "2",
+                                          class: "form-check-input",
+                                          data: { type: "toggle",
+                                                  id: "search_#{context}_lectures" }) +
+                        form.label(:lecture_option,
+                                   I18n.t("search.media.lecture_options.own_selection"),
+                                   value: "2",
+                                   class: "form-check-label")
+                      end,
 
-          # "Subscribed" option
-          concat(content_tag(:div, class: "form-check mb-2") do
-            form.radio_button(:lecture_option, "1", class: "form-check-input") +
-            form.label(:lecture_option,
-                       I18n.t("search.media.lecture_options.subscribed"),
-                       value: "1",
-                       class: "form-check-label")
-          end)
+                      # "Subscribed" option
+                      content_tag(:div, class: "form-check mb-2") do
+                        form.radio_button(:lecture_option, "1",
+                                          class: "form-check-input") +
+                        form.label(:lecture_option,
+                                   I18n.t("search.media.lecture_options.subscribed"),
+                                   value: "1",
+                                   class: "form-check-label")
+                      end,
 
-          # "Own selection" option
-          concat(content_tag(:div, class: "form-check mb-2") do
-            form.radio_button(:lecture_option, "2",
-                              class: "form-check-input",
-                              data: { type: "toggle", id: "search_#{context}_lectures" }) +
-            form.label(:lecture_option,
-                       I18n.t("search.media.lecture_options.own_selection"),
-                       value: "2",
-                       class: "form-check-label")
-          end)
-
-          # Lecture selection dropdown
-          concat(form.select(:media_lectures,
-                             helpers.options_for_select(add_prompt(Lecture.select)),
-                             {},
-                             { multiple: true,
-                               class: "pl-4 selectize",
-                               disabled: true,
-                               required: true }))
+                      # "All" option
+                      content_tag(:div, class: "form-check mb-2") do
+                        form.radio_button(:lecture_option, "0",
+                                          checked: true,
+                                          class: "form-check-input") +
+                        form.label(:lecture_option,
+                                   I18n.t("search.media.lecture_options.all"),
+                                   value: "0",
+                                   class: "form-check-label")
+                      end
+                    ])
         end
       end
-
-      private
-
-        def add_prompt(collection)
-          [[I18n.t("basics.select"), ""]] + collection
-        end
     end
   end
 end
