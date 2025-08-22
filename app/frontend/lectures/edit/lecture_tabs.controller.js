797c45ff51b44e012ae806b32d94c45f904778d7
Splines <info@splines.me>
2025-08-22 23:53:32 +0200

    Simplify lecture tabs stimulus controller

--- app/frontend/lectures/edit/lecture_tabs.controller.js
+++ app/frontend/lectures/edit/lecture_tabs.controller.js
@@ -7,18 +7,13 @@ export default class extends Controller {
   static targets = ["tabButton"];
 
   connect() {
-    this.configureUrlHashesForBootstrapTabs();
-  }
-
-  configureUrlHashesForBootstrapTabs() {
     this.tabButtonTargets.forEach((tabButton) => {
       tabButton.addEventListener("shown.bs.tab", this.onTabFocus);
     });
   }
 
   onTabFocus(event) {
-    const href = event.currentTarget.getAttribute("href"); // e.g. "#orga"
-    const tabName = href.replace("#", "");
+    const tabName = event.currentTarget.dataset.tabName;
     const url = new URL(window.location);
     url.searchParams.set("tab", tabName);
     history.replaceState({ turbo: true, url: url.toString() }, "", url.toString());
