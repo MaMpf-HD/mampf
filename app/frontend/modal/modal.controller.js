a4f9d357d12cf005e9961c23c2d7c7a68b5f5acf
Splines <info@splines.me>
2025-09-26 12:48:52 +0200

    Hide modal upon successfull submission

--- app/frontend/modal/modal.controller.js
+++ app/frontend/modal/modal.controller.js
@@ -5,9 +5,18 @@ export default class extends Controller {
   connect() {
     this.modal = new Modal(this.element);
     this.modal.show();
+    this.element.addEventListener("turbo:submit-end", () => this.hideModalOnSuccess(event));
   }
 
   disconnect() {
     this.modal.dispose();
+  }
+
+  hideModalOnSuccess(event) {
+    if (!event.detail.success) return;
+
+    if (this.modal) {
+      this.modal.hide();
+    }
   }
 }
