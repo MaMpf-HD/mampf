0f79257de876d6f7c472ce76cc4eb41221db2d0e
Splines <info@splines.me>
2025-09-28 11:22:33 +0200

    Capture event parameter

--- app/frontend/modal/modal.controller.js
+++ app/frontend/modal/modal.controller.js
@@ -4,7 +4,7 @@ import { Modal } from "bootstrap";
 export default class extends Controller {
   connect() {
     this.modal = Modal.getOrCreateInstance(this.element);
-    this.element.addEventListener("turbo:submit-end", () => this.hideModalOnSuccess(event));
+    this.element.addEventListener("turbo:submit-end", event => this.hideModalOnSuccess(event));
   }
 
   disconnect() {
