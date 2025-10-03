e25f55e0dbb7336ace32da1ce48769dccbdcd68d
Splines <info@splines.me>
2025-10-03 18:28:46 +0200

    Import TomSelect as npm package & use for linked info slides

--- app/frontend/vignettes/slides/form/_form.js
+++ app/frontend/vignettes/slides/form/_form.js
@@ -1,5 +1,14 @@
-$(document).ready(function () {
-  new TomSelect("#vignettes-linked-info-slides", {
-    plugins: ["remove_button"],
-  });
+import TomSelect from "tom-select";
+
+$(document).on("turbo:frame-render", function () {
+  document.querySelectorAll(".vignettes-linked-info-slides:not(.ts-wrapper)")
+    .forEach((element) => {
+      if (element.tomselect) {
+        return;
+      }
+
+      new TomSelect(element, {
+        plugins: ["remove_button"],
+      });
+    });
 });
