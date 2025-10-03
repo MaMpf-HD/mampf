6bda43fe6370cd772d84ae9a3e6467b31cff0c0d
fosterfarrell9 <28628554+fosterfarrell9@users.noreply.github.com>
2025-10-03 15:48:58 +0200

    Fix nil check in pagy_bootstrap_nav (#894)

--- app/helpers/pagy_helper.rb
+++ app/helpers/pagy_helper.rb
@@ -2,7 +2,7 @@ module PagyHelper
   # Overrides the default pagy_bootstrap_nav to prevent it from rendering
   # when there is only one page.
   def pagy_bootstrap_nav(pagy, **vars)
-    return if pagy.pages <= 1
+    return if pagy.nil? || pagy.pages <= 1
 
     super
   end
