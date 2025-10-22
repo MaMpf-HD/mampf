dd03408bc347a09ab791b86a18a3149bb94a8825
Splines <37160523+Splines@users.noreply.github.com>
2025-10-22 12:16:14 +0000

    Rework zsh history saving

--- .devcontainer/zsh/.zshrc
+++ .devcontainer/zsh/.zshrc
@@ -4,3 +4,15 @@ alias l='exa'
 alias la='eza -al --group-directories-first'
 alias ll='eza -l --group-directories-first'
 alias ls="eza --group-directories-first"
+
+# zsh history
+HISTFILE=$HOME/commandhistory/.zsh_history
+HISTSIZE=10000
+SAVEHIST=10000
+# write to history file immediately, but don't share between active sessions
+# https://unix.stackexchange.com/questions/389881/history-isnt-preserved-in-zsh#comment858751_470707
+setopt INC_APPEND_HISTORY
+
+# zsh history substring search
+bindkey ';2A' history-substring-search-up    # Shift+Up
+bindkey ';2B' history-substring-search-down  # Shift+Down
--- .devcontainer/zsh/install-zsh.sh
+++ .devcontainer/zsh/install-zsh.sh
@@ -6,6 +6,13 @@ sudo apt update \
     && DEBIAN_FRONTEND=noninteractive sudo apt install -y \
        -o Dpkg::Options::="--force-confnew" zsh
 
+# zsh history (~/commandhistory/ is preserved as a volume in docker-compose)
+# Persist bash history
+# adapted from https://code.visualstudio.com/remote/advancedcontainers/persist-bash-history
+mkdir -p "$HOME/commandhistory"
+touch "$HOME/commandhistory/.zsh_history"
+chown -R "$(id -un):$(id -gn)" "$HOME/commandhistory" || true
+
 # initial zsh config
 cp .devcontainer/zsh/.zshrc ~/.zshrc
 
@@ -33,9 +40,7 @@ echo 'source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh' >> ~/.zshrc
 git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
 echo "source ~/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
 
-# zsh history substring search
+# zsh history substring search (install AFTER syntax highlighting)
 # https://github.com/zsh-users/zsh-history-substring-search
 git clone --depth=1 https://github.com/zsh-users/zsh-history-substring-search.git ~/.zsh/zsh-history-substring-search
 echo "source ~/.zsh/zsh-history-substring-search/zsh-history-substring-search.zsh" >> ~/.zshrc
-echo "bindkey ';2A' history-substring-search-up" >> ~/.zshrc    # Shift+Up
-echo "bindkey ';2B' history-substring-search-down" >> ~/.zshrc  # Shift+Down
