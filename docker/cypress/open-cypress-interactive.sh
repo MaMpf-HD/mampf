05c873897ba4e74f147057c6a8249786883ddfb9
Splines <37160523+Splines@users.noreply.github.com>
2025-10-23 16:28:26 +0000

    Try out Cypress with xpra/x11

---
+++ docker/cypress/open-cypress-interactive.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# set -e
+
+# timeout 1m bash -c "
+#     while ! curl -s $CYPRESS_baseUrl > /dev/null; do
+#         echo waiting for MaMpf to come online at $CYPRESS_baseUrl;
+#         sleep 1;
+#     done
+# ";
+
+# # https://on.cypress.io/command-line#cypress-open
+# # https://www.cypress.io/blog/2019/05/02/run-cypress-with-a-single-docker-command#Interactive-mode
+# RAILS_ENV="test" cypress open --project /mampf-tests/ --e2e --browser chrome
+
+sleep infinity
