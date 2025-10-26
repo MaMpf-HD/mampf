3dd003865d9bb432e8d04223196d9b3c21e43fd4
Splines <37160523+Splines@users.noreply.github.com>
2025-10-26 00:12:40 +0000

    Enable public puma file server for tests

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -15,6 +15,8 @@ Rails.application.configure do
   # Enable server timing.
   config.server_timing = true
 
+  config.public_file_server.enabled = true
+
   # Enable/disable Action Controller caching. By default Action Controller caching is disabled.
   # Run rails dev:cache to toggle Action Controller caching.
   if Rails.root.join("tmp/caching-test.txt").exist?
