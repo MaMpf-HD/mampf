7c6347fc15d0803626e3301b93120ec93e581210
Splines <37160523+Splines@users.noreply.github.com>
2025-10-26 00:29:53 +0000

    Wait for MaMpf also in CI/CD

--- docker/cypress/run-cypress-cicd.sh
+++ docker/cypress/run-cypress-cicd.sh
@@ -1,13 +1,19 @@
 #!/usr/bin/env bash
-# For usage in the CI/CD pipeline (GitHub Actions)
-
 set -e
 
-# timeout 2m bash -c "
-#     while ! curl -s $CYPRESS_baseUrl > /dev/null; do
-#         echo waiting for MaMpf to come online at $CYPRESS_baseUrl;
-#         sleep 1;
-#     done
-# ";
+# For usage in the CI/CD pipeline (GitHub Actions)
+
+# Wait for MaMpf container
+start_app=$SECONDS
+while ! curl -sSf "$CYPRESS_baseUrl" >/dev/null 2>&1; do
+    if [ $((SECONDS - start_app)) -ge 120 ]; then
+        echo "Timeout waiting for MaMpf at $CYPRESS_baseUrl" >&2
+        exit 1
+    fi
+    echo "Waiting (at most 2 minutes) for MaMpf to come online at $CYPRESS_baseUrl ..."
+    sleep 1
+done
+
+echo "✨ Found MaMpf test app running, will now start Cypress"
 
 RAILS_ENV="test" cypress run --project /mampf-tests/ --e2e --browser chrome
