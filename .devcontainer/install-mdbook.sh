021649170b0296f3b053685dd8541b8507eec175
Splines <37160523+Splines@users.noreply.github.com>
2025-10-27 20:04:15 +0000

    Source cargo

--- .devcontainer/install-mdbook.sh
+++ .devcontainer/install-mdbook.sh
@@ -3,6 +3,8 @@ set -e
 
 # Install Rust (https://doc.rust-lang.org/cargo/getting-started/installation.html)
 curl https://sh.rustup.rs -sSf | sh -s -- -y
+# shellcheck source=/dev/null
+. "$HOME/.cargo/env"  
 
 # Install cargo-binstall (https://github.com/cargo-bins/cargo-binstall?tab=readme-ov-file#installation)
 curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
