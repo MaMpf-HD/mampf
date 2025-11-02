42b2baaec6ed800e58f83da2221ce944bd9a8720
Splines <37160523+Splines@users.noreply.github.com>
2025-11-02 19:11:44 +0000

    Rename `app-for-cypress` -> `app-test`

--- vite.config.mjs
+++ vite.config.mjs
@@ -26,7 +26,7 @@ export default defineConfig({
     fs: {
       allow: Object.values(gemPaths),
     },
-    allowedHosts: process.env.NODE_ENV === "test" ? ["app-for-cypress"] : [],
+    allowedHosts: process.env.NODE_ENV === "test" ? ["app-test"] : [],
   },
 
   // TODO (keep track).
