8c98e97bd861d205edd8cfac8c431af92b151ab9
Splines <37160523+Splines@users.noreply.github.com>
2025-11-05 16:29:01 +0000

    Lint Playwright config itself

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig, devices } from "@playwright/test";
 
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: './spec/e2e',
+  testDir: "./spec/_playwright/e2e/",
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -14,38 +14,43 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    baseURL: 'http://localhost:3145',
+    baseURL: "http://app-test:3145",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: "on-first-retry",
   },
 
   /* Configure projects for major browsers */
   projects: [
     {
-      name: 'chromium',
+      // https://playwright.dev/docs/auth#moderate-one-account-per-parallel-worker
+      name: "Clean Database",
+      testMatch: /.*\.clean-database\.spec\.ts/,
+    },
+    {
+      name: "chromium",
       use: {
-        ...devices['Desktop Chrome'],
+        ...devices["Desktop Chrome"],
         // https://dev.to/muhendiskedibey/how-to-full-screen-a-browser-in-playwright-1np1
         deviceScaleFactor: undefined,
         viewport: null,
         launchOptions: {
-          args: ["--start-maximized", "--force-device-scale-factor=1.25"]
+          args: ["--start-maximized", "--force-device-scale-factor=1.25"],
         },
       },
     },
     {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
+      name: "firefox",
+      use: { ...devices["Desktop Firefox"] },
     },
 
     {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
+      name: "webkit",
+      use: { ...devices["Desktop Safari"] },
     },
 
     /* Test against mobile viewports. */
@@ -68,11 +73,4 @@ export default defineConfig({
     //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
     // },
   ],
-
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://localhost:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
 });
