6054287180cfd3021d4b42a72484c02a7ca0958e
Splines <37160523+Splines@users.noreply.github.com>
2025-11-05 20:12:12 +0000

    Opt out of parallel tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,13 +6,13 @@ import { defineConfig, devices } from "@playwright/test";
 export default defineConfig({
   testDir: "./e2e/",
   /* Run tests in files in parallel */
-  fullyParallel: true,
+  fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  /* Opt out of parallel tests. */
+  workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -26,7 +26,6 @@ export default defineConfig({
 
   /* Configure projects for major browsers */
   projects: [
-    { name: "setup", testMatch: /.*\.setup\.ts/ },
     // {
     //   // https://playwright.dev/docs/auth#moderate-one-account-per-parallel-worker
     //   name: "Clean Database",
@@ -43,7 +42,6 @@ export default defineConfig({
           args: ["--start-maximized", "--force-device-scale-factor=1.25"],
         },
       },
-      dependencies: ["setup"],
     },
     // {
     //   name: "firefox",
