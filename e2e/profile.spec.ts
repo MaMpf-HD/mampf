e5f9bfe4922e896cc31f194af6f346f09d7a85ee
Splines <37160523+Splines@users.noreply.github.com>
2025-11-06 20:25:27 +0000

    Ignore case

--- e2e/profile.spec.ts
+++ e2e/profile.spec.ts
@@ -5,13 +5,13 @@ import { expect, test } from "./_support/fixtures";
 test("can see user information", async ({ student }) => {
   await student.page.goto("/profile/edit");
   await expect(student.page.getByText("display name")).toBeVisible();
-  await expect(student.page.getByRole("textbox", { name: "Display name" })).toHaveValue(student.user.name);
+  await expect(student.page.getByRole("textbox", { name: "display name" })).toHaveValue(student.user.name);
 });
 
 test("can see user information (with direct destructuring)", async ({ student: { page, user } }) => {
   await page.goto("/profile/edit");
   await expect(page.getByText("display name")).toBeVisible();
-  await expect(page.getByRole("textbox", { name: "Display name" })).toHaveValue(user.name);
+  await expect(page.getByRole("textbox", { name: "display name" })).toHaveValue(user.name);
 });
 
 test("only admins have admin icon in menu bar", async ({ student: { page: studentPage }, admin: { page: adminPage } }) => {
