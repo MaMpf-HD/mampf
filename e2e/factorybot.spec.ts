b7d789b07d3ad2d6bc040c7f4944facbd864cfcb
Splines <37160523+Splines@users.noreply.github.com>
2025-11-16 12:27:39 +0000

    Write tests for instance methods calling

--- e2e/factorybot.spec.ts
+++ e2e/factorybot.spec.ts
@@ -5,7 +5,7 @@
 
 import { expect, test } from "./_support/fixtures";
 
-test("can create factories with traits and arguments", async ({ factory, tutor }) => {
+test("can create factories with traits and arguments", async ({ factory }) => {
   const CONTENT_MODE = "playwright";
   const lecture = await factory.createNoValidate("lecture", [], { content_mode: CONTENT_MODE });
   expect(lecture.content_mode).toBe(CONTENT_MODE);
@@ -15,10 +15,8 @@ test("can create factories with traits and arguments", async ({ factory, tutor }
   expect(tutorial.lecture_id).toBe(lectureForAll.id);
 });
 
-test("can call instance methods", async ({ factory }) => {
-  // const tutorial = await factory.create("tutorial", [], { lecture_id: lectureForAll.id, tutor_ids: [tutor.user.id] });
-//
-  const lecture = await factory.create("lecture", "with_sparse_toc", "released_for_all");
+test("can call instance methods", async ({ factory, tutor, student }) => {
+  const lecture = await factory.create("lecture", ["released_for_all"]);
   const title = await lecture.__call("title");
   expect(title).toBeTruthy();
   console.log("Lecture title:", title);
@@ -26,10 +24,16 @@ test("can call instance methods", async ({ factory }) => {
   const compactTitle = await lecture.__call("compact_title");
   expect(compactTitle).toBeTruthy();
   console.log("Compact title:", compactTitle);
+
+  const tutorial = await factory.create("tutorial", [],
+    { lecture_id: lecture.id, tutor_ids: [tutor.user.id, student.user.id] });
+  const tutorNames = await tutorial.__call("tutor_names");
+  expect(tutorNames).toContain(tutor.user.name_in_tutorials);
+  expect(tutorNames).toContain(student.user.name_in_tutorials);
 });
 
 test("can call methods that need a user as parameter", async ({ factory, student }) => {
-  const lecture = await factory.create("lecture", "released_for_all");
+  const lecture = await factory.create("lecture", ["released_for_all"]);
   const visibleForUser = await lecture.__call("visible_for_user?", student.user);
   expect(visibleForUser).toBe(true);
 
