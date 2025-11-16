858b299398f191b0915817caec79385d11d8dea2
Splines <37160523+Splines@users.noreply.github.com>
2025-11-16 12:50:50 +0000

    Fix error message role param

--- app/controllers/cypress/user_creator_playwright_controller.rb
+++ app/controllers/cypress/user_creator_playwright_controller.rb
@@ -6,7 +6,7 @@ module Cypress
     def create
       unless params[:role].is_a?(String)
         msg = "Role argument must be a string indicating the user role."
-        msg += " But we got: '#{params["0"]}'"
+        msg += " But we got: '#{params[:role]}'"
         raise(ArgumentError, msg)
       end
 
