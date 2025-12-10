3122ec8630418d9d660c245ac6138fda60b93931
Splines <info@splines.me>
2025-12-10 16:14:37 +0000

    Change app_port to nginx container

--- docker/production/start-mampf.sh
+++ docker/production/start-mampf.sh
@@ -5,5 +5,5 @@ cd /usr/src/app
 bundle exec rails db:migrate
 # exec bundle exec sidekiq &> >(tee -a /usr/src/app/log/runtime.log)
 
-echo "Starting MaMpf web server"
+echo "✨ Starting MaMpf web server ✨"
 bundle exec rails s -p 3000 -b "0.0.0.0"
