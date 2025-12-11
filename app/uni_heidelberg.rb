47ab08aac8c38eb7c57fa3e292c7a8f97656790d
Splines <info@splines.me>
2025-12-11 15:53:15 +0000

    Extract meaningful attributes

--- app/uni_heidelberg.rb
+++ app/uni_heidelberg.rb
@@ -1,13 +1,15 @@
 # Try out this file by running
-# $ UNI_ID=abc123 UNI_PASSWORD=my-uni-password rails runner app/active-directory-hd-trial.rb
+# $ UNI_ID=abc123 UNI_PASSWORD=my-uni-password rails runner app/uni_heidelberg.rb
 #
 # rubocop:disable Rails/Output
 
 require "net/ldap"
 
-BASE = "DC=ad,DC=uni-heidelberg,DC=de".freeze
+LDAP_HOST = "ad.uni-heidelberg.de".freeze
+LDAP_PORT = 389
+LDAP_BASE = "DC=ad,DC=uni-heidelberg,DC=de".freeze
 
-# Constructs the user principal name for a given university ID.
+# Shows user attributes for a given university ID.
 #
 # See: https://ldapwiki.com/wiki/Wiki.jsp?page=UserPrincipalName
 def user_principle_name(uni_id)
@@ -16,14 +18,32 @@ end
 
 def show_user_attributes(ldap, uni_id)
   filter = Net::LDAP::Filter.eq("userPrincipalName", user_principle_name(uni_id))
-  ldap.search(base: BASE,
+  ldap.search(base: LDAP_BASE,
               filter: filter,
-              attributes: [:displayname, :cn]) do |entry|
+              attributes: [:displayname, :cn, :objectclass,
+                           :givenname, :sn, :whencreated, :whenchanged,
+                           :pwdlastset, :lastlogontimestamp, :mail]) do |entry|
     uni_id = entry[:cn].first
     displayname = entry[:displayname].first
+    roles = entry[:objectclass]
+    first_name = entry[:givenname].first
+    last_name = entry[:sn].first
+    created_at = entry[:whencreated].first
+    changed_at = entry[:whenchanged].first
+    password_last_set = entry[:pwdlastset].first
+    last_login = entry[:lastlogontimestamp].first
+    mail = entry[:mail].first
 
     puts "University ID: #{uni_id}"
     puts "Display Name: #{displayname}"
+    puts "Roles: #{roles.join(", ")}"
+    puts "First Name: #{first_name}"
+    puts "Last Name: #{last_name}"
+    puts "Created At: #{created_at}"
+    puts "Changed At: #{changed_at}"
+    puts "Password Last Set: #{password_last_set}"
+    puts "Last Login: #{last_login}"
+    puts "Email: #{mail}"
   end
 end
 
@@ -39,10 +59,10 @@ def authenticate_uni_heidelberg_user(uni_id, password)
   end
 
   ldap = Net::LDAP.new
-  # https://www.urz.uni-heidelberg.de/de/service-katalog/identity-management/active-directory
-  ldap.host = "ad.uni-heidelberg.de"
-  ldap.port = 389
+  ldap.host = LDAP_HOST
+  ldap.port = LDAP_PORT
   ldap.auth(user_principle_name(uni_id), password)
+
   if ldap.bind
     puts "Authentication successful for user #{uni_id}"
     show_user_attributes(ldap, uni_id)
