f93aec9e60cc2465fdc3b78cfad20ae34963c26f
fosterfarrell9 <28628554+fosterfarrell9@users.noreply.github.com>
2025-12-12 15:09:26 +0000

    Change date in capacity migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_12_10_000000) do
+ActiveRecord::Schema[8.0].define(version: 2025_12_11_000000) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
   enable_extension "pg_trgm"
@@ -1008,7 +1008,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_12_10_000000) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-  
+
   create_table "vignettes_answers", force: :cascade do |t|
     t.string "type"
     t.bigint "vignettes_question_id", null: false
