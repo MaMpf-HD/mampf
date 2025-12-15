9561a582bd3f06f09b4dd94d2a8c1a05c581b164
fosterfarrell9 <28628554+fosterfarrell9@users.noreply.github.com>
2025-12-15 17:34:19 +0000

    Add factories and specs for lecture, speaker, and tutorial memberships; implement rosterable concern

---
+++ spec/models/rosters/maintenance_service_spec.rb
@@ -0,0 +1,78 @@
+require "rails_helper"
+
+RSpec.describe(Rosters::MaintenanceService, type: :model) do
+  subject { described_class.new }
+  let(:user) { create(:user) }
+  let(:tutorial) { create(:tutorial, capacity: 1) }
+  let(:other_tutorial) { create(:tutorial, capacity: 5) }
+
+  describe "#add_user!" do
+    context "when roster has capacity" do
+      it "adds the user to the roster" do
+        expect do
+          subject.add_user!(user, tutorial)
+        end.to change { tutorial.members.count }.by(1)
+      end
+    end
+
+    context "when roster is full" do
+      before { create(:tutorial_membership, tutorial: tutorial) }
+
+      it "raises CapacityExceededError" do
+        expect do
+          subject.add_user!(user, tutorial)
+        end.to raise_error(Rosters::MaintenanceService::CapacityExceededError)
+      end
+
+      it "adds the user if force is true" do
+        expect do
+          subject.add_user!(user, tutorial, force: true)
+        end.to change { tutorial.members.count }.by(1)
+      end
+    end
+
+    context "when user is already in roster" do
+      before { create(:tutorial_membership, tutorial: tutorial, user: user) }
+
+      it "does nothing" do
+        expect do
+          subject.add_user!(user, tutorial)
+        end.not_to(change { tutorial.members.count })
+      end
+    end
+  end
+
+  describe "#remove_user!" do
+    before { create(:tutorial_membership, tutorial: tutorial, user: user) }
+
+    it "removes the user from the roster" do
+      expect do
+        subject.remove_user!(user, tutorial)
+      end.to change { tutorial.members.count }.by(-1)
+    end
+  end
+
+  describe "#move_user!" do
+    before { create(:tutorial_membership, tutorial: tutorial, user: user) }
+
+    it "moves the user from one roster to another" do
+      expect do
+        subject.move_user!(user, tutorial, other_tutorial)
+      end.to change { tutorial.members.count }.by(-1)
+                                              .and(change { other_tutorial.members.count }.by(1))
+    end
+
+    context "when target roster is full" do
+      let(:full_tutorial) { create(:tutorial, capacity: 0) }
+
+      it "raises CapacityExceededError and does not remove user from source" do
+        expect do
+          subject.move_user!(user, tutorial, full_tutorial)
+        end.to raise_error(Rosters::MaintenanceService::CapacityExceededError)
+
+        expect(tutorial.members).to include(user)
+        expect(full_tutorial.members).not_to include(user)
+      end
+    end
+  end
+end
---
+++ spec/support/shared_examples/rosterable_concern.rb
@@ -0,0 +1,110 @@
+RSpec.shared_examples("a rosterable model") do
+  let(:user) { create(:user) }
+  let(:campaign) { create(:registration_campaign) }
+  let(:other_campaign) { create(:registration_campaign) }
+
+  # We need a persisted subject for DB operations
+  let(:rosterable) { create(described_class.name.underscore.to_sym) }
+
+  # Helper to find the join record without knowing the column name explicitly in the test body
+  def find_roster_entry(rosterable, user)
+    rosterable.roster_entries.find_by(rosterable.roster_user_id_column => user.id)
+  end
+
+  describe "interface" do
+    subject { rosterable }
+    it { is_expected.to respond_to(:roster_entries) }
+    it { is_expected.to respond_to(:add_user_to_roster!) }
+    it { is_expected.to respond_to(:remove_user_from_roster!) }
+    it { is_expected.to respond_to(:materialize_allocation!) }
+  end
+
+  describe "#add_user_to_roster!" do
+    it "adds the user to the roster" do
+      expect do
+        rosterable.add_user_to_roster!(user)
+      end.to change { rosterable.roster_entries.count }.by(1)
+
+      expect(find_roster_entry(rosterable, user)).to be_present
+    end
+
+    it "associates the user with the campaign if provided" do
+      rosterable.add_user_to_roster!(user, campaign)
+      entry = find_roster_entry(rosterable, user)
+      expect(entry.source_campaign).to eq(campaign)
+    end
+  end
+
+  describe "#remove_user_from_roster!" do
+    before { rosterable.add_user_to_roster!(user) }
+
+    it "removes the user from the roster" do
+      expect do
+        rosterable.remove_user_from_roster!(user)
+      end.to change { rosterable.roster_entries.count }.by(-1)
+
+      expect(find_roster_entry(rosterable, user)).to be_nil
+    end
+  end
+
+  describe "#materialize_allocation!" do
+    let(:users_to_keep) { create_list(:user, 2) }
+    let(:users_to_add) { create_list(:user, 2) }
+    let(:users_to_remove) { create_list(:user, 2) }
+    let(:manual_users) { create_list(:user, 2) }
+    let(:other_campaign_users) { create_list(:user, 2) }
+
+    before do
+      # Users to keep (already in roster via campaign)
+      users_to_keep.each { |u| rosterable.add_user_to_roster!(u, campaign) }
+
+      # Users to remove (already in roster via campaign, but won't be in target list)
+      users_to_remove.each { |u| rosterable.add_user_to_roster!(u, campaign) }
+
+      # Manual users (no campaign)
+      manual_users.each { |u| rosterable.add_user_to_roster!(u) }
+
+      # Other campaign users
+      other_campaign_users.each { |u| rosterable.add_user_to_roster!(u, other_campaign) }
+    end
+
+    it "syncs the roster correctly" do
+      target_users = users_to_keep + users_to_add
+
+      rosterable.materialize_allocation!(user_ids: target_users.map(&:id), campaign: campaign)
+
+      # Check users to add are present
+      users_to_add.each do |u|
+        entry = find_roster_entry(rosterable, u)
+        expect(entry).to be_present
+        expect(entry.source_campaign).to eq(campaign)
+      end
+
+      # Check users to keep are still present
+      users_to_keep.each do |u|
+        entry = find_roster_entry(rosterable, u)
+        expect(entry).to be_present
+        expect(entry.source_campaign).to eq(campaign)
+      end
+
+      # Check users to remove are gone
+      users_to_remove.each do |u|
+        expect(find_roster_entry(rosterable, u)).to be_nil
+      end
+
+      # Check manual users are untouched
+      manual_users.each do |u|
+        entry = find_roster_entry(rosterable, u)
+        expect(entry).to be_present
+        expect(entry.source_campaign).to be_nil
+      end
+
+      # Check other campaign users are untouched
+      other_campaign_users.each do |u|
+        entry = find_roster_entry(rosterable, u)
+        expect(entry).to be_present
+        expect(entry.source_campaign).to eq(other_campaign)
+      end
+    end
+  end
+end
