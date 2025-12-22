5a238c70f8ddc15c798ebebf7008dcee6fb1d583
Splines <info@splines.me>
2025-12-22 17:47:27 +0000

    Improve lecture browse controller

--- app/frontend/main/lecture_browse.controller.js
+++ app/frontend/main/lecture_browse.controller.js
@@ -1,23 +1,28 @@
 import { Controller } from "@hotwired/stimulus";
 
+/**
+ * Controller that loads all lectures when the user scrolls to the bottom of the
+ * page.
+ */
 export default class extends Controller {
-  static targets = ["results", "form"];
+  static targets = ["form"];
 
   connect() {
     this.loadAllLectures = this.loadAllLectures.bind(this);
-    this.observer = new IntersectionObserver(
-      (entries) => {
-        entries.forEach((entry) => {
-          if (entry.isIntersecting && !this.loaded) {
-            this.loadAllLectures();
-            this.loaded = true;
-          }
-        });
-      },
-      {
-        root: null,
-        threshold: 0.1,
-      },
+    this.observer = new IntersectionObserver((entries) => {
+      if (this.loaded) return;
+
+      entries.forEach((entry) => {
+        if (!this.loaded && entry.isIntersecting) {
+          this.loadAllLectures();
+          this.loaded = true;
+        }
+      });
+    },
+    {
+      root: null,
+      threshold: 0.1,
+    },
     );
 
     this.observer.observe(this.element);
@@ -31,7 +36,6 @@ export default class extends Controller {
 
   loadAllLectures() {
     const form = this.hasFormTarget ? this.formTarget : this.element.querySelector("form");
-
     if (!form) {
       return;
     }
@@ -39,6 +43,7 @@ export default class extends Controller {
     const formData = new FormData(form);
     const params = new URLSearchParams(formData);
 
+    // TODO: do this more elegantly with Hotwire
     fetch(`${form.action}?${params}`, {
       method: "GET",
       headers: {
