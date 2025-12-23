fd51c7265465544ccf26e2946a6d2d0283bf1e9b
Phương Liên Tạ <phuonglienta2121999@gmail.com>
2025-12-23 04:20:12 +0000

    add banner and decoration

--- app/frontend/entrypoints/initHotwire.js
+++ app/frontend/entrypoints/initHotwire.js
@@ -35,6 +35,9 @@ window.Stimulus.register("vignettes-number", VignettesNumberController);
 import RegistrationItemController from "~/registration/item_search_form/_search_form.controller.js";
 window.Stimulus.register("registration-item-search", RegistrationItemController);
 
+import TouchedCheckController from "~/registration/touched_check/touched_check.controller.js";
+window.Stimulus.register("touched-check", TouchedCheckController);
+
 import RegistrationPolicyFormController from "~/registration/policies/policy_form.controller.js";
 window.Stimulus.register("registration-policy-form", RegistrationPolicyFormController);
 
---
+++ app/frontend/registration/touched_check/touched_check.controller.js
@@ -0,0 +1,29 @@
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+  connect() {
+    this.element.dataset.touched = "false";
+    this.update(false);
+  }
+
+  mark() {
+    this.element.dataset.touched = "true";
+    this.update(true);
+  }
+
+  reset() {
+    this.element.dataset.touched = "false";
+    this.update(false);
+  }
+
+  update(status) {
+    this.element
+      .querySelectorAll("[data-requires-touch-enable]")
+      .forEach(btn => btn.disabled = !status);
+
+    if (status == true)
+      document.querySelector("[data-requires-touch-display]").classList.remove("d-none");
+    else
+      document.querySelector("[data-requires-touch-display]").classList.add("d-none");
+  }
+}
