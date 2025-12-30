c68caab2a965315146fc641ce5c164460e489811
fosterfarrell9 <28628554+fosterfarrell9@users.noreply.github.com>
2025-12-30 12:15:00 +0000

    Add member management actions to roster maintenance controller

--- app/controllers/roster/maintenance_controller.rb
+++ app/controllers/roster/maintenance_controller.rb
@@ -3,7 +3,7 @@ module Roster
   # item dashboard. Handles student membership visualization and maintenance actions.
   class MaintenanceController < ApplicationController
     before_action :set_lecture, only: [:index]
-    before_action :set_rosterable, only: [:show, :update]
+    before_action :set_rosterable, only: [:show, :update, :add_member, :remove_member, :move_member]
 
     def current_ability
       @current_ability ||= LectureAbility.new(current_user)
@@ -47,7 +47,76 @@ module Roster
       end
     end
 
+    def add_member
+      authorize! :edit, @lecture
+      return if locked_check_failed?
+
+      user = User.find_by(email: params[:email])
+      if user
+        Rosters::MaintenanceService.new(@rosterable).add_user!(user)
+        redirect_back_or_to fallback_path, notice: t("roster.messages.user_added")
+      else
+        redirect_back_or_to fallback_path, alert: t("roster.errors.user_not_found")
+      end
+    rescue Rosters::CapacityExceededError
+      redirect_back_or_to fallback_path, alert: t("roster.errors.capacity_exceeded")
+    rescue StandardError => e
+      redirect_back_or_to fallback_path, alert: e.message
+    end
+
+    def remove_member
+      authorize! :edit, @lecture
+      return if locked_check_failed?
+
+      user = User.find(params[:user_id])
+      Rosters::MaintenanceService.new(@rosterable).remove_user!(user)
+      redirect_back_or_to fallback_path, notice: t("roster.messages.user_removed")
+    rescue StandardError => e
+      redirect_back_or_to fallback_path, alert: e.message
+    end
+
+    def move_member
+      authorize! :edit, @lecture
+      return if locked_check_failed?
+
+      user = User.find(params[:user_id])
+      target = find_target_rosterable(params[:target_id])
+
+      if target.nil?
+        redirect_back_or_to fallback_path, alert: t("roster.errors.target_not_found")
+        return
+      end
+
+      if target.locked?
+        redirect_back_or_to fallback_path, alert: t("roster.errors.target_locked")
+        return
+      end
+
+      Rosters::MaintenanceService.new(@rosterable).move_user!(user, target)
+      redirect_back_or_to fallback_path, notice: t("roster.messages.user_moved")
+    rescue Rosters::CapacityExceededError
+      redirect_back_or_to fallback_path, alert: t("roster.errors.capacity_exceeded")
+    rescue StandardError => e
+      redirect_back_or_to fallback_path, alert: e.message
+    end
+
     private
+
+      def fallback_path
+        lecture_roster_path(@lecture, group_type: group_type_for_rosterable)
+      end
+
+      def locked_check_failed?
+        if @rosterable.locked?
+          redirect_back_or_to fallback_path, alert: t("roster.errors.item_locked")
+          return true
+        end
+        false
+      end
+
+      def find_target_rosterable(id)
+        @lecture.tutorials.find_by(id: id) || @lecture.talks.find_by(id: id)
+      end
 
       def rosterable_params
         params.expect(rosterable: [:managed_by_campaign])
