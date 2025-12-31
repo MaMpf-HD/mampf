d663ddf7fad4646f2e98f1464a8d0720f6841866
fosterfarrell9 <28628554+fosterfarrell9@users.noreply.github.com>
2025-12-31 13:24:14 +0000

    Rename migration

--- db/migrate/20251229000000_add_manual_roster_mode_to_rosterables.rb
+++ db/migrate/20251229000000_add_manual_roster_mode_to_rosterables.rb
@@ -1,4 +1,4 @@
-class AddManagedByCampaignToRosterables < ActiveRecord::Migration[8.0]
+class AddManualRosterModeToRosterables < ActiveRecord::Migration[8.0]
   def change
     add_column :tutorials, :manual_roster_mode, :boolean, default: false, null: false
     add_column :talks, :manual_roster_mode, :boolean, default: false, null: false
