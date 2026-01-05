c3a4063eb85360368350e1ca0a06827dd122baae
fosterfarrell9 <28628554+fosterfarrell9@users.noreply.github.com>
2026-01-05 13:47:17 +0000

    Implement Turbo Streams for Talks management and add localization for creation messages

--- app/controllers/talks_controller.rb
+++ app/controllers/talks_controller.rb
@@ -19,9 +19,31 @@ class TalksController < ApplicationController
     authorize! :new, @talk
     I18n.locale = @talk.lecture.locale_with_inheritance ||
                   current_user.locale || I18n.default_locale
+
+    respond_to do |format|
+      format.js
+      format.html
+      format.turbo_stream do
+        render turbo_stream: turbo_stream.update(
+          "modal-container",
+          partial: "talks/roster_modal",
+          locals: { talk: @talk }
+        )
+      end
+    end
   end
 
   def edit
+    respond_to do |format|
+      format.html
+      format.turbo_stream do
+        render turbo_stream: turbo_stream.update(
+          "modal-container",
+          partial: "talks/roster_modal",
+          locals: { talk: @talk }
+        )
+      end
+    end
   end
 
   def create
@@ -34,21 +56,39 @@ class TalksController < ApplicationController
     I18n.locale = @talk&.lecture&.locale_with_inheritance ||
                   current_user.locale || I18n.default_locale
     position = params[:talk][:predecessor]
-    # place the chapter in the correct position
-    if position.present?
+
+    saved = if position.present?
       @talk.insert_at(position.to_i + 1)
+      @talk.valid?
     else
       @talk.save
     end
-    redirect_to edit_lecture_path(@talk.lecture) if @talk.valid?
-    @errors = @talk.errors
+
+    respond_to do |format|
+      format.js do
+        @errors = @talk.errors unless saved
+        render :create
+      end
+      format.html do
+        if saved
+          redirect_to edit_lecture_path(@talk.lecture)
+        else
+          @errors = @talk.errors
+          render :new
+        end
+      end
+      format.turbo_stream do
+        group_type = parse_group_type
+        streams = create_turbo_streams(group_type, saved)
+        render turbo_stream: streams, status: saved ? :ok : :unprocessable_content
+      end
+    end
   end
 
   def update
     I18n.locale = @talk.lecture.locale_with_inheritance ||
                   current_user.locale || I18n.default_locale
-    @talk.update(talk_params)
-    if @talk.valid?
+    if @talk.update(talk_params) && @talk.valid?
       dates = parse_talk_dates(params[:talk][:dates])
       @talk.update(dates: dates)
 
@@ -59,16 +99,50 @@ class TalksController < ApplicationController
         position -= 1 if position > @talk.position
         @talk.insert_at(position + 1)
       end
-      redirect_to edit_talk_path(@talk)
+
+      respond_to do |format|
+        format.html { redirect_to edit_talk_path(@talk) }
+        format.turbo_stream do
+          group_type = parse_group_type
+          render turbo_stream: [
+            update_roster_groups_list_stream(group_type),
+            refresh_campaigns_index_stream(@talk.lecture),
+            turbo_stream.update("modal-container", ""),
+            stream_flash
+          ]
+        end
+      end
       return
     end
+
     @errors = @talk.errors
+    respond_to do |format|
+      format.html { render :edit }
+      format.turbo_stream do
+        render turbo_stream: turbo_stream.replace(
+          dom_id(@talk, "form"),
+          partial: "talks/roster_modal_form",
+          locals: { talk: @talk }
+        ), status: :unprocessable_content
+      end
+    end
   end
 
   def destroy
     lecture = @talk.lecture
     @talk.destroy
-    redirect_to edit_lecture_path(lecture)
+
+    respond_to do |format|
+      format.html { redirect_to edit_lecture_path(lecture) }
+      format.turbo_stream do
+        group_type = parse_group_type
+        render turbo_stream: [
+          update_roster_groups_list_stream(group_type),
+          refresh_campaigns_index_stream(lecture),
+          stream_flash
+        ]
+      end
+    end
   end
 
   def assemble
@@ -92,7 +166,7 @@ class TalksController < ApplicationController
     end
 
     def talk_params
-      attributes = [:title, :lecture_id, :details, :description,
+      attributes = [:title, :lecture_id, :details, :description, :capacity,
                     :display_description, { speaker_ids: [], tag_ids: [] }]
       if @talk && !current_user.in?(@talk.speakers) &&
          !@talk.display_description
@@ -115,5 +189,50 @@ class TalksController < ApplicationController
       return [] unless dates_param
 
       dates_param.values.filter_map { |d| d[:date] }.compact_blank
+    end
+
+    def parse_group_type
+      if params[:group_type].is_a?(Array)
+        params[:group_type].map(&:to_sym)
+      else
+        params[:group_type].presence&.to_sym || :talks
+      end
+    end
+
+    def create_turbo_streams(group_type, saved)
+      streams = []
+
+      if saved
+        flash.now[:notice] = t("controllers.talks.created")
+        streams << update_roster_groups_list_stream(group_type)
+        streams << refresh_campaigns_index_stream(@talk.lecture)
+        streams << turbo_stream.update("modal-container", "")
+      else
+        streams << turbo_stream.replace(view_context.dom_id(@talk, "form"),
+                                        partial: "talks/roster_modal_form",
+                                        locals: { talk: @talk })
+      end
+
+      streams << stream_flash if flash.present?
+      streams
+    end
+
+    def update_roster_groups_list_stream(group_type)
+      component = RosterOverviewComponent.new(lecture: @talk.lecture,
+                                              group_type: group_type)
+      turbo_stream.update("roster_groups_list",
+                          partial: "roster/components/groups_tab",
+                          locals: {
+                            groups: component.groups,
+                            total_participants: component.total_participants,
+                            group_type: group_type,
+                            component: component
+                          })
+    end
+
+    def refresh_campaigns_index_stream(lecture)
+      turbo_stream.replace("campaigns_container",
+                           partial: "registration/campaigns/index",
+                           locals: { lecture: lecture })
     end
 end
