f061121a00e22f907fa39ebdd7bb491d730944a3
fosterfarrell9 <28628554+fosterfarrell9@users.noreply.github.com>
2026-01-14 14:28:23 +0000

    Enhance registration item display: add type column to items table, improve patterns info layout, and update localization for clarity

--- app/helpers/registration/items_helper.rb
+++ app/helpers/registration/items_helper.rb
@@ -4,6 +4,31 @@ module Registration
       @registration_items_service ||= Registration::AvailableItemsService.new(campaign)
     end
 
+    def item_display_type(item)
+      case item.registerable_type
+      when "Tutorial"
+        t("registration.item.types.tutorial")
+      when "Talk"
+        t("registration.item.types.talk")
+      when "Cohort"
+        cohort = item.registerable
+        base_type = case cohort.purpose.to_sym
+                    when :enrollment
+                      t("registration.item.types.enrollment_group")
+                    when :planning
+                      t("registration.item.types.planning_survey")
+                    when :general
+                      t("registration.item.types.other_group")
+        end
+
+        if cohort.propagate_to_lecture
+          base_type
+        else
+          "#{base_type} <i class='bi bi-person-x ms-1' style='color: #495057;' data-bs-toggle='tooltip' title='#{t("registration.item.hints.no_propagation")}'></i>".html_safe
+        end
+      end
+    end
+
     def format_capacity(capacity)
       if capacity.nil?
         t("basics.unlimited")
