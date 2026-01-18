0e22e7fc99729a2aa881bdfdd0c4bb57643a9f89
Tim-Grube <115377141+Tim-Grube@users.noreply.github.com.>
2026-01-18 19:51:49 +0100

    add: comments to streak activities

--- lib/extensions/commontator/comment.rb
+++ lib/extensions/commontator/comment.rb
@@ -6,9 +6,17 @@ module Extensions
       included do
         has_one :annotation, foreign_key: :public_comment_id
 
+        after_create :increment_user_streak
+
         def medium
           thread.commontable
         end
+
+        private
+
+          def increment_user_streak
+            creator.increment_streak if creator.is_a?(User)
+          end
       end
     end
   end
