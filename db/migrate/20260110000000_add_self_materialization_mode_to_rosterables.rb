377e61e0f027e7d3199926c37a8047d3f0221b35
fosterfarrell9 <28628554+fosterfarrell9@users.noreply.github.com>
2026-01-18 17:57:10 +0000

    Update migration to use ActiveRecord version 8.0

--- db/migrate/20260110000000_add_self_materialization_mode_to_rosterables.rb
+++ db/migrate/20260110000000_add_self_materialization_mode_to_rosterables.rb
@@ -1,4 +1,4 @@
-class AddSelfMaterializationModeToRosterables < ActiveRecord::Migration[7.1]
+class AddSelfMaterializationModeToRosterables < ActiveRecord::Migration[8.0]
   def change
     add_column :tutorials, :self_materialization_mode, :integer, default: 0
     add_column :cohorts, :self_materialization_mode, :integer, default: 0
