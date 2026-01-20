9bee66ca763d800998aa3c9bff0efeb1374e9630
fosterfarrell9 <28628554+fosterfarrell9@users.noreply.github.com>
2026-01-20 16:34:45 +0000

    Remove quick create for registerables

--- app/controllers/registration/items_controller.rb
+++ app/controllers/registration/items_controller.rb
@@ -20,11 +20,7 @@ module Registration
     end
 
     def create
-      if params[:registration_item][:new_registerable].present?
-        create_new_registerable
-      else
-        create_existing_item
-      end
+      create_existing_item
     end
 
     def update
@@ -108,88 +104,6 @@ module Registration
         else
           respond_with_error(@item.errors.full_messages.to_sentence)
         end
-      end
-
-      def create_new_registerable
-        type = params[:registration_item][:registerable_type]
-        unless Registration::Item::REGISTERABLE_CLASSES.key?(type)
-          return respond_with_error(t("registration.item.invalid_type"))
-        end
-
-        if save_new_registerable_item(type)
-          respond_with_success(t("registration.item.created"))
-        else
-          respond_with_error(@item.errors.full_messages.to_sentence)
-        end
-      end
-
-      def save_new_registerable_item(type)
-        registerable = build_registerable(type)
-        authorize! :create, registerable
-
-        persist_registerable_and_item(registerable, type)
-      end
-
-      def build_registerable(type)
-        klass = Registration::Item::REGISTERABLE_CLASSES[type]
-
-        attributes = {
-          title: params[:registration_item][:title],
-          capacity: params[:registration_item][:capacity]
-        }
-
-        if klass == Cohort
-          attributes[:context] = @campaign.campaignable
-          attributes[:purpose] = Cohort::TYPE_TO_PURPOSE[type]
-          attributes[:propagate_to_lecture] = determine_propagate_flag(type)
-        else
-          attributes[:lecture] = @campaign.campaignable
-        end
-
-        klass.new(attributes)
-      end
-
-      def determine_propagate_flag(type)
-        case Cohort::TYPE_TO_PURPOSE[type]
-        when :enrollment
-          true
-        when :planning
-          false
-        when :general
-          params[:registration_item][:propagate_to_lecture] == "1"
-        end
-      end
-
-      def persist_registerable_and_item(registerable, type)
-        ActiveRecord::Base.transaction do
-          unless registerable.save
-            build_item_with_errors(registerable)
-            raise(ActiveRecord::Rollback)
-          end
-
-          build_and_authorize_item(registerable, type)
-          raise(ActiveRecord::Rollback) unless @item.save
-        end
-        @item&.persisted?
-      end
-
-      def build_item_with_errors(registerable)
-        @item = @campaign.registration_items.build
-        registerable.errors.full_messages.each { |m| @item.errors.add(:base, m) }
-      end
-
-      def build_and_authorize_item(registerable, type)
-        registerable_type = if Cohort::TYPE_TO_PURPOSE.key?(type)
-          "Cohort"
-        else
-          type
-        end
-
-        @item = @campaign.registration_items.build(
-          registerable: registerable,
-          registerable_type: registerable_type
-        )
-        authorize! :create, @item
       end
 
       def respond_with_success(message)
