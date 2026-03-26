07b16fcc99bcdefe77d4c28dba73ddc865a8357a
fosterfarrell9 <28628554+fosterfarrell9@users.noreply.github.com>
2026-03-26 15:02:48 +0000

    Add clearable option to datetime picker and update form fields

--- app/frontend/controllers/datetimepicker_controller.js
+++ app/frontend/controllers/datetimepicker_controller.js
@@ -3,6 +3,8 @@ import { TempusDominus, Namespace } from "@eonasdan/tempus-dominus";
 import "@eonasdan/tempus-dominus/dist/css/tempus-dominus.min.css";
 
 export default class extends Controller {
+  static values = { clearable: { type: Boolean, default: false } };
+
   connect() {
     this.initDatetimePicker();
   }
@@ -17,6 +19,9 @@ export default class extends Controller {
     this.picker = new TempusDominus(this.element, {
       display: {
         sideBySide: true,
+        buttons: {
+          clear: this.clearableValue,
+        },
         icons: this.getDateTimePickerIcons(),
       },
       localization: {
