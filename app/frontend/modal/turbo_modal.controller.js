8831f8fec3e769e0aaaf23f79255772d5615a85d
Splines <info@splines.me>
2026-04-04 10:53:40 +0000

    Add TODO to turbo modal controller

--- app/frontend/modal/turbo_modal.controller.js
+++ app/frontend/modal/turbo_modal.controller.js
@@ -2,6 +2,11 @@ import { Controller } from "@hotwired/stimulus";
 import { Modal } from "bootstrap";
 import * as Turbo from "@hotwired/turbo";
 
+/**
+ * Don't use this controller!
+ *
+ * Controller for opening modals with Turbo content.
+ */
 export default class extends Controller {
   static values = {
     selector: String,
@@ -20,6 +25,15 @@ export default class extends Controller {
     const url = this.element.getAttribute("href");
 
     try {
+      // TODO: We should find a better way to load modals without having
+      // to make our own fetch request and render the response manually.
+      // I've tried out Rails Request.JS (https://github.com/rails/request.js)
+      // and it works fine. However, even with that, it feels like there are
+      // simpler solutions out there. When you search for blog posts about
+      // Modals with Turbo, you will find lots of different approaches that
+      // are easier than this and generalize better.
+      // We use this here for now, but should check for better solutions
+      // in case we need more modal forms.
       const response = await fetch(url, {
         headers: {
           "Accept": "text/vnd.turbo-stream.html, text/html, application/xhtml+xml",
