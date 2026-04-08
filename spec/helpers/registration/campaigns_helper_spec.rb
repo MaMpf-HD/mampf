48b7dcfcf263f7d7c6d7f6cc132bfc4a46b1edbc
Splines <info@splines.me>
2026-04-08 22:29:24 +0000

    Fix weird line breaks

--- spec/helpers/registration/campaigns_helper_spec.rb
+++ spec/helpers/registration/campaigns_helper_spec.rb
@@ -14,16 +14,16 @@ RSpec.describe(Registration::CampaignsHelper, type: :helper) do
 
   describe "#campaign_badge_color" do
     it "returns the correct color sequence based on status" do
-      expect(helper.campaign_badge_color(build(:registration_campaign,
-                                               status: :draft))).to eq("secondary")
-      expect(helper.campaign_badge_color(build(:registration_campaign,
-                                               status: :open))).to eq("success")
-      expect(helper.campaign_badge_color(build(:registration_campaign,
-                                               status: :closed))).to eq("warning")
-      expect(helper.campaign_badge_color(build(:registration_campaign,
-                                               status: :processing))).to eq("info")
-      expect(helper.campaign_badge_color(build(:registration_campaign,
-                                               status: :completed))).to eq("dark")
+      expect(helper.campaign_badge_color(build(:registration_campaign, status: :draft)))
+        .to eq("secondary")
+      expect(helper.campaign_badge_color(build(:registration_campaign, status: :open)))
+        .to eq("success")
+      expect(helper.campaign_badge_color(build(:registration_campaign, status: :closed)))
+        .to eq("warning")
+      expect(helper.campaign_badge_color(build(:registration_campaign, status: :processing)))
+        .to eq("info")
+      expect(helper.campaign_badge_color(build(:registration_campaign, status: :completed)))
+        .to eq("dark")
     end
   end
 
