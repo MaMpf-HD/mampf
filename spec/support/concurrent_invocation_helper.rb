d9cac73a2efada53325ebc043299f1b21f858e55
Splines <info@splines.me>
2026-04-10 15:04:20 +0000

    Add docstrings

--- spec/support/concurrent_invocation_helper.rb
+++ spec/support/concurrent_invocation_helper.rb
@@ -1,4 +1,8 @@
+# Helper for running synchronized concurrent invocations in specs.
+
 module ConcurrentInvocationHelper
+  # Runs the given block concurrently in `thread_count` threads,
+  # synchronizing start time and returning all results (or raised exceptions).
   def run_concurrently(thread_count: 2)
     ready = Queue.new
     start = Queue.new
