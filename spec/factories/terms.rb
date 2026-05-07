ec051676cbd7989a6ea983c3f09b802fa0f84f06
fosterfarrell9 <28628554+fosterfarrell9@users.noreply.github.com>
2026-05-07 08:01:21 +0000

    Fix term factory year range

--- spec/factories/terms.rb
+++ spec/factories/terms.rb
@@ -3,7 +3,7 @@ require "faker"
 FactoryBot.define do
   factory :term do
     season { ["WS", "SS"].sample }
-    year { Faker::Number.between(from: 2000, to: 100_000) }
+    year { Faker::Number.between(from: 2000, to: 2100) }
 
     trait :summer do
       season { "SS" }
