515f3ac3ef353249211748ff4ce63663142dd654
fosterfarrell9 <28628554+fosterfarrell9@users.noreply.github.com>
2026-05-20 16:26:47 +0000

    Refactor password denylist formatting for improved readability

--- app/validators/password_denylist_validator.rb
+++ app/validators/password_denylist_validator.rb
@@ -1,25 +1,23 @@
-require "set"
-
 class PasswordDenylistValidator < ActiveModel::EachValidator
-  DENYLIST = Set.new(%w[
-    123456789012
-    123412341234
-    abc123abc123
-    admin12345
-    adminadmin12
-    asdfghjkl123
-    changeme123
-    iloveyou123
-    letmein12345
-    passwort1234
-    password123
-    password1234
-    password12345
-    password123456
-    qwerty12345
-    qwertyuiop12
-    welcome12345
-  ]).freeze
+  DENYLIST = Set.new([
+                       "123456789012",
+                       "123412341234",
+                       "abc123abc123",
+                       "admin12345",
+                       "adminadmin12",
+                       "asdfghjkl123",
+                       "changeme123",
+                       "iloveyou123",
+                       "letmein12345",
+                       "passwort1234",
+                       "password123",
+                       "password1234",
+                       "password12345",
+                       "password123456",
+                       "qwerty12345",
+                       "qwertyuiop12",
+                       "welcome12345"
+                     ]).freeze
 
   def validate_each(record, attribute, value)
     return if value.blank?
