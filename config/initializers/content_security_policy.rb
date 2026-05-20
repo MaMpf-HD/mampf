357454f222ad3ad163d1496125d37ba3ee7fdd4c
fosterfarrell9 <28628554+fosterfarrell9@users.noreply.github.com>
2026-05-20 06:46:14 +0000

    Enhance Content Security Policy configuration

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -4,35 +4,55 @@
 # See the Securing Rails Applications Guide for more information:
 # https://guides.rubyonrails.org/security.html#content-security-policy-header
 
-# Rails.application.configure do
-#   config.content_security_policy do |policy|
-#     policy.default_src :self, :https
-#     policy.font_src    :self, :https, :data
-#     policy.img_src     :self, :https, :data
-#     policy.object_src  :none
-#     policy.script_src  :self, :https
+Rails.application.configure do
+  config.content_security_policy do |policy|
+    policy.default_src(:self)
+    policy.base_uri(:self)
+    policy.font_src(:self,
+                    :data,
+                    "https://cdn.jsdelivr.net")
+    policy.frame_ancestors(:self)
+    policy.frame_src(:self,
+                     "https://www.geogebra.org",
+                     "https://*.geogebra.org")
+    policy.img_src(:self,
+                   :blob,
+                   :data,
+                   :https)
+    policy.media_src(:self,
+                     :blob)
+    policy.object_src(:none)
+    policy.script_src(:self,
+                      "https://cdn.jsdelivr.net",
+                      "https://cdnjs.cloudflare.com",
+                      "https://www.geogebra.org",
+                      "https://*.geogebra.org")
+    policy.style_src(:self,
+                     :unsafe_inline,
+                     "https://cdn.jsdelivr.net",
+                     "https://cdnjs.cloudflare.com")
+    policy.connect_src(:self,
+                       "https://www.geogebra.org",
+                       "https://*.geogebra.org")
+    policy.form_action(:self)
 
-# Allow @vite/client to hot reload javascript changes in development
-# if Rails.env.development?
-#   policy.script_src(*policy.script_src, :unsafe_eval,
-#                     "http://#{ViteRuby.config.host_with_port}")
-# end
+    if Rails.env.development?
+      vite_host = ViteRuby.config.host_with_port
 
-# You may need to enable this in production as well depending on your setup.
-# policy.script_src *policy.script_src, :blob if Rails.env.test?
+      policy.script_src(*policy.script_src,
+                        :unsafe_eval,
+                        "http://#{vite_host}")
+      policy.style_src(*policy.style_src,
+                       "http://#{vite_host}")
+      policy.connect_src(*policy.connect_src,
+                         "http://#{vite_host}",
+                         "ws://#{vite_host}")
+    end
+  end
 
-# policy.style_src   :self, :https
-# Allow @vite/client to hot reload style changes in development
-# policy.style_src *policy.style_src, :unsafe_inline if Rails.env.development?
-
-#     # Specify URI for violation reports
-#     # policy.report_uri "/csp-violation-report-endpoint"
-#   end
-#
-#   # Generate session nonces for permitted importmap, inline scripts, and inline styles.
-#   config.content_security_policy_nonce_generator = ->(request) { request.session.id.to_s }
-#   config.content_security_policy_nonce_directives = %w(script-src style-src)
-#
-#   # Report violations without enforcing the policy.
-#   # config.content_security_policy_report_only = true
-# end
+  config.content_security_policy_nonce_generator = lambda { |request|
+    request.session.id.to_s
+  }
+  config.content_security_policy_nonce_directives = ["script-src"]
+  config.content_security_policy_report_only = true
+end
