6af6ec88cb76c09cccbea44f36cfefd735f47a36
fosterfarrell9 <28628554+fosterfarrell9@users.noreply.github.com>
2026-05-24 10:30:31 +0000

    Harden screenshot uploader

--- app/uploaders/screenshot_uploader.rb
+++ app/uploaders/screenshot_uploader.rb
@@ -2,8 +2,11 @@ require "image_processing/mini_magick"
 # ScreenshotUploader class
 # used for storing video thumbnails
 class ScreenshotUploader < Shrine
+  MAX_SIZE = 10 * 1024 * 1024
+  MAX_DIMENSIONS = [4096, 4096].freeze
+
   # shrine plugins
-  plugin :upload_endpoint
+  plugin :upload_endpoint, max_size: MAX_SIZE
   plugin :store_dimensions
   plugin :determine_mime_type
   plugin :validation_helpers
@@ -13,6 +16,11 @@ class ScreenshotUploader < Shrine
   Attacher.validate do
     validate_mime_type_inclusion ["image/jpeg", "image/png", "image/gif"],
                                  message: "falscher MIME-Typ"
+    validate_max_size MAX_SIZE, message: I18n.t("package.too_big")
+    validate_max_dimensions MAX_DIMENSIONS,
+                            message: I18n.t("image.too_large_dimensions",
+                                            max_width: MAX_DIMENSIONS[0],
+                                            max_height: MAX_DIMENSIONS[1])
   end
 
   # store a resized version of the screenshot
