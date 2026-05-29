c57a0172edd623a53ef93f005f4970f15f3efc85
Phương Liên Tạ <phuonglienta2121999@gmail.com>
2026-05-29 17:12:50 +0000

    replace mocks with real data in SelfMaterializationController request spec

--- spec/requests/roster/self_materialization_controller_spec.rb
+++ spec/requests/roster/self_materialization_controller_spec.rb
@@ -1,33 +1,38 @@
 require "rails_helper"
 
 RSpec.describe("Roster::SelfMaterializationController", type: :request) do
-  let(:user)     { create(:confirmed_user_en) }
-  let(:lecture)  { create(:lecture) }
-  let(:tutorial) { create(:tutorial, lecture: lecture) }
-  let(:service)  { instance_double(Rosters::SelfMaterializationService) }
+  let(:user)    { create(:confirmed_user_en) }
+  let(:lecture) { create(:lecture) }
+  let(:tutorial) do
+    create(:tutorial,
+           lecture: lecture,
+           skip_campaigns: true,
+           self_materialization_mode: :add_and_remove)
+  end
+  let(:camp_tutorial) do
+    create(:tutorial,
+           lecture: lecture,
+           skip_campaigns: false)
+  end
+  let(:campaign) { create(:registration_campaign, campaignable: lecture) }
+  let(:item) do
+    create(:registration_item,
+           registration_campaign: campaign,
+           registerable: camp_tutorial)
+  end
 
   before do
     sign_in user
     Flipper.enable(:registration_campaigns)
     Flipper.enable(:roster_maintenance)
-
-    allow_any_instance_of(LectureAbility)
-      .to receive(:authorize!)
-      .and_return(true)
-
-    allow(Rosters::SelfMaterializationService)
-      .to receive(:new)
-      .with(tutorial, user)
-      .and_return(service)
   end
 
   describe "POST /tutorials/:id/roster/self_add" do
-    before { allow(service).to receive(:self_add!) }
+    it "adds the user to the tutorial" do
+      expect do
+        post(self_add_tutorial_path(tutorial), as: :turbo_stream)
+      end.to change { tutorial.members.count }.by(1)
 
-    it "calls the service" do
-      post self_add_tutorial_path(tutorial), as: :turbo_stream
-
-      expect(service).to have_received(:self_add!)
       expect(response).to have_http_status(:ok)
       expect(response.body).to include(
         I18n.t("roster.messages.user_added",
@@ -36,39 +41,67 @@ RSpec.describe("Roster::SelfMaterializationController", type: :request) do
       )
     end
 
-    context "when RosterLockedError" do
-      before do
-        allow(service).to receive(:self_add!)
-          .and_raise(Rosters::SelfMaterializationService::RosterLockedError)
+    it "updates the rosterized entries turbo frame" do
+      post self_add_tutorial_path(tutorial), as: :turbo_stream
+
+      expect(response.body).to include('target="student_registration_rosterized_entries"')
+    end
+
+    it "updates the self materialization zone turbo frame" do
+      post self_add_tutorial_path(tutorial), as: :turbo_stream
+
+      expect(response.body).to include('target="self_materialization_zone"')
+    end
+
+    context "when user is already in another tutorial of the same lecture" do
+      let(:other_tutorial) do
+        create(:tutorial, lecture: lecture, skip_campaigns: true,
+                          self_materialization_mode: :add_and_remove)
       end
 
-      it "returns locked error flash" do
+      before { create(:tutorial_membership, tutorial: other_tutorial, user: user) }
+
+      it "returns a conflict error flash" do
         post self_add_tutorial_path(tutorial), as: :turbo_stream
+
+        expect(response).to have_http_status(:ok)
+        expect(response.body).to include(
+          I18n.t("roster.errors.user_already_in_bundle",
+                 user: user.info,
+                 group: other_tutorial.title)
+        )
+      end
+
+      it "does not add the user to the tutorial" do
+        expect do
+          post(self_add_tutorial_path(tutorial), as: :turbo_stream)
+        end.not_to(change { tutorial.members.count })
+      end
+
+    end
+
+    context "when the tutorial is locked" do
+      it "returns a locked error flash" do
+        post self_add_tutorial_path(camp_tutorial), as: :turbo_stream
+
         expect(response).to have_http_status(:ok)
         expect(response.body).to include(I18n.t("roster.errors.item_locked"))
       end
-    end
 
-    context "when RosterFullError" do
-      before do
-        allow(service).to receive(:self_add!)
-          .and_raise(Rosters::SelfMaterializationService::RosterFullError)
-      end
-
-      it "returns capacity error flash" do
-        post self_add_tutorial_path(tutorial), params: { type: "Tutorial" }, as: :turbo_stream
-        expect(response).to have_http_status(:ok)
-        expect(response.body).to include(I18n.t("roster.errors.capacity_exceeded"))
+      it "does not add the user" do
+        expect do
+          post(self_add_tutorial_path(camp_tutorial), as: :turbo_stream)
+        end.not_to(change { camp_tutorial.members.count })
       end
     end
 
-    context "when SelfAddNotAllowedError" do
-      before do
-        allow(service).to receive(:self_add!)
-          .and_raise(Rosters::SelfMaterializationService::SelfAddNotAllowedError)
+    context "when self-add is not allowed" do
+      let(:tutorial) do
+        create(:tutorial, lecture: lecture, skip_campaigns: true,
+                          self_materialization_mode: :remove_only)
       end
 
-      it "returns not allowed error flash" do
+      it "returns a not allowed error flash" do
         post self_add_tutorial_path(tutorial), as: :turbo_stream
 
         expect(response).to have_http_status(:ok)
@@ -76,112 +109,128 @@ RSpec.describe("Roster::SelfMaterializationController", type: :request) do
           I18n.t("roster.errors.self_add_not_allowed", type: "Tutorial")
         )
       end
-    end
 
-    context "when CapacityExceededError" do
-      before do
-        allow(service).to receive(:self_add!)
-          .and_raise(Rosters::MaintenanceService::CapacityExceededError)
+      it "does not add the user" do
+        expect do
+          post(self_add_tutorial_path(tutorial), as: :turbo_stream)
+        end.not_to(change { tutorial.members.count })
       end
 
-      it "returns capacity error flash" do
+    end
+
+    context "when the tutorial is full" do
+      let(:tutorial) do
+        create(:tutorial, lecture: lecture, skip_campaigns: true,
+                          self_materialization_mode: :add_and_remove,
+                          capacity: 1)
+      end
+
+      before do
+        other_user = create(:confirmed_user)
+        create(:tutorial_membership, tutorial: tutorial, user: other_user)
+      end
+
+      it "returns a capacity error flash" do
         post self_add_tutorial_path(tutorial), as: :turbo_stream
+
         expect(response).to have_http_status(:ok)
         expect(response.body).to include(I18n.t("roster.errors.capacity_exceeded"))
       end
-    end
 
-    context "when UserAlreadyInBundleError" do
-      let(:conflicting_group) { create(:tutorial, lecture: lecture, title: "Mo 14-16") }
-
-      before do
-        allow(service).to receive(:self_add!).and_raise(
-          Rosters::UserAlreadyInBundleError.new(conflicting_group)
-        )
-      end
-
-      it "returns a fully interpolated conflict error flash" do
-        post self_add_tutorial_path(tutorial), as: :turbo_stream
-
-        expect(response).to have_http_status(:ok)
-        expect(response.body).to include(
-          I18n.t("roster.errors.user_already_in_bundle",
-                 user: user.info,
-                 group: conflicting_group.title)
-        )
+      it "does not add the user" do
+        expect do
+          post(self_add_tutorial_path(tutorial), as: :turbo_stream)
+        end.not_to(change { tutorial.members.count })
       end
     end
 
-    context "when rendering the updated registration page state" do
-      let(:tutorial) do
-        create(:tutorial,
-               lecture: lecture,
-               skip_campaigns: true,
-               self_materialization_mode: :add_and_remove,
-               title: "Ti")
-      end
+    context "when user is already a member of this tutorial" do
+      before { create(:tutorial_membership, tutorial: tutorial, user: user) }
 
-      before do
-        allow(lecture).to receive(:locale_with_inheritance).and_return("de")
-        allow(Rosters::SelfMaterializationService).to receive(:new).and_call_original
-      end
-
-      it "uses the user locale and updates the confirmed registrations section" do
+      it "returns success without error" do
         post self_add_tutorial_path(tutorial), as: :turbo_stream
 
         expect(response).to have_http_status(:ok)
-        expect(response.body).to include("target=\"student_registration_rosterized_entries\"")
+      end
+
+      it "does not duplicate the membership" do
+        expect do
+          post(self_add_tutorial_path(tutorial), as: :turbo_stream)
+        end.not_to(change { tutorial.members.count })
+      end
+    end
+
+    context "uses user locale (not lecture locale) for response" do
+      before { user.update!(locale: :en) }
+
+      it "renders response in user locale" do
+        post self_add_tutorial_path(tutorial), as: :turbo_stream
+
         expect(response.body).to include(
           I18n.t("registration.user_registration.index.confirmed_cases", locale: :en)
-        )
-        expect(response.body).to include(
-          I18n.t("registration.user_registration.withdraw", locale: :en)
-        )
-        expect(response.body).not_to include(
-          I18n.t("registration.user_registration.withdraw", locale: :de)
         )
       end
     end
   end
 
   describe "DELETE /tutorials/:id/roster/self_remove" do
-    before { allow(service).to receive(:self_remove!) }
+    before { create(:tutorial_membership, tutorial: tutorial, user: user) }
 
-    it "calls the service" do
-      delete self_remove_tutorial_path(tutorial), as: :turbo_stream
+    it "removes the user from the tutorial" do
+      expect do
+        delete(self_remove_tutorial_path(tutorial), as: :turbo_stream)
+      end.to change { tutorial.members.count }.by(-1)
+
       expect(response).to have_http_status(:ok)
-      expect(service).to have_received(:self_remove!)
       expect(response.body).to include(
         I18n.t("roster.messages.user_removed", user: user.info)
       )
     end
 
-    context "when RosterLockedError" do
-      before do
-        allow(service).to receive(:self_remove!)
-          .and_raise(Rosters::SelfMaterializationService::RosterLockedError)
-      end
+    it "updates the rosterized entries turbo frame" do
+      delete self_remove_tutorial_path(tutorial), as: :turbo_stream
 
-      it "returns locked error flash" do
-        delete self_remove_tutorial_path(tutorial), as: :turbo_stream
+      expect(response.body).to include('target="student_registration_rosterized_entries"')
+    end
+
+    context "when the tutorial is locked" do
+
+      it "returns a locked error flash" do
+        delete self_remove_tutorial_path(camp_tutorial), as: :turbo_stream
+
         expect(response).to have_http_status(:ok)
         expect(response.body).to include(I18n.t("roster.errors.item_locked"))
       end
-    end
 
-    context "when SelfRemoveNotAllowedError" do
-      before do
-        allow(service).to receive(:self_remove!)
-          .and_raise(Rosters::SelfMaterializationService::SelfRemoveNotAllowedError)
+      it "does not remove the user" do
+        expect do
+          delete(self_remove_tutorial_path(camp_tutorial), as: :turbo_stream)
+        end.not_to(change { camp_tutorial.members.count })
       end
 
-      it "returns not allowed error flash" do
+    end
+
+    context "when self-remove is not allowed" do
+      let(:tutorial) do
+        create(:tutorial, lecture: lecture, skip_campaigns: true,
+                          self_materialization_mode: :add_only)
+      end
+
+      it "returns a not allowed error flash" do
         delete self_remove_tutorial_path(tutorial), as: :turbo_stream
+
         expect(response).to have_http_status(:ok)
         expect(response.body).to include(
           I18n.t("roster.errors.self_remove_not_allowed", type: "Tutorial")
         )
       end
+
+      it "does not remove the user" do
+        expect do
+          delete(self_remove_tutorial_path(tutorial), as: :turbo_stream)
+        end.not_to(change { tutorial.members.count })
+      end
+
     end
   end
 end
