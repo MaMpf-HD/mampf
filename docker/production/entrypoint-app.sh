b882e78d85ea774243e56bf355fcebada91968d8
fosterfarrell9 <28628554+fosterfarrell9@users.noreply.github.com>
2026-05-31 14:06:14 +0000

    Update entrypoint script to bind Rails server to all interfaces

--- docker/production/entrypoint-app.sh
+++ docker/production/entrypoint-app.sh
@@ -8,4 +8,4 @@ echo "running mampf app"
 prometheus_exporter --label "{\"container\": \"${HOSTNAME}\"}" -b 0.0.0.0 -p 9394 -a lib/collectors/mampf_collector.rb > /usr/src/app/log/prometheus_exporter.log 2>&1 &
 
 exec bundle exec thrust ./bin/rails server \
-  -p "${THRUSTER_TARGET_PORT:-3001}" -b 0.0.0.0
+  -p "${THRUSTER_TARGET_PORT:-3001}" -b ::
