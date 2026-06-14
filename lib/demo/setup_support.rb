1e7c41148d2397b48d0bbeda2362e6b57c262c2d
fosterfarrell9 <28628554+fosterfarrell9@users.noreply.github.com>
2026-06-14 22:09:41 +0000

    Add non-production checks to setup methods

--- lib/demo/setup_support.rb
+++ lib/demo/setup_support.rb
@@ -21,10 +21,12 @@ module Demo
     end
 
     def setup_campaigns!
+      ensure_non_production!
       Demo::CampaignSetupSupport.setup!
     end
 
     def setup_rosters!
+      ensure_non_production!
       setup_flags!
 
       Rails.logger.debug("=== Demo Roster Setup ===")
@@ -36,6 +38,7 @@ module Demo
     end
 
     def setup!
+      ensure_non_production!
       setup_rosters!
     end
 
