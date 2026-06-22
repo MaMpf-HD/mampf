358056f0e04c795ae8a913fd089278091c19ada7
fosterfarrell9 <28628554+fosterfarrell9@users.noreply.github.com>
2026-06-22 11:10:58 +0000

    Fix linter complaint

--- db/migrate/20260622000000_set_registration_policy_config_not_null.rb
+++ db/migrate/20260622000000_set_registration_policy_config_not_null.rb
@@ -5,7 +5,9 @@ class SetRegistrationPolicyConfigNotNull < ActiveRecord::Migration[8.0]
 
   def up
     RegistrationPolicy.where(config: nil).find_each do |policy|
+      # rubocop:disable Rails/SkipsModelValidations
       policy.update_columns(config: {})
+      # rubocop:enable Rails/SkipsModelValidations
     end
 
     change_column_null :registration_policies, :config, false
