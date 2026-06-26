9093b7edc4978074676f31bfb51b17f1a517799a
Splines <37160523+Splines@users.noreply.github.com>
2026-06-26 18:57:07 +0000

    Don't issue individual UPDATE statements

--- db/migrate/20260626000000_set_registration_policy_config_not_null.rb
+++ db/migrate/20260626000000_set_registration_policy_config_not_null.rb
@@ -3,11 +3,9 @@ class SetRegistrationPolicyConfigNotNull < ActiveRecord::Migration[8.0]
   end
 
   def up
-    RegistrationPolicy.where(config: nil).find_each do |policy|
-      # rubocop:disable Rails/SkipsModelValidations
-      policy.update_columns(config: {})
-      # rubocop:enable Rails/SkipsModelValidations
-    end
+    # rubocop:disable Rails/SkipsModelValidations
+    RegistrationPolicy.where(config: nil).update_all(config: {})
+    # rubocop:enable Rails/SkipsModelValidations
 
     change_column_null :registration_policies, :config, false
   end
