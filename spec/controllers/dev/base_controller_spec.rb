56c9ed4259942ef2ea6f0920be83b17b38158b8b
Splines <info@splines.me>
2026-07-17 21:37:46 +0000

    Add negative tests as well

--- spec/controllers/dev/base_controller_spec.rb
+++ spec/controllers/dev/base_controller_spec.rb
@@ -16,25 +16,44 @@ RSpec.describe(Dev::BaseController, type: :controller) do
   end
 
   describe "#verify_development_environment" do
-    before do
-      allow(Rails.env).to receive(:development?).and_return(true)
-    end
+    context "in development" do
+      before do
+        allow(Rails.env).to receive(:development?).and_return(true)
+      end
 
-    it "allows loopback hosts" do
-      ["127.0.0.1", "[::1]", "localhost"].each do |host|
-        request.host = host
-        get :index
-        expect(response).to have_http_status(:ok)
+      it "allows loopback hosts" do
+        ["127.0.0.1", "[::1]", "localhost"].each do |host|
+          request.host = host
+          get :index
+          expect(response).to have_http_status(:ok)
+        end
+      end
+
+      it "rejects non-local hosts" do
+        request.host = "example.com"
+
+        expect { get(:index) }.to raise_error(
+          ActionController::RoutingError,
+          "Not Found"
+        )
       end
     end
 
-    it "rejects non-local hosts" do
-      request.host = "example.com"
+    context "outside development" do
+      before do
+        allow(Rails.env).to receive(:development?).and_return(false)
+      end
 
-      expect { get(:index) }.to raise_error(
-        ActionController::RoutingError,
-        "Not Found"
-      )
+      it "rejects loopback hosts" do
+        ["127.0.0.1", "[::1]", "localhost"].each do |host|
+          request.host = host
+
+          expect { get(:index) }.to raise_error(
+            ActionController::RoutingError,
+            "Not Found"
+          )
+        end
+      end
     end
   end
 end
