fec1121c0320a22afeae59169b659010237edd13
Splines <37160523+Splines@users.noreply.github.com>
2026-07-17 15:34:57 +0000

    Revert "Rename route"
    This reverts commit 7bf8084761dbd3f182f131e2b6fa126a420c5e34.

--- spec/controllers/dev/base_controller_spec.rb
+++ spec/controllers/dev/base_controller_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe(Dev::BaseController, type: :controller) do
 
   before do
     routes.draw do
-      get "index" => "anonymous#index"
+      get "index" => "dev/base#index"
     end
   end
 
