d4968c1635d1c22e593225bc5e3adfdee4fc26a6
Phương Liên Tạ <phuonglienta2121999@gmail.com>
2026-07-27 01:31:26 +0000

    Add conditional checks for roster_cache in enabled_roster_for_lecture? and rostered_tutorial_for methods

--- app/helpers/submissions_helper.rb
+++ app/helpers/submissions_helper.rb
@@ -105,16 +105,24 @@ module SubmissionsHelper
   end
 
   def enabled_roster_for_lecture?(lecture)
-    roster_cache[:enabled].fetch(lecture.id) do
-      roster_cache[:enabled][lecture.id] =
-        Flipper.enabled?(:roster_maintenance) && lecture.roster_eligible_tutorials?
+    if respond_to?(:roster_cache) && roster_cache
+      roster_cache[:enabled].fetch(lecture.id) do
+        roster_cache[:enabled][lecture.id] =
+          Flipper.enabled?(:roster_maintenance) && lecture.roster_eligible_tutorials?
+      end
+    else
+      Flipper.enabled?(:roster_maintenance) && lecture.roster_eligible_tutorials?
     end
   end
 
   def rostered_tutorial_for(lecture)
-    roster_cache[:tutorial].fetch(lecture.id) do
-      roster_cache[:tutorial][lecture.id] =
-        enabled_roster_for_lecture?(lecture) ? current_user.tutorial_rosterized(lecture) : nil
+    if respond_to?(:roster_cache) && roster_cache
+      roster_cache[:tutorial].fetch(lecture.id) do
+        roster_cache[:tutorial][lecture.id] =
+          enabled_roster_for_lecture?(lecture) ? current_user.tutorial_rosterized(lecture) : nil
+      end
+    else
+      enabled_roster_for_lecture?(lecture) ? current_user.tutorial_rosterized(lecture) : nil
     end
   end
 
