fd3b7f803c4dac90aaccca414d48f47e50270b1d
Elia <51531784+EliaPfl@users.noreply.github.com>
2026-08-12 00:46:10 +0200

    Decaffeination of watchlists (#1047)
    This pull request refactors the Watchlist management UI and backend to
    use Turbo Frames and Turbo Streams for a more modern, interactive, and
    maintainable experience. It removes CoffeeScript and AJAX-based flows,
    simplifies controller logic, and updates the views to fully leverage
    Hotwire. Additionally, new end-to-end tests are added to ensure correct
    behavior for creating, editing, and deleting terms.
    It also fixes a bug where the watchlist visibility (public/private)
    could no longer be changed via the UI, ensuring that visibility updates
    are now correctly applied and persisted.
    
    ---------
    
    Co-authored-by: pn <212894315+PN-1000@users.noreply.github.com>
    Co-authored-by: fosterfarrell9 <28628554+fosterfarrell9@users.noreply.github.com>

--- app/models/watchlist_entry.rb
+++ app/models/watchlist_entry.rb
@@ -2,6 +2,7 @@ class WatchlistEntry < ApplicationRecord
   default_scope { order :medium_position }
   belongs_to :watchlist
   belongs_to :medium
+
   acts_as_list scope: :watchlist, top_of_list: 0, column: :medium_position
 
   validates :medium_id, uniqueness: { scope: :watchlist_id }
