7622a364c2b91d04ecbf6f2b38def8981b02894c
Splines <info@splines.me>
2026-08-20 19:32:19 +0000

    Improve lecture.id retrieval in test

--- e2e/lecture-subscriptions.spec.ts
+++ e2e/lecture-subscriptions.spec.ts
@@ -3,21 +3,19 @@ import { LecturePage } from "./page-objects/lecture_page";
 
 test.describe("student", () => {
   test("can subscribe", async ({ factory, student: { page } }) => {
-    for (let i = 0; i < 6; i++) {
-      await factory.create("lecture", ["released_for_all", "with_sparse_toc"]);
-    }
+    const lecture = await factory.create(
+      "lecture", ["released_for_all", "with_sparse_toc"],
+    );
 
     await page.goto("/main/start");
     await page.getByRole("link", { name: "Search", exact: true }).click();
     await page.getByRole("button", { name: "Lecture Search" }).click();
     await page.getByRole("button", { name: "Search", exact: true }).click();
 
-    const lectureCard = page.locator(".lectureCard").first();
-    const lectureId = await lectureCard.getAttribute("data-id");
-    await lectureCard.getByTitle("Subscribe").click();
-    await expect(lectureCard.getByTitle("Unsubscribe")).toBeVisible();
+    await page.getByTitle("Subscribe").click();
+    await expect(page.getByTitle("Unsubscribe")).toBeVisible();
 
-    await new LecturePage(page, Number(lectureId)).goto();
+    await new LecturePage(page, lecture.id).goto();
     await expect(page).toHaveURL(/\/outline$/);
     await expect(page.getByText("Lecture Contents")).toBeVisible();
     await expect(page.getByText("There is no content information.")).not.toBeVisible();
