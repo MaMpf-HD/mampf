4de6655618063063acdba99e9efee43e354a2e28
Florian Bürckel <florianbuerckel@gmail.com>
2026-08-23 11:00:56 +0200

    dev: configure development network and MampfSearch environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,6 +3,9 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # MampfSearch reaches MaMpf through the shared Docker network.
+  config.hosts << "mampf"
+
   # Make code changes take effect immediately without server restart.
   config.enable_reloading = true
 
