656bf176acb97fe0288b67c1d0a3c4dd4c6d47a2
Splines <info@splines.me>
2026-09-12 17:59:57 +0000

    Avoid create duplicate lectures in seed

--- lib/seeds/build_support.rb
+++ lib/seeds/build_support.rb
@@ -194,13 +194,11 @@ module Seeds
       end
 
       def lecture_for(term)
-        find_or_create_lecture!(term, "lecture", "Analysis #{label(term)}",
-                                "Ana #{label(term)}")
+        find_or_create_lecture!(term, "lecture", "Analysis", "Ana")
       end
 
       def seminar_for(term)
-        find_or_create_lecture!(term, "seminar", "Seminar #{label(term)}",
-                                "Sem #{label(term)}")
+        find_or_create_lecture!(term, "seminar", "Seminar", "Sem")
       end
 
       def label(term)
