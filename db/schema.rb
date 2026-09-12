c4af537ab39919ced3ea283307ca79fd34ad50bb
fosterfarrell9 <28628554+fosterfarrell9@users.noreply.github.com>
2026-09-12 16:14:28 +0000

    Drop the link to the old MÜSLI
    The sidebar carried an external link to the system whose job MaMpf
    now does itself, switched on per lecture with a flag on the edit
    page. With groups, points and exam eligibility living here, the link
    pointed back to the past. The flag, its column, the form field and
    the MUESLI_SERVER setting go with it.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_09_12_000000) do
+ActiveRecord::Schema[8.0].define(version: 2026_09_12_000001) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
   enable_extension "pg_trgm"
@@ -513,7 +513,6 @@ ActiveRecord::Schema[8.0].define(version: 2026_09_12_000000) do
     t.integer "start_section"
     t.text "organizational_concept"
     t.boolean "organizational"
-    t.boolean "muesli"
     t.text "released"
     t.text "content_mode"
     t.text "passphrase"
